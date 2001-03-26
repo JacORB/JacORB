5f7c8f8223afc16768916affe439099466d4afe0
Nicolas Noffke <nicolas.noffke@googlemail.com>
2001-03-26 11:50:36 +0000

    *** empty log message ***

--- demo/hello/Client.java
+++ demo/hello/Client.java
@@ -1,46 +1,68 @@
 package demo.hello;
 
 import java.io.*;
+
 import org.omg.CORBA.*;
 
 public class Client 
 {
-    public static void main(String args[]) 
+    public static void main( String args[] ) 
     {
+        if( args.length != 1 ) 
+	{
+            System.out.println( "Usage: java demo.hello.Client <ior_file>" );
+            System.exit( 1 );
+        }
+
         try 
 	{
+            File f = new File( args[ 0 ] );
+
+            //check if file exists
+            if( ! f.exists() )
+            {
+                System.out.println("File " + args[0] + 
+                                   " does not exist.");
+                
+                System.exit( -1 );
+            }
+            
+            //check if args[0] points to a directory
+            if( f.isDirectory() )
+            {
+                System.out.println("File " + args[0] + 
+                                   " is a directory.");
+                
+                System.exit( -1 );
+            }
+
             // initialize the ORB.
             ORB orb = ORB.init( args, null );
 
-            if( args.length == 0 )
-            {
-                System.err.println("usage: jaco Client <ior>");
-                System.exit(1);
-            }
+            BufferedReader br =
+                new BufferedReader( new FileReader( f ));
 
-            // get object reference from command-line argument
-            org.omg.CORBA.Object obj = orb.string_to_object( args[0] );
+            // get object reference from command-line argument file
+            org.omg.CORBA.Object obj = 
+                orb.string_to_object( br.readLine() );
 
-            // and narrowed it to HelloWorld.GoodDay
+            br.close();
+
+            // and narrow it to HelloWorld.GoodDay
+            // if this fails, a BAD_PARAM will be thrown
             GoodDay goodDay = GoodDayHelper.narrow( obj );
 
-            // check if stringified IOR is of the right type
-            if( goodDay == null ) 
-	    {
-                 System.err.println("stringified IOR is not of type GoodDay");
-                 System.exit( 1 );
-            }
 
             // invoke the operation and print the result
             System.out.println( goodDay.hello() );
 
             // invoke the operation again and print the wide string result
-            System.out.println( "wide string: " + goodDay.hello_wide("Hello Wörld, from ö 1 2 3 0 *&^%$#@!@") );
+            System.out.println( "wide string: " + 
+                                goodDay.hello_wide( "Hello Wörld, from ö 1 2 3 0 *&^%$#@!@"));
         }
-        // catch CORBA system exceptions
-        catch(SystemException ex) 
+        catch( Exception ex ) 
 	{
-            System.err.println(ex);
+            System.err.println( ex );
         }
     }
 }
--- demo/hello/Server.java
+++ demo/hello/Server.java
@@ -1,6 +1,7 @@
 package demo.hello;
 
 import java.io.*;
+
 import org.omg.CORBA.*;
 import org.omg.PortableServer.*;
 
@@ -9,46 +10,46 @@ public class Server
 {
     public static void main(String[] args) 
     {
-        if( args.length != 1 ) 
+        if( args.length != 2 ) 
 	{
             System.out.println(
-                "Usage: java demo.hello.Server <location>");
+                "Usage: java demo.hello.Server <ior_file> <location>");
             System.exit( 1 );
         }
 
-        try {
-
-	    /* turn off any output, otherwise it will garble up our
-	       IOR file */
-
-	    java.util.Properties props = new java.util.Properties();
-	    props.put("jacorb.verbosity","0");
-
+        try 
+        {            
             //init ORB
-	    ORB orb = ORB.init( args, props );
+	    ORB orb = ORB.init( args, null );
 
 	    //init POA
-	    POA poa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+	    POA poa = 
+                POAHelper.narrow( orb.resolve_initial_references( "RootPOA" ));
 
 	    poa.the_POAManager().activate();
 
             // create a GoodDay object
-            GoodDayImpl goodDayImpl = new GoodDayImpl( args[0] );	
+            GoodDayImpl goodDayImpl = new GoodDayImpl( args[1] );	
     
             // create the object reference
-            org.omg.CORBA.Object obj = poa.servant_to_reference( goodDayImpl );
+            org.omg.CORBA.Object obj = 
+                poa.servant_to_reference( goodDayImpl );
 
-            // print stringified object reference
-            System.out.println( orb.object_to_string( obj ) );
+            PrintWriter pw = 
+                new PrintWriter( new FileWriter( args[ 0 ] ));
+
+            // print stringified object reference to file
+            pw.println( orb.object_to_string( obj ));
+            
+            pw.flush();
+            pw.close();
     
             // wait for requests
 	    orb.run();
         }
-        catch(SystemException e) {
-	    e.printStackTrace();
-        }
-        catch(java.lang.Exception ie) {
-	    ie.printStackTrace();
+        catch( Exception e ) 
+        {
+            System.out.println( e );
         }
     }
 }
