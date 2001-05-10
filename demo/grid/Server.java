bc39d95380647161275a50ca891383a6739d7381
Nicolas Noffke <nicolas.noffke@googlemail.com>
2001-05-10 15:29:23 +0000

    *** empty log message ***

--- demo/grid/Server.java
+++ demo/grid/Server.java
@@ -2,67 +2,41 @@ package demo.grid;
 
 import java.io.*;
 import org.omg.CosNaming.*;
-import org.omg.PortableServer.*;
 
-/**
- * This is an "imrified" version of the grid server.
- * $Id$
- */
 public class Server
 {
     public static void main( String[] args )
     {
-
-        System.setProperty( "jacorb.implname", "grid" );
-        System.setProperty( "jacorb.use_imr", "on" );
-
+	org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
 	try
 	{
-            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
-
-       	    org.omg.PortableServer.POA root_poa = 
+       	    org.omg.PortableServer.POA poa = 
 		org.omg.PortableServer.POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 
-	    root_poa.the_POAManager().activate();
-
-	    org.omg.CORBA.Policy [] policies = new org.omg.CORBA.Policy[2];
-
-	    policies[0] = root_poa.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
-	    policies[1] = root_poa.create_id_assignment_policy(IdAssignmentPolicyValue.USER_ID);
-	    POA grid_poa = root_poa.create_POA( "GridServerPOA", 
-                                                root_poa.the_POAManager(), 
-                                                policies );
-	    grid_poa.the_POAManager().activate();
-
-	    for (int i=0; i<policies.length; i++) 
-		policies[i].destroy();			
-
-            gridImpl gi = new gridImpl();
-
-            byte[] id = "grid".getBytes();
-
-            grid_poa.activate_object_with_id( id, gi );
+	    poa.the_POAManager().activate();
 	    
-            // use the naming service
+	    org.omg.CORBA.Object o = 
+                poa.servant_to_reference( new gridImpl() );
 
-            NamingContextExt nc = 
-                NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
-            try
-            {
-                nc.resolve( nc.to_name("grid.example") );
-            }
-            catch( org.omg.CosNaming.NamingContextPackage.NotFound nf )
-            {
-                //Server isn't bound, so bind it
-                org.omg.CORBA.Object o = 
-                    grid_poa.servant_to_reference( gi );
+	    if( args.length == 1 ) 
+	    {
+	       	// write the object reference to args[0]
 
-                nc.bind( nc.to_name("grid.example"), o);
-            }
+		PrintWriter ps = 
+                    new PrintWriter(new FileOutputStream(new File( args[0] )));
+		ps.println( orb.object_to_string( o ) );
+		ps.close();
+	    } 
+	    else
+	    {
+                // use the naming service
 
-            Thread.sleep(15000);
+		NamingContextExt nc = 
+                    NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+		nc.bind( nc.to_name("grid.example"), o);
+	    }
 
-            orb.shutdown(true);
+            orb.run();
 	} 
 	catch ( Exception e )
 	{
