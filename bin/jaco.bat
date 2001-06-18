9a030a3e9b4f1aef57beda1b6faed5068114f431
Nicolas Noffke <nicolas.noffke@googlemail.com>
2001-06-18 07:53:39 +0000

    these files are generated automatically

--- bin/jaco.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem call java interpreter
-java -Xbootclasspath:C:\Work\JacORB\lib\jacorb.jar;C:\Programme\JavaSoft\JRE\1.3.0_01\lib\rt.jar;%CLASSPATH% -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
-
