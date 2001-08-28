8f735914610be9a76ef26e7ce0dc3511e342b142
Gerald Brose <mail@geraldbrose.de>
2001-08-28 13:31:59 +0000

    removed superflous loops

--- src/org/jacorb/poa/AOM.java
+++ src/org/jacorb/poa/AOM.java
@@ -112,16 +112,7 @@ public class AOM
 
         if ( unique ) 
         {
-            //System.out.println("AOM " + this + " unique, putting oid str : " + oidStr + " for  " + servant.hashCode() );
             servantMap.put(servant, oidStr);
-            for( Enumeration e = servantMap.keys(); e.hasMoreElements();)
-            {
-                Object o = e.nextElement();
-                //                System.out.println("servantMap(" + o.hashCode() + 
-                //                                   ") = " + servantMap.get( o ) );
-            }
-
-
         }
                 
         logTrace.printLog(Debug.POA | 2, oid, "object is activated");
@@ -172,15 +163,6 @@ public class AOM
             throw new POAInternalError("error: not UNIQUE_ID policy (getObjectId)");
 
         String oidStr = (String)servantMap.get(servant);
-
-        //System.out.println("AOM " + this + " getObjectId oid str : " + oidStr + " for  " + servant.hashCode() );
-
-            for( Enumeration e = servantMap.keys(); e.hasMoreElements();)
-            {
-                Object o = e.nextElement();
-                //                System.out.println("servantMap(" + o.hashCode() + 
-                //                 ") = " + servantMap.get( o ) );
-            }
 
         if (oidStr != null) 
             return POAUtil.string_to_objectId(oidStr);
