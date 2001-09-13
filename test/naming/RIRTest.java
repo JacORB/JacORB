9860d0b71131c2e32ac677dde50395746b34efe2
Gerald Brose <mail@geraldbrose.de>
2001-09-13 12:40:05 +0000

    *** empty log message ***

---
+++ test/naming/RIRTest.java
@@ -0,0 +1,69 @@
+package testes.naming;
+
+import org.omg.CosNaming.*;
+
+public class RIRTest
+{
+
+    public static void main(String args[]) 
+    { 
+	try
+	{
+	    org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args,null);
+
+            org.omg.CORBA.Object obj;
+
+            try
+            {
+                obj = orb.string_to_object("corbaname:rir:");
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+
+            try
+            {            
+                obj = orb.string_to_object("corbaname:rir:#");
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+
+            try
+            {
+                obj = orb.string_to_object("corbaname:rir:/");
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+
+            try
+            {
+                obj = orb.string_to_object("corbaname:rir:NameService");
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+
+	    System.out.println("done. ");       
+	    System.exit(0);
+	}
+	catch (Exception e) 
+	{
+	    e.printStackTrace();
+	    System.exit(1);
+	}
+    }
+}
+
+
+
+
+
+
+
+
