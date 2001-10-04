2f13d9c136276e8877b204c22ae3e5c2a3559d85
Gerald Brose <mail@geraldbrose.de>
2001-10-04 13:58:00 +0000

    *** empty log message ***

---
+++ src/org/jacorb/orb/giop/ConnectionListener.java
@@ -0,0 +1,18 @@
+package org.jacorb.orb.connection;
+
+/**
+ * ConnectionListener.java
+ *
+ *
+ * Created: Thu Oct  4 15:56:02 2001
+ *
+ * @author Nicolas Noffke
+ * @version $Id$
+ */
+
+public interface ConnectionListener 
+{   
+    public void connectionClosed();
+    public void connectionTimedOut();
+    public void streamClosed();
+}// ConnectionListener
