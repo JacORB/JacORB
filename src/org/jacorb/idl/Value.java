3fb7dd894133c5e980290116e6b0c1665cabc574
Andre Speigel <spiegel@drmirror.net>
2001-10-25 14:04:44 +0000

    Renamed from ValueDeclaration.

--- src/org/jacorb/idl/Value.java
+++ src/org/jacorb/idl/Value.java
@@ -21,7 +21,6 @@
 package org.jacorb.idl;
 
 /**
- * 
  * @author Gerald Brose
  * @version $Id$
  */
@@ -30,44 +29,126 @@ import java.util.Vector;
 import java.util.Enumeration;
 import java.io.*;
 
-class Value 
-    extends ValueDeclaration 
+class Value
+    extends TypeDeclaration
 {
-    public Value(int num)
+    private Value value;
+
+    public Value(int num) 
     {
 	super(num);
+	pack_name = "";
     }
+
+    public Object clone()
+    {
+	return value.clone();
+    }
+
+    public void setValue ( Value value)
+    {
+        this.value = value;
+    }
+
+    public TypeDeclaration declaration()
+    {
+	return value;
+    };
+
+    public String typeName()
+    {
+	return value.typeName();
+    }
+
+    public String signature()
+    {
+	return value.signature();
+    }
+
+   /**
+     *	we have to be able to distinguish between explicitly typedef'd
+     *	type names and anonymously defined type names
+     */
+
+//      public void markTypeDefd(String alias)
+//      {
+//  	value_decl.markTypeDefd(alias);
+//      }
+
+    /**
+     * @returns a string for an expression of type TypeCode 
+     * 			that describes this type
+     */
+
+    public String getTypeCodeExpression()
+    {
+	return value.getTypeCodeExpression();
+    }
+
+    public boolean basic()
+    {
+	return value.basic();
+    } 
 
     public void setPackage( String s)
     {
+        s = parser.pack_replace(s);
+	value.setPackage(s);
     }
 
-    public void parse()
-    {	
-        System.out.println("(Warning: Values types  are not implemented and therefore ignored!)");
+    public void set_included(boolean i)
+    {
+	included = i;
+	value.set_included(i);
+    }
+
+    public void parse() 	 
+    {
+	value.parse();
+    }
+
+    public String holderName()
+    {
+	return value.holderName();
     }
 
     public void print(PrintWriter ps)
     {
+	value.print( ps );
     }
+
+    public String toString()
+    {
+	return value.toString();
+    }
+ 
+    public void setEnclosingSymbol( IdlSymbol s )
+    {
+	if( enclosing_symbol != null && enclosing_symbol != s )
+	{
+	    System.err.println("was " + enclosing_symbol.getClass().getName() + " now: " + s.getClass().getName());
+	    throw new RuntimeException("Compiler Error: trying to reassign container for " + name );
+	}
+	enclosing_symbol = s;
+	value.setEnclosingSymbol(s);
+    }
+
+    public String printReadExpression(String streamname)
+    {
+	return value.printReadExpression( streamname);
+    }
+
+    public String printReadStatement(String var_name, String streamname)
+    {
+	return var_name + "=" + printReadExpression(streamname) + ";";
+    }
+
+    public String printWriteStatement(String var_name, String streamname)
+    {
+	return value.printWriteStatement(var_name, streamname);
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
