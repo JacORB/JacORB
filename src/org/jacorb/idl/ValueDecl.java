043f762131d7ec5859ff331399cf9be4137badba
Andre Speigel <spiegel@drmirror.net>
2001-10-25 13:10:11 +0000

    Initial revision.

---
+++ src/org/jacorb/idl/ValueDecl.java
@@ -0,0 +1,74 @@
+/*
+ *        JacORB - a free Java ORB
+ *
+ *   Copyright (C) 1997-2001  Gerald Brose.
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.jacorb.idl;
+
+/**
+ * 
+ * @author Andre Spiegel
+ * @version $Id$
+ */
+
+import java.util.Vector;
+import java.util.Enumeration;
+import java.io.*;
+
+class ValueDecl 
+    extends ValueDeclaration 
+{
+    public ValueDecl (int num)
+    {
+	super(num);
+    }
+
+    public void setPackage( String s)
+    {
+    }
+
+    public void parse()
+    {	
+        System.out.println("(Warning: Values types  are not implemented and therefore ignored!)");
+    }
+
+    public void print(PrintWriter ps)
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
