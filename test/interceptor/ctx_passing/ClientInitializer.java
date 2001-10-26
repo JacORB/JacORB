d6a780a06f84de8a7c94de347e3f87e58e45aaaf
Gerald Brose <mail@geraldbrose.de>
2001-10-26 22:02:20 +0000

    *** empty log message ***

---
+++ test/interceptor/ctx_passing/ClientInitializer.java
@@ -0,0 +1,58 @@
+package test.interceptor.ctx_passing;
+
+import org.omg.PortableInterceptor.*;
+import org.omg.IOP_N.*;
+
+/**
+ * ClientInitializer.java
+ *
+ *
+ * Created: Fri Oct 26 10:58:29 2001
+ *
+ * @author Nicolas Noffke
+ * @version $Id$
+ */
+
+public class ClientInitializer 
+    extends org.jacorb.orb.LocalityConstrainedObject 
+    implements ORBInitializer  
+{
+    public static int slot_id = -1;
+
+    public ClientInitializer()
+    {        
+    }
+
+    // implementation of org.omg.PortableInterceptor.ORBInitializerOperations interface
+
+    /**
+     *
+     * @param param1 <description>
+     */
+    public void pre_init(ORBInitInfo info) 
+    {
+    }
+
+    /**
+     *
+     * @param param1 <description>
+     */
+    public void post_init(ORBInitInfo info) 
+    {
+        try
+        {
+            slot_id = info.allocate_slot_id();
+        
+            Encoding encoding = new Encoding(ENCODING_CDR_ENCAPS.value, 
+                                             (byte) 1, (byte) 0);
+            Codec codec = info.codec_factory().create_codec(encoding);
+        
+            info.add_client_request_interceptor( new ClientInterceptor( slot_id, codec ));
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+}// ClientInitializer
+
---
+++ test/interceptor/ctx_passing/ServerInitializer.java
@@ -0,0 +1,58 @@
+package test.interceptor.ctx_passing;
+
+import org.omg.PortableInterceptor.*;
+import org.omg.IOP_N.*;
+
+/**
+ * ServerInitializer.java
+ *
+ *
+ * Created: Fri Oct 26 11:14:36 2001
+ *
+ * @author Nicolas Noffke
+ * @version $Id$
+ */
+
+public class ServerInitializer 
+    extends org.jacorb.orb.LocalityConstrainedObject 
+    implements ORBInitializer   
+{
+    public static int slot_id = -1;
+
+    public ServerInitializer ()
+    {
+    }
+
+    // implementation of org.omg.PortableInterceptor.ORBInitializerOperations interface
+
+    /**
+     *
+     * @param param1 <description>
+     */
+    public void pre_init(ORBInitInfo info) 
+    {
+    }
+
+    /**
+     *
+     * @param param1 <description>
+     */
+    public void post_init(ORBInitInfo info) 
+    {
+        try
+        {
+            slot_id = info.allocate_slot_id();
+        
+            Encoding encoding = new Encoding(ENCODING_CDR_ENCAPS.value, 
+                                             (byte) 1, (byte) 0);
+            Codec codec = info.codec_factory().create_codec(encoding);
+        
+            info.add_server_request_interceptor( new ServerInterceptor( slot_id, codec ));        
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+}// ServerInitializer
+
