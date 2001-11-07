acf509184cffee57865c6a77bc05b0e07ef474bb
Gerald Brose <mail@geraldbrose.de>
2001-11-07 14:23:34 +0000

    *** empty log message ***

--- src/org/jacorb/security/level2/ServerAccessDecisionInterceptor.java
+++ src/org/jacorb/security/level2/ServerAccessDecisionInterceptor.java
@@ -67,7 +67,6 @@ public class ServerAccessDecisionInterceptor
     public void receive_request_service_contexts(ServerRequestInfo ri) 
         throws ForwardRequest
     {
-        throw new org.omg.CORBA.NO_MEMORY();
     }
 
     public void receive_request(ServerRequestInfo ri)
