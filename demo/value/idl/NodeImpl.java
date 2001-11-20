af064aeadfdaa3b1efab58e628c1cc3e84fc505d
Andre Speigel <spiegel@drmirror.net>
2001-11-20 11:00:45 +0000

    Add comment explaining no-arg constructor.

--- demo/value/idl/NodeImpl.java
+++ demo/value/idl/NodeImpl.java
@@ -5,6 +5,11 @@ public class NodeImpl extends Node
 
     public NodeImpl()
     {
+        // This no-arg constructor is used at unmarshalling time.
+        // Because of its presence, there is no need for a separate
+        // value factory for this type.  This is a special JacORB feature,
+        // see README and org.jacorb.orb.ORB.lookup_value_factory() for 
+        // details.
     }
 
     public NodeImpl (int id)
