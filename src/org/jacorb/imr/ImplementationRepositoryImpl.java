510be33ee3bfa40578787a862463051d298a67a0
Steve Osselton <steve.osselton@prismtech.com>
2001-12-11 09:28:32 +0000

    Fix for bug #41 - imr not starting when jacorb.use_imr property is enabled

--- src/org/jacorb/imr/ImplementationRepositoryImpl.java
+++ src/org/jacorb/imr/ImplementationRepositoryImpl.java
@@ -515,7 +515,8 @@ public class ImplementationRepositoryImpl
 	String _ior_file_str = null;
 	String _backup_file_str = null;
 
-        System.setProperty( "jacorb.implname", "the_ImR" );
+        System.setProperty ("jacorb.implname", "the_ImR");
+        System.setProperty ("jacorb.use_imr", "off");
 
 	try{
 	    for (int i = 0; i < args.length ; i++){
