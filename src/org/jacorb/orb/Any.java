2a2ef4495d80f7e8ca40c8a237669031e58d047a
Jason Courage <jacorb@prismtech.com>
2002-02-20 10:50:28 +0000

    Improved code in insert_Object method.

--- src/org/jacorb/orb/Any.java
+++ src/org/jacorb/orb/Any.java
@@ -514,42 +514,28 @@ public final class Any
     { 
         value = o;
 
-        org.omg.CORBA.ORB orb;
+        org.omg.CORBA.ORB orb = null;
         String typeId = null;
         String name = "";
 
         if( value == null )
         {
-            orb = org.omg.CORBA.ORB.init();
-            typeId = "IDL:omg.org/CORBA/Object:1.0";
-            name = "Object";
+           orb = org.omg.CORBA.ORB.init();
+           typeId = "IDL:omg.org/CORBA/Object:1.0";
+           name = "Object";
         }
         else
         {            
-            orb = ((org.omg.CORBA.portable.ObjectImpl)o)._orb();
-            typeId = ((org.omg.CORBA.portable.ObjectImpl)o)._ids()[0];
+           orb = ((org.omg.CORBA.portable.ObjectImpl)o)._orb();
+           typeId = ((org.omg.CORBA.portable.ObjectImpl)o)._ids()[0];
 
-            // check if the repository Id is IDL format
-            if (typeId.startsWith ("IDL:"))
-            {
-               // parse the name from the repository Id string
-               try
-               {
-                  name = typeId.substring (0, typeId.lastIndexOf (':'));
-                  int start = name.lastIndexOf ('/');
-
-                  if (start == -1)
-                  {
-                     start = name.lastIndexOf (':');
-                  }
-                  name = name.substring (start + 1);
-               }
-               catch (Exception ex)
-               {
-                  name = null;
-                  ex.printStackTrace ();
-               }
-            }               
+           // check if the repository Id is in IDL format
+           if (typeId.startsWith ("IDL:"))
+           {
+              // parse the name from the repository Id string
+              name = typeId.substring (4, typeId.lastIndexOf (':'));
+              name = name.substring (name.lastIndexOf ('/') + 1);
+           }
         }
         typeCode = orb.create_interface_tc( typeId , name );
     }
