50db98748e11f65d8f7d8dfc26c07dccb0a72323
Jason Courage <jacorb@prismtech.com>
2002-03-04 14:19:40 +0000

    Renamed file to correct name for system exception (was TRANSACTION_ROLLBACK.java).

---
+++ src/java_mapping/CORBA/TRANSACTION_ROLLEDBACK.java
@@ -0,0 +1,27 @@
+package org.omg.CORBA;
+public final class TRANSACTION_ROLLEDBACK
+	extends org.omg.CORBA.SystemException
+{
+	public TRANSACTION_ROLLEDBACK()
+	{
+		super( "", 0, org.omg.CORBA.CompletionStatus.COMPLETED_MAYBE);
+	}
+
+	public TRANSACTION_ROLLEDBACK(String reason)
+	{
+		super( reason, 0, org.omg.CORBA.CompletionStatus.COMPLETED_MAYBE);
+	}
+
+	public TRANSACTION_ROLLEDBACK(int minor, org.omg.CORBA.CompletionStatus completed)
+	{
+		super( "", minor, completed);
+	}
+
+	public TRANSACTION_ROLLEDBACK(String reason, int minor, org.omg.CORBA.CompletionStatus completed)
+	{
+		super( reason, minor, completed);
+	}
+
+}
+
+
