56803ec8d2c0b05aa91c35e56caef68e115b7a4d
Jason Courage <jacorb@prismtech.com>
2002-03-08 16:28:37 +0000

    Fixed _type() to correctly calculate the TypeCode of a fixed point type contained in the Holder.

--- src/java_mapping/CORBA/FixedHolder.java
+++ src/java_mapping/CORBA/FixedHolder.java
@@ -1,44 +1,62 @@
 package org.omg.CORBA;
 
 /**
- *
+ * Holder class for fixed point types.
  *
  */
 
 public class FixedHolder
-    implements org.omg.CORBA.portable.Streamable
+   implements org.omg.CORBA.portable.Streamable
 {
-    public java.math.BigDecimal value;
+   public java.math.BigDecimal value;
 
-    public FixedHolder(){}
+   public FixedHolder(){}
 
-    public FixedHolder(java.math.BigDecimal o)
-    {
-	value = o;
-    }
+   public FixedHolder(java.math.BigDecimal o)
+   {
+      value = o;
+   }
 
-    public TypeCode _type()
-    {
-	String s = value.toString();
-	short digits = 0;
-	short scale = 0;	   
-	for( ; s.charAt( digits ) != '.'; digits++ );
+   public TypeCode _type()
+   {
+      String s = value.toString();
+      short digits = 0;
+      short scale = 0;
 
-	for( int i = digits + 1; i < s.length() ; scale++ );		
-		
-	return ORB.init().create_fixed_tc(digits,scale);
-    }
+      if ( ! s.startsWith("0.") )
+      {
+         for( int i = 0; i < s.length(); i++ )
+         {
+            if ( s.charAt(i) == '.' )
+            {
+               break;
+            }
+            digits++;
+         }
+      }
 
-    public void _read(org.omg.CORBA.portable.InputStream in)
-    {
-	value = in.read_fixed();
-    }
+      int decimal = s.indexOf('.');
+      if ( decimal != -1 )
+      {
+         s = s.substring( decimal + 1 );
+         for( int i = 0; i < s.length(); i++ )
+         {
+            digits++;
+            scale++;
+         }
+      }
+      
+      return ORB.init().create_fixed_tc( digits, scale );
+   }
 
-    public void _write(org.omg.CORBA.portable.OutputStream out)
-    {
-	out.write_fixed(value);
-    }
+   public void _read(org.omg.CORBA.portable.InputStream in)
+   {
+      value = in.read_fixed();
+   }
+
+   public void _write(org.omg.CORBA.portable.OutputStream out)
+   {
+      out.write_fixed(value);
+   }
 
 }
-
-
