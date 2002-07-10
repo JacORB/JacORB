c781a99ab27938e4f1b078472d44e730dd1e4b62
Steve Osselton <steve.osselton@prismtech.com>
2002-07-10 07:21:14 +0000

    Copyright update

--- src/org/jacorb/orb/dns/DNSLookup.java
+++ src/org/jacorb/orb/dns/DNSLookup.java
@@ -1,15 +1,26 @@
 package org.jacorb.orb.dns;
 
-import java.net.InetAddress;
-/**
- * DNSLookup.java
+/*
+ *        JacORB  - a free Java ORB
  *
+ *   Copyright (C) 1997-2002  Gerald Brose.
  *
- * Created: Thu Apr  5 10:45:02 2002
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
  *
- * @author Nicolas Noffke
- * @version $Id$
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+import java.net.InetAddress;
 
 public class DNSLookup  
 {
--- src/org/jacorb/orb/dns/DNSLookupDelegate.java
+++ src/org/jacorb/orb/dns/DNSLookupDelegate.java
@@ -1,21 +1,29 @@
 package org.jacorb.orb.dns;
 
-import java.net.InetAddress;
-
-/**
- * DNSLookupDelegate.java
+/*
+ *        JacORB  - a free Java ORB
  *
+ *   Copyright (C) 1997-2002  Gerald Brose.
  *
- * Created: Thu Apr  5 10:54:29 2002
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
  *
- * @author Nicolas Noffke
- * @version $Id$
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+import java.net.InetAddress;
 
 public interface DNSLookupDelegate  
 {                    
     public String inverseLookup( String ip );
-
     public String inverseLookup( InetAddress addr );
-
-} // DNSLookupDelegate
+}
