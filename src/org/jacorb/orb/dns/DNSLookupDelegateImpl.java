87e51afdb503bbc5ca0cdb503fedf1f968aab034
Steve Osselton <steve.osselton@prismtech.com>
2002-07-10 07:18:34 +0000

    Update so delegate only created if DNS support classes available

--- src/org/jacorb/orb/dns/DNSLookupDelegateImpl.java
+++ src/org/jacorb/orb/dns/DNSLookupDelegateImpl.java
@@ -1,23 +1,3 @@
-/*
- *        JacORB - a free Java ORB
- *
- *   Copyright (C) 1999-2002 Gerald Brose
- *
- *   This library is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU Library General Public
- *   License as published by the Free Software Foundation; either
- *   version 2 of the License, or (at your option) any later version.
- *
- *   This library is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *   Library General Public License for more details.
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this library; if not, write to the Free
- *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
 package org.jacorb.orb.dns;
 
 import org.xbill.DNS.*;
@@ -87,32 +67,12 @@ public class DNSLookupDelegateImpl
                     
     public String inverseLookup( String ip )
     {
-        try
-        {
-            return _inverseLookup( dns.inaddrString( ip ));
-        }
-        catch( NoClassDefFoundError e )
-        {
-            Debug.output( 1, "DNS lookup support is compiled, but the classes of the org.xbill.dns package are not present" );
-            Debug.output( 2, e );
-        }            
-        
-        return null;
+        return _inverseLookup (dns.inaddrString (ip));
     }
 
     public String inverseLookup( InetAddress addr )
     {
-        try
-        {
-            return _inverseLookup( dns.inaddrString( addr ));
-        }
-        catch( NoClassDefFoundError e )
-        {
-            Debug.output( 1, "DNS lookup support is compiled, but the classes of the org.xbill.dns package are not present" );
-            Debug.output( 2, e );
-        }
-        
-        return null;
+        return _inverseLookup (dns.inaddrString (addr));
     }
 
 } // DNSLookupDelegateImpl
