3ad8dac0f4c666b76e38aa3739e9dfae3527e434
cvs2git <>
2002-07-16 10:00:54 +0000

    This commit was manufactured by cvs2git to create branch 'RC_1_4_1_BRANCH'.
    Cherrypick from master 2002-07-16 10:00:53 UTC Gerald Brose <mail@geraldbrose.de> '*** empty log message ***':
        test/POA/deactivate/Deactivate.java
        test/POA/deactivate/Test_impl.java

---
+++ test/POA/deactivate/Test_impl.java
@@ -0,0 +1,21 @@
+package test.POA.deactivate;
+
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POAPackage.*;
+
+
+public class Test_impl extends Servant
+{
+    public Test_impl()
+    {}	
+
+    public String[] _all_interfaces(org.omg.PortableServer.POA poa, byte[] obj_id)
+
+	{
+
+		return new String[]{"IDL:omg.org/CORBA/Object:1.0"};
+
+	}
+
+}
