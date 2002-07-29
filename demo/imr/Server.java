3ea94130df1c425b2c670bfaba3f3d4083ef6086
Nicolas Noffke <nicolas.noffke@googlemail.com>
2002-07-29 07:24:46 +0000

    removed demos ac and domain, fixed dii demo, added complete imr demo

--- demo/imr/Server.java
+++ demo/imr/Server.java
@@ -1,67 +1,97 @@
-package demo.grid;
+package demo.imr;
 
 import java.io.*;
-import org.omg.CosNaming.*;
+
+import org.omg.CORBA.*;
 import org.omg.PortableServer.*;
 
-public class Server
+import org.jacorb.util.*;
+
+public class Server 
+    extends SomeIfPOA
 {
-    public static void main( String[] args )
+    public Server()
     {
+    }
 
-        System.setProperty( "jacorb.implname", "grid" );
-        System.setProperty( "jacorb.use_imr", "on" );
-	try
+    public void op()
+    {
+        System.out.println("Server: Received call from client");        
+    }
+        
+    public static void main(String[] args) 
+    {
+        if( args.length != 2 ) 
 	{
-            System.out.println("ImR Grid Server");
-            org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
+            System.out.println(
+                "Usage: jaco demo.imr.Server <ior_file> <timeout in secs>");
+            System.exit( 1 );
+        }
 
+        System.setProperty( "jacorb.implname", "imr_demo" );
+        System.setProperty( "jacorb.use_imr", "on" );
+
+        try 
+        {   
+            long timeout = Integer.parseInt( args[1] ) * 1000;
+            //init ORB
+	    ORB orb = ORB.init( args, null );
+
+            //get root POA
        	    org.omg.PortableServer.POA root_poa = 
-		org.omg.PortableServer.POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		org.omg.PortableServer.POAHelper.narrow(
+                    orb.resolve_initial_references("RootPOA"));
 
-	    root_poa.the_POAManager().activate();
-
+            //create necessary policies
 	    org.omg.CORBA.Policy[] policies = new org.omg.CORBA.Policy[2];
 
 	    policies[0] = root_poa.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
 	    policies[1] = root_poa.create_id_assignment_policy(IdAssignmentPolicyValue.USER_ID);
-	    POA grid_poa = root_poa.create_POA( "GridServerPOA", 
+            
+            //create user POA with these policies
+	    POA demo_poa = root_poa.create_POA( "ImRDemoServerPOA", 
                                                 root_poa.the_POAManager(), 
                                                 policies );
+            
+            //destroy policies
 	    for (int i=0; i<policies.length; i++) 
 		policies[i].destroy();			
 
-            gridImpl gi = new gridImpl();
+            //instanciate implementation
+            Server s = new Server();
 
-            byte[] id = "grid".getBytes();
+            //create object id
+            byte[] id = "imr_demo".getBytes();
 
-            grid_poa.activate_object_with_id( id, gi );
-	    
-            // use the naming service
+            //activate object
+            demo_poa.activate_object_with_id( id, s );
 
-            NamingContextExt nc = 
-                NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
-            try
-            {
-                nc.resolve( nc.to_name("grid.example") );
-            }
-            catch( org.omg.CosNaming.NamingContextPackage.NotFound nf )
-            {
-                //Server isn't bound, so bind it
-                org.omg.CORBA.Object o = 
-                    grid_poa.servant_to_reference( gi );
-
-                nc.bind( nc.to_name("grid.example"), o);
-            }            
-        
+            //make POA accept requests
 	    root_poa.the_POAManager().activate();
 
-            orb.run();
-	} 
-	catch ( Exception e )
-	{
-	    e.printStackTrace();
-	}        
+            
+            // create the object reference
+            org.omg.CORBA.Object obj = 
+                demo_poa.servant_to_reference( s );
+
+            PrintWriter pw = 
+                new PrintWriter( new FileWriter( args[ 0 ] ));
+
+            // print stringified object reference to file
+            pw.println( orb.object_to_string( obj ));
+            
+            pw.flush();
+            pw.close();
+    
+            // wait for requests
+	    Thread.sleep( timeout );
+
+            orb.shutdown( true );
+        }
+        catch( Exception e ) 
+        {
+            System.out.println( e );
+        }
     }
 }
 
