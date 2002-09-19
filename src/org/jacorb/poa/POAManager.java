ef51366bfbf6bfdb87a2800961f502a0a46a95d6
Nick Cross <jacorb@goots.org>
2002-09-19 10:31:13 +0000

    Whitespace tab cleanup

--- src/org/jacorb/poa/POAManager.java
+++ src/org/jacorb/poa/POAManager.java
@@ -19,7 +19,7 @@ package org.jacorb.poa;
  *   License along with this library; if not, write to the Free
  *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
- 
+
 import org.jacorb.util.Environment;
 
 import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
@@ -36,210 +36,264 @@ import java.lang.Thread;
  * @version $Id$
  */
 
-public class POAManager 
+public class POAManager
     extends org.omg.PortableServer._POAManagerLocalBase
-  //    extends org.omg.CORBA.LocalObject 
-  //    implements org.omg.PortableServer.POAManager 
 {
-	public State state = State.HOLDING;
-	private org.jacorb.orb.ORB orb;
-	private Vector poas = new Vector();
-	private POAManagerMonitor monitor;
-	protected POAManager(org.jacorb.orb.ORB _orb) {
-		orb = _orb;
-		monitor = new POAManagerMonitorLightImpl();
-		monitor.init(this);
-		monitor.openMonitor();
-		monitor.printMessage("ready");
-	}
-	public void activate() throws AdapterInactive {
-		switch (state.value()) {
-			case State._INACTIVE :
-				throw new AdapterInactive();
-			case State._ACTIVE :
-				break;
-			default:
-				state = State.ACTIVE;
-				monitor.setToActive();
+    public State state = State.HOLDING;
+    private org.jacorb.orb.ORB orb;
+    private Vector poas = new Vector();
+    private POAManagerMonitor monitor;
 
-				final POA [] poaArray;
 
-				synchronized (this) {
-					poaArray = new POA[poas.size()];
-					poas.copyInto(poaArray);
-				}
-				// notify all registered poas
-				Thread thread = new Thread() { 
-					public void run() {
-						for (int i=0; i<poaArray.length; i++) {
-							try {
-								poaArray[i].changeToActive();
-							} catch (Throwable e) {}
-						}
-					}
-				};				
-				thread.start();
-		}
-	}
-	public void deactivate(boolean etherealize_objects, boolean wait_for_completion) throws AdapterInactive {
+    protected POAManager(org.jacorb.orb.ORB _orb)
+    {
+        orb = _orb;
+        monitor = new POAManagerMonitorLightImpl();
+        monitor.init(this);
+        monitor.openMonitor();
+        monitor.printMessage("ready");
+    }
 
-		if (wait_for_completion && isInInvocationContext()) {
-			throw new org.omg.CORBA.BAD_INV_ORDER();
-		}
-		
-		switch (state.value()) {
-			case State._INACTIVE :
-				throw new AdapterInactive();
-			default :
-				state = State.INACTIVE;	
-				monitor.setToInactive(wait_for_completion, etherealize_objects);
-	
-				final boolean etherealize = etherealize_objects;
-				final POA [] poaArray;
 
-				synchronized (this) {
-					poaArray = new POA[poas.size()];
-					poas.copyInto(poaArray);
-				}
-				// notify all registered poas
-				Thread thread = new Thread() { 
-					public void run() {
-						for (int i=poaArray.length-1; i>=0; i--) {
-							try {
-								poaArray[i].changeToInactive(etherealize);
-							} catch (Throwable e) {}
-						}
-					}
-				};				
-				thread.start();
-				if (wait_for_completion) {
-					try {
-						thread.join();
-					} catch (InterruptedException e) {}
-				}
-		}
-	}
-	public void discard_requests(boolean wait_for_completion) throws AdapterInactive {
+    public void activate() throws AdapterInactive
+    {
+        switch (state.value())
+        {
+            case State._INACTIVE :
+            throw new AdapterInactive();
+            case State._ACTIVE :
+            break;
+            default:
+            state = State.ACTIVE;
+            monitor.setToActive();
 
-		if (wait_for_completion && isInInvocationContext()) {
-			throw new org.omg.CORBA.BAD_INV_ORDER();
-		}
-		
-		switch (state.value()) {
-			case State._INACTIVE :
-				throw new AdapterInactive();
-			case State._DISCARDING :
-				break;
-			default :
-				state = State.DISCARDING;
-				monitor.setToDiscarding(wait_for_completion);
+            final POA [] poaArray;
 
-				final POA [] poaArray;
+            synchronized (this)
+            {
+                poaArray = new POA[poas.size()];
+                poas.copyInto(poaArray);
+            }
+            // notify all registered poas
+            Thread thread = new Thread()
+            {
+                public void run()
+                {
+                    for (int i=0; i<poaArray.length; i++)
+                    {
+                        try {
+                            poaArray[i].changeToActive();
+                        } catch (Throwable e) {}
+                    }
+                }
+            };
+            thread.start();
+        }
+    }
 
-				synchronized (this) {
-					poaArray = new POA[poas.size()];
-					poas.copyInto(poaArray);
-				}
-				// notify all registered poas
-				Thread thread = new Thread() { 
-					public void run() {
-						for (int i=poaArray.length-1; i>=0; i--) {
-							try {
-								poaArray[i].changeToDiscarding();
-							} catch (Throwable e) {}
-						}
-					}
-				};				
-				thread.start();
-				if (wait_for_completion) {
-					try {
-						thread.join();
-					} catch (InterruptedException e) {}
-				}
-		}
-	}
-	public State get_state() {
-		return state;
-	}
-	synchronized protected POA getRegisteredPOA(String name) {
-		POA result;
-		Enumeration en = poas.elements();
-		while (en.hasMoreElements()) {
-			result = (POA) en.nextElement();
-			if (name.equals(result._getQualifiedName())) {
-				return result;
-			}
-		}
-		throw new Error("POA not registered: " +
-			POAConstants.ROOT_POA_NAME+POAConstants.OBJECT_KEY_SEPARATOR+name);
-	}
-	public void hold_requests(boolean wait_for_completion) throws AdapterInactive {
 
-		if (wait_for_completion && isInInvocationContext()) {
-			throw new org.omg.CORBA.BAD_INV_ORDER();
-		}	
-		switch (state.value()) {
-			case State._INACTIVE :
-				throw new AdapterInactive();
-			case State._HOLDING :
-				break;
-			default :
-				state = State.HOLDING;
-				monitor.setToHolding(wait_for_completion);
+    public void deactivate
+        (boolean etherealize_objects, boolean wait_for_completion)
+        throws AdapterInactive
+    {
+        if (wait_for_completion && isInInvocationContext())
+        {
+            throw new org.omg.CORBA.BAD_INV_ORDER();
+        }
 
-				final POA [] poaArray;
+        switch (state.value())
+        {
+            case State._INACTIVE :
+            throw new AdapterInactive();
+            default :
+            state = State.INACTIVE;
+            monitor.setToInactive(wait_for_completion, etherealize_objects);
 
-				synchronized (this) {
-					poaArray = new POA[poas.size()];
-					poas.copyInto(poaArray);
-				}
-				// notify all registered poas
-				Thread thread = new Thread() { 
-					public void run() {
-						for (int i=poaArray.length-1; i>=0; i--) {
-							try {
-								poaArray[i].changeToHolding();
-							} catch (Throwable e) {}
-						}
-					}
-				};				
-				thread.start();
-				if (wait_for_completion) {
-					try {
-						thread.join();
-					} catch (InterruptedException e) {}
-				}
-		}					
-	}
-	/**
-	 * it returns true if the current thread is not in an invocation
-	 * context dispatched by some POA belonging to the same ORB as this POAManager.
-	 */
-	private boolean isInInvocationContext() {
-		try {
-			if (orb.getPOACurrent().getORB() == orb) return true;
-			
-		} catch (org.omg.PortableServer.CurrentPackage.NoContext e) {}
-		return false;
-	}
-	synchronized protected void registerPOA(POA poa) {
-		if (!poas.contains(poa)) {
-			poas.addElement(poa);
-			monitor.addPOA(poa._getQualifiedName());
-		}
-	}
-	protected void setMonitor(POAManagerMonitor _monitor) {
-		monitor = _monitor;
-	}
-	synchronized protected void unregisterPOA(POA poa) {
-		poas.removeElement(poa);
-		monitor.removePOA(poa._getQualifiedName());
-	}
+            final boolean etherealize = etherealize_objects;
+            final POA [] poaArray;
+
+            synchronized (this)
+            {
+                poaArray = new POA[poas.size()];
+                poas.copyInto(poaArray);
+            }
+            // notify all registered poas
+            Thread thread = new Thread()
+            {
+                public void run()
+                {
+                    for (int i=poaArray.length-1; i>=0; i--)
+                    {
+                        try {
+                            poaArray[i].changeToInactive(etherealize);
+                        } catch (Throwable e) {}
+                    }
+                }
+            };
+            thread.start();
+            if (wait_for_completion)
+            {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+
+    public void discard_requests(boolean wait_for_completion)
+        throws AdapterInactive
+    {
+        if (wait_for_completion && isInInvocationContext())
+        {
+            throw new org.omg.CORBA.BAD_INV_ORDER();
+        }
+
+        switch (state.value())
+        {
+            case State._INACTIVE :
+            throw new AdapterInactive();
+            case State._DISCARDING :
+            break;
+            default :
+            state = State.DISCARDING;
+            monitor.setToDiscarding(wait_for_completion);
+
+            final POA [] poaArray;
+
+            synchronized (this)
+            {
+                poaArray = new POA[poas.size()];
+                poas.copyInto(poaArray);
+            }
+            // notify all registered poas
+            Thread thread = new Thread()
+            {
+                public void run()
+                {
+                    for (int i=poaArray.length-1; i>=0; i--)
+                    {
+                        try {
+                            poaArray[i].changeToDiscarding();
+                        } catch (Throwable e) {}
+                    }
+                }
+            };
+            thread.start();
+            if (wait_for_completion)
+            {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+
+    public State get_state()
+    {
+        return state;
+    }
+
+
+    synchronized protected POA getRegisteredPOA(String name)
+    {
+        POA result;
+        Enumeration en = poas.elements();
+        while (en.hasMoreElements())
+        {
+            result = (POA) en.nextElement();
+            if (name.equals(result._getQualifiedName()))
+            {
+                return result;
+            }
+        }
+        throw new Error("POA not registered: " +
+                        POAConstants.ROOT_POA_NAME+POAConstants.OBJECT_KEY_SEPARATOR+name);
+    }
+
+
+    public void hold_requests(boolean wait_for_completion)
+        throws AdapterInactive
+    {
+        if (wait_for_completion && isInInvocationContext())
+        {
+            throw new org.omg.CORBA.BAD_INV_ORDER();
+        }
+        switch (state.value())
+        {
+            case State._INACTIVE :
+            throw new AdapterInactive();
+            case State._HOLDING :
+            break;
+            default :
+            state = State.HOLDING;
+            monitor.setToHolding(wait_for_completion);
+
+            final POA [] poaArray;
+
+            synchronized (this)
+            {
+                poaArray = new POA[poas.size()];
+                poas.copyInto(poaArray);
+            }
+            // notify all registered poas
+            Thread thread = new Thread()
+            {
+                public void run()
+                {
+                    for (int i=poaArray.length-1; i>=0; i--)
+                    {
+                        try {
+                            poaArray[i].changeToHolding();
+                        } catch (Throwable e) {}
+                    }
+                }
+            };
+            thread.start();
+            if (wait_for_completion)
+            {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+
+    /**
+     * it returns true if the current thread is not in an invocation
+     * context dispatched by some POA belonging to the same ORB as this POAManager.
+     */
+    private boolean isInInvocationContext()
+    {
+        try {
+            if (orb.getPOACurrent().getORB() == orb) return true;
+
+        } catch (org.omg.PortableServer.CurrentPackage.NoContext e) {}
+        return false;
+    }
+
+
+    synchronized protected void registerPOA(POA poa)
+    {
+        if (!poas.contains(poa))
+        {
+            poas.addElement(poa);
+            monitor.addPOA(poa._getQualifiedName());
+        }
+    }
+
+
+    protected void setMonitor(POAManagerMonitor _monitor)
+    {
+        monitor = _monitor;
+    }
+
+
+    synchronized protected void unregisterPOA(POA poa)
+    {
+        poas.removeElement(poa);
+        monitor.removePOA(poa._getQualifiedName());
+    }
 }
-
-
-
-
-
-
-
