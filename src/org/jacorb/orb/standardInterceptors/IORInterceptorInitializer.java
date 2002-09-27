176e0947e458ce64438fafed689582c5fce0b4fc
Nick Cross <jacorb@goots.org>
2002-09-27 15:16:18 +0000

    Prevent SASInterceptor being built under 1.3

--- src/org/jacorb/orb/standardInterceptors/IORInterceptorInitializer.java
+++ src/org/jacorb/orb/standardInterceptors/IORInterceptorInitializer.java
@@ -20,41 +20,43 @@
  */
 package org.jacorb.orb.standardInterceptors;
 
+import java.lang.reflect.Constructor;
 import org.omg.PortableInterceptor.*;
 import org.jacorb.orb.*;
 import org.jacorb.util.Environment;
+
 /**
- * This class initializes the default IOR interceptors 
+ * This class initializes the default IOR interceptors
  * used by JacORB.
  *
  * @author Nicolas Noffke
  * @version $Id$
  */
 
-public class IORInterceptorInitializer 
-    extends org.omg.CORBA.LocalObject 
+public class IORInterceptorInitializer
+    extends org.omg.CORBA.LocalObject
     implements ORBInitializer
 {
-  
+
     public IORInterceptorInitializer() {
-    
+
     }
 
     // implementation of org.omg.PortableInterceptor.ORBInitializerOperations interface
 
     /**
-     * Adds the SSLComponentInterceptor and the CodeSetInfoInterceptor 
+     * Adds the SSLComponentInterceptor and the CodeSetInfoInterceptor
      * to the set of IORInterceptors.
      *
      * @param info the info object.
      */
-    public void post_init(ORBInitInfo info) 
+    public void post_init(ORBInitInfo info)
     {
         try
         {
             ORB orb = ((org.jacorb.orb.portableInterceptor.ORBInitInfoImpl) info).getORB();
             if( Environment.isPropertyOn( "jacorb.security.support_ssl" ) &&
-                Environment.hasProperty( "jacorb.security.ssl.server.supported_options" ) && 
+                Environment.hasProperty( "jacorb.security.ssl.server.supported_options" ) &&
                 Environment.hasProperty( "jacorb.security.ssl.server.required_options" ))
             {
                 info.add_ior_interceptor(new SSLComponentInterceptor(orb));
@@ -62,15 +64,30 @@ public class IORInterceptorInitializer
 
             if( Environment.isPropertyOn( "jacorb.security.support_sas" ))
             {
-                info.add_ior_interceptor(new SASComponentInterceptor(orb));
+                try
+                {
+                    Class sas = Class.forName ("SASComponentInterceptor");
+                    Constructor csas = sas.getConstructor
+                        (new Class[] { orb.getClass () });
+
+                    info.add_ior_interceptor
+                    (
+                        (IORInterceptor)csas.newInstance (new Object[] {orb})
+                    );
+                }
+                catch (Exception e)
+                {
+                    org.jacorb.util.Debug.output (1, "Unable to instantiate SASComponentInterceptor");
+                    org.jacorb.util.Debug.output (1, e);
+                }
             }
 
-            int giop_minor = 
-                Integer.parseInt( 
-                    Environment.getProperty( 
+            int giop_minor =
+                Integer.parseInt(
+                    Environment.getProperty(
                         "jacorb.giop_minor_version",
                         "2" ));
-                
+
             if( giop_minor > 0 )
             {
                 info.add_ior_interceptor(new CodeSetInfoInterceptor(orb));
@@ -87,11 +104,9 @@ public class IORInterceptorInitializer
      * @param info <description>
      */
 
-    public void pre_init(ORBInitInfo info) 
+    public void pre_init(ORBInitInfo info)
     {
         // do nothing
     }
 
 } // IORInterceptorInitializer
-
-
