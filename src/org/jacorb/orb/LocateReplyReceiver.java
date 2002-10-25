c7e94cecc965d188baa2ccb88974518b2ee9c1f4
Andre Speigel <spiegel@drmirror.net>
2002-10-25 08:52:08 +0000

    Initial revision.

---
+++ src/org/jacorb/orb/LocateReplyReceiver.java
@@ -0,0 +1,67 @@
+/*
+ *        JacORB - a free Java ORB
+ *
+ *   Copyright (C) 1997-2002  Gerald Brose.
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.jacorb.orb;
+
+import org.jacorb.orb.*;
+import org.jacorb.orb.connection.MessageInputStream;
+import org.jacorb.orb.connection.LocateReplyInputStream;
+import org.jacorb.util.*;
+
+import org.omg.GIOP.*;
+import org.omg.CORBA.portable.RemarshalException;
+
+/**
+ * A special ReplyPlaceholder that receives LocateReplies.
+ *
+ * @author Andre Spiegel
+ * @version $Id$
+ */
+public class LocateReplyReceiver 
+    extends org.jacorb.orb.connection.ReplyPlaceholder
+{
+    public LocateReplyReceiver()
+    {        
+        super();
+    }
+
+    /**
+     * This method blocks until a reply becomes available.
+     */    
+    public synchronized LocateReplyInputStream getReply() 
+	throws RemarshalException
+    {
+        return (LocateReplyInputStream)super.getInputStream();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
