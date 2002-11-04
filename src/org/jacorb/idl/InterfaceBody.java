240fbdcf0fab3bd189611104f5ddd1d9ead19fa0
Andre Speigel <spiegel@drmirror.net>
2002-11-04 17:47:57 +0000

    (addDefinition): New method. A few simplifications.

--- src/org/jacorb/idl/InterfaceBody.java
+++ src/org/jacorb/idl/InterfaceBody.java
@@ -171,6 +171,11 @@ class InterfaceBody
             ( (IdlSymbol)e.nextElement() ).setPackage( s );
     }
 
+    public void addDefinition (Declaration d)
+    {
+        this.v.add (new Definition (d));
+    }
+
     public void parse()
     {
         escapeName();
@@ -329,21 +334,13 @@ class InterfaceBody
                     }
                 }
             }
-            if( my_interface.inheritanceSpec.v.size() > 0 )
+            for (Iterator i = my_interface.inheritanceSpec.v.iterator(); 
+                 i.hasNext(); )
             {
-                for( Enumeration e = my_interface.inheritanceSpec.v.elements(); e.hasMoreElements(); )
+                TypeSpec ts = ((ScopedName)i.next()).resolvedTypeSpec();
+                if (ts instanceof ConstrTypeSpec)
                 {
-                    ScopedName sn = ( (ScopedName)e.nextElement() );
-                    Interface base = null;
-                    try
-                    {
-                        base = (Interface)( (ConstrTypeSpec)sn.resolvedTypeSpec() ).c_type_spec;
-                    }
-                    catch( Exception ex )
-                    {
-                        ex.printStackTrace();
-                        parser.fatal_error( "Cannot find base interface " + sn, token );
-                    }
+                    Interface base = (Interface)((ConstrTypeSpec)ts).c_type_spec;
                     Operation[] base_ops = base.getBody().getMethods();
                     for( int j = 0; j < base_ops.length; j++ )
                     {
@@ -368,15 +365,19 @@ class InterfaceBody
                                   boolean is_local )
     {
         Operation[] ops = getMethods();
-        if( ops.length > 0 )
+        for( int i = 0; i < ops.length; i++ )
+            ops[ i ].printMethod( ps, classname, is_local );
+        
+        if ( parser.generate_ami_callback && 
+             !(my_interface instanceof ReplyHandler) )
         {
             for( int i = 0; i < ops.length; i++ )
-                ops[ i ].printMethod( ps, classname, is_local );
+                ops[ i ].print_sendc_Method( ps, classname );
         }
     }
 
 
-    /** print methods to the stub file */
+    /** print methods to the skeleton file */
 
     public void printDelegatedMethods( PrintWriter ps )
     {
