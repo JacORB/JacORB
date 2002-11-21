1b66ee914cbb2e76adc9bf53a6825e0a39f72328
Andre Speigel <spiegel@drmirror.net>
2002-11-21 11:22:13 +0000

    Initial revision.

---
+++ src/org/jacorb/util/Time.java
@@ -0,0 +1,117 @@
+package org.jacorb.util;
+
+/*
+ *        JacORB - a free Java ORB
+ *
+ *   Copyright (C) 2002 Gerald Brose
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+import org.omg.TimeBase.*;
+
+/**
+ * Contains static methods to handle CORBA time values.
+ * 
+ * @author Andre Spiegel <spiegel@gnu.org>
+ * @version $Id$
+ */
+public class Time
+{
+    /**
+     * Difference between the CORBA Epoch and the Unix Epoch: the time
+     * from 1582/10/15 00:00 until 1970/01/01 00:00 in 100 ns units.
+     */
+    public static final long UNIX_OFFSET = 122192928000000000L;
+
+    /**
+     * Returns the current time as a CORBA UtcT.
+     */
+    public static UtcT corbaTime()
+    {
+        return corbaTime (System.currentTimeMillis());
+    }
+
+    /**
+     * Converts the given unixTime into a CORBA UtcT.
+     * @param unixTime the number of milliseconds since 1970/01/01 00:00 UTC.
+     */
+    public static UtcT corbaTime (long unixTime)
+    {
+        UtcT result = new UtcT();
+
+        result.time = (unixTime * 10000) + UNIX_OFFSET;
+
+        // unixTime is always UTC.
+        // Therefore, no time zone offset.
+        result.tdf  = 0;
+        
+        // nothing reasonable to put here
+        result.inacchi = 0;
+        result.inacclo = 0;
+        
+        return result;
+    }   
+
+    /**
+     * Converts the given Java date into a CORBA UtcT.
+     */
+    public static UtcT corbaTime (java.util.Date date)
+    {
+        return corbaTime (date.getTime());
+    }
+    
+    /**
+     * Returns a CORBA UtcT that represents an instant that lies
+     * a given number of CORBA time units (100 ns) in the future.
+     */
+    public static UtcT corbaFuture (long corbaUnits)
+    {
+        UtcT result = corbaTime();
+        result.time = result.time + corbaUnits;
+        return result;   
+    }
+    
+    /**
+     * Returns the number of milliseconds between now and the given CORBA 
+     * time.  The value is positive if that time is in the future, and 
+     * negative otherwise.
+     */
+    public static long millisTo (UtcT time)
+    {
+        long unixTime = (time.time - UNIX_OFFSET) / 10000;
+        
+        // if the time is not UTC, correct time zone
+        if (time.tdf != 0)
+            unixTime = unixTime - (time.tdf * 60000);
+        
+        return unixTime - System.currentTimeMillis();
+    }
+
+    /**
+     * Returns true if the instant represented by the given UtcT is
+     * already in the past, false otherwise.  As a special convenience,
+     * this method also returns false if the argument is null.
+     */
+    public static boolean hasPassed (UtcT time)
+    {
+        if (time != null)
+            return millisTo (time) < 0;
+        else
+            return false;
+    }
+
+}
---
+++ test/regression/src/org/jacorb/test/orb/policies/TimingTest.java
@@ -0,0 +1,452 @@
+package org.jacorb.test.orb.policies;
+
+import junit.framework.*;
+import junit.extensions.*;
+
+import org.omg.TimeBase.*;
+import org.omg.CORBA.*;
+import org.jacorb.orb.policies.*;
+
+import org.jacorb.Tests.*;
+import org.jacorb.test.common.*;
+import org.omg.Messaging.*;
+
+import org.jacorb.util.Time;
+
+public class TimingTest extends CallbackTestCase
+{
+    private TimingServer server = null;
+
+    public TimingTest(String name, ClientServerSetup setup)
+    {
+        super(name, setup);
+    }
+
+    protected void setUp() throws Exception
+    {
+        server = TimingServerHelper.narrow (setup.getServerObject());
+    }
+
+    private class ReplyHandler 
+        extends CallbackTestCase.ReplyHandler
+        implements AMI_TimingServerHandlerOperations
+    {
+
+        public void ex_op_excep(ExceptionHolder excep_holder)
+        {
+            wrong_exception ("ex_op_excep", excep_holder);
+        }
+
+        public void ex_op(char ami_return_val)
+        {
+            wrong_reply ("ex_op");
+        }
+
+        public void operation_excep(ExceptionHolder excep_holder)
+        {
+            wrong_exception ("operation_excep", excep_holder);
+        }
+
+        public void operation(int ami_return_val)
+        {
+            wrong_reply ("operation");
+        }
+
+    }
+
+    private AMI_TimingServerHandler ref ( ReplyHandler handler )
+    {
+        AMI_TimingServerHandlerPOATie tie =
+            new AMI_TimingServerHandlerPOATie( handler )
+            {
+                public org.omg.CORBA.portable.OutputStream 
+                    _invoke( String method, 
+                             org.omg.CORBA.portable.InputStream _input, 
+                             org.omg.CORBA.portable.ResponseHandler handler )
+                    throws org.omg.CORBA.SystemException   
+                {
+                    try
+                    {
+                        return super._invoke( method, _input, handler );
+                    }
+                    catch( AssertionFailedError e )
+                    {
+                        return null;
+                    }
+                }
+            };
+        return tie._this( setup.getClientOrb() );
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite ("Timing Policies");
+        ClientServerSetup setup = 
+            new ClientServerSetup 
+                (suite,
+                 "org.jacorb.test.orb.policies.TimingServerImpl");
+                  
+        suite.addTest (new TimingTest ("test_sync_no_timing", setup));
+        suite.addTest (new TimingTest ("test_async_no_timing", setup));
+        suite.addTest (new TimingTest ("test_reply_start_time_sync_expired", setup));
+        suite.addTest (new TimingTest ("test_reply_start_time_sync_wait", setup));
+        suite.addTest (new TimingTest ("test_reply_start_time_async_expired", setup));
+        suite.addTest (new TimingTest ("test_reply_start_time_async_wait", setup));
+
+        suite.addTest (new TimingTest ("test_reply_end_time_sync_pre_expired", setup));
+        //suite.addTest (new TimingTest ("test_reply_end_time_async_pre_expired", setup));
+        
+        suite.addTest (new TimingTest ("test_relative_roundtrip_sync_ok", setup));
+        suite.addTest (new TimingTest ("test_relative_roundtrip_sync_expired", setup));
+        suite.addTest (new TimingTest ("test_relative_roundtrip_async_ok", setup));
+        suite.addTest (new TimingTest ("test_relative_roundtrip_async_expired", setup));
+
+        return setup;
+    }
+    
+    /**
+     * Do a few synchronous invocations as a sanity check
+     * and to get all the necessary classes loaded.
+     */
+    public void test_sync_no_timing()
+    {
+        int result = server.operation (1, 0);
+        assertEquals (1, result);
+        
+        result = server.operation (2, 10);
+        assertEquals (2, result);
+        
+        result = server.operation (3, 100);
+        assertEquals (3, result);
+        
+        try
+        {
+            char ch = server.ex_op('e', 50);
+            fail ("should have raised EmptyException");
+        }
+        catch (EmptyException ex)
+        {
+            // ok
+        }
+
+        try
+        {
+            char ch = server.ex_op('$', 50);
+            fail ("should have raised DATA_CONVERSION");
+        }
+        catch (EmptyException ex)
+        {
+            fail ("unexpected exception: " + ex);   
+        }
+        catch (org.omg.CORBA.DATA_CONVERSION ex)
+        {
+            // ok
+        }
+        
+    }
+
+    /**
+     * Do a few asynchronous invocations as a sanity check
+     * and to get all the necessary classes loaded.
+     */
+    public void test_async_no_timing()
+    {
+        ReplyHandler handler = new ReplyHandler()
+        {
+            public void operation (int ami_return_val)
+            {
+                assertEquals (7, ami_return_val);
+                pass();
+            }
+        };
+        ((_TimingServerStub)server).sendc_operation (ref (handler), 7, 50);
+        handler.wait_for_reply (300);
+        
+        handler = new ReplyHandler()
+        {
+            public void ex_op_excep (ExceptionHolder excep_holder)
+            {
+                assertEquals (EmptyException.class,
+                              getException (excep_holder).getClass());
+                pass();
+            }
+        };
+        ((_TimingServerStub)server).sendc_ex_op (ref (handler), 'e', 50);
+        handler.wait_for_reply (300);
+
+        handler = new ReplyHandler()
+        {
+            public void ex_op_excep (ExceptionHolder excep_holder)
+            {
+                assertEquals (org.omg.CORBA.DATA_CONVERSION.class,
+                              getException (excep_holder).getClass());
+                pass();
+            }
+        };
+        ((_TimingServerStub)server).sendc_ex_op (ref (handler), '$', 50);
+        handler.wait_for_reply (300);
+    }
+
+    /**
+     * Sets a ReplyStartTime which will already have expired
+     * when the reply arrives.
+     */    
+    public void test_reply_start_time_sync_expired()
+    {
+        clearPolicies (server);
+        long start = System.currentTimeMillis();
+        setReplyStartTime (server, start);
+        int result = server.operation (18, 10);
+
+        long delta = System.currentTimeMillis() - start;
+        if (delta > 100)
+            fail ("reply too late (" + delta + "ms)");
+    }
+
+    /**
+     * Sets a ReplyStartTime which will not have been reached
+     * when the reply arrives.
+     */
+    public void test_reply_start_time_sync_wait()
+    {
+        clearPolicies (server);
+        long start = System.currentTimeMillis();
+        setReplyStartTime (server, start + 200);
+        int result = server.operation (18, 100);
+        assertEquals (18, result);
+
+        long delta = System.currentTimeMillis() - start;
+        if (delta < 200)
+            fail ("reply too early (" + delta + "ms)");
+        else if (delta > 250)
+            fail ("reply too late (" + delta + "ms)");
+    }
+
+    /**
+     * Sets a ReplyStartTime which will already have expired
+     * when the reply arrives.
+     */    
+    public void test_reply_start_time_async_expired()
+    {
+        ReplyHandler handler = new ReplyHandler()
+        {
+            public void operation (int ami_return_val)
+            {
+                assertEquals (19, ami_return_val);
+                pass();
+            }
+        };
+        
+        clearPolicies (server);
+        long start = System.currentTimeMillis();
+        setReplyStartTime (server, start);
+        ((_TimingServerStub)server).sendc_operation (ref (handler), 19, 50);
+        handler.wait_for_reply (100);
+        
+        long delta = System.currentTimeMillis() - start;
+        if (delta > 150)
+            fail ("reply too late (" + delta + "ms)");
+    }
+
+    /**
+     * Sets a ReplyStartTime which will not have been reached
+     * when the reply arrives.
+     */
+    public void test_reply_start_time_async_wait()
+    {
+        ReplyHandler handler = new ReplyHandler()
+        {
+            public void operation (int ami_return_val)
+            {
+                assertEquals (19, ami_return_val);
+                pass();
+            }
+        };
+        
+        clearPolicies (server);
+        long start = System.currentTimeMillis();
+        setReplyStartTime (server, start + 200);
+        ((_TimingServerStub)server).sendc_operation (ref (handler), 19, 100);
+        handler.wait_for_reply (250);
+        
+        long delta = System.currentTimeMillis() - start;
+        if (delta < 200)
+            fail ("reply too early (" + delta + "ms)");
+    }
+    
+    /**
+     * Sets a ReplyEndTime which has expired prior to invocation.
+     */
+    public void test_reply_end_time_sync_pre_expired()
+    {
+        clearPolicies (server);
+        setReplyEndTime (server, System.currentTimeMillis() - 100);
+        try
+        {
+            int result = server.operation (44, 100);
+            fail ("should have raised TIMEOUT");
+        }
+        catch (org.omg.CORBA.TIMEOUT t)
+        {
+            // ok
+        }
+    }
+
+    /**
+     * Sets a RelativeRoundtripTimeout which will
+     * be met by the invocation.
+     */
+    public void test_relative_roundtrip_sync_ok()
+    {
+        clearPolicies (server);
+        setRelativeRoundtripTimeout (server, 200);
+        try
+        {
+            int result = server.operation (434, 50);
+        }
+        catch (org.omg.CORBA.TIMEOUT t)
+        {
+            fail ("should not have been a TIMEOUT");
+        }
+    }
+
+    /**
+     * Sets a RelativeRoundtripTimeout which will
+     * expire during invocation.
+     */    
+    public void test_relative_roundtrip_sync_expired()
+    {
+        clearPolicies (server);
+        setRelativeRoundtripTimeout (server, 200);
+        try
+        {
+            int result = server.operation (343, 300);
+            fail ("should have raised TIMEOUT");
+        }
+        catch (org.omg.CORBA.TIMEOUT t)
+        {
+            // ok
+        }
+    }
+
+    /**
+     * Sets a RelativeRoundtripTimeout which will
+     * be met by the invocation.
+     */
+    public void test_relative_roundtrip_async_ok()
+    {
+        ReplyHandler handler = new ReplyHandler()
+        {
+            public void operation (int ami_return_val)
+            {
+                assertEquals (765, ami_return_val);
+                pass();
+            }
+        };
+        
+        clearPolicies (server);
+        setRelativeRoundtripTimeout (server, 200);
+        ((_TimingServerStub)server).sendc_operation (ref (handler), 765, 50);
+        handler.wait_for_reply (150);
+        
+    }    
+
+    /**
+     * Sets a RelativeRoundtripTimeout which will
+     * expire during the invocation.
+     */
+    public void test_relative_roundtrip_async_expired()
+    {
+        ReplyHandler handler = new ReplyHandler()
+        {
+            public void operation (int ami_return_val)
+            {
+                fail ("should have raised TIMEOUT");
+            }
+            
+            public void operation_excep (ExceptionHolder excep_holder)
+            {
+                assertEquals (org.omg.CORBA.TIMEOUT.class,
+                              getException (excep_holder).getClass());
+                pass();
+            }
+        };
+        
+        clearPolicies (server);
+        setRelativeRoundtripTimeout (server, 50);
+        ((_TimingServerStub)server).sendc_operation (ref (handler), 767, 100);
+        handler.wait_for_reply (200);
+    }    
+
+    // convenience methods for policy manipulation
+
+    // These methods create policies in the really cumbersome way
+    // via the ORB, so that the mechanism gets tested.  Each of the
+    // policy types in org.jacorb.orb.policies also has a convenience
+    // constructor that makes it much easier.
+
+    private void clearPolicies (TimingServer server)
+    {
+        server._set_policy_override (new Policy[]{}, SetOverrideType.SET_OVERRIDE);
+    }
+
+    private void setReplyStartTime (TimingServer server, long unixTime)
+    {
+        UtcT corbaTime = Time.corbaTime (unixTime);
+
+        org.omg.CORBA.ORB orb = setup.getClientOrb();
+        org.omg.CORBA.Any a   = orb.create_any();
+        UtcTHelper.insert (a, corbaTime);
+        try
+        {
+            Policy p =
+                orb.create_policy (REPLY_START_TIME_POLICY_TYPE.value, a);
+            server._set_policy_override (new Policy[]{ p }, 
+                                         SetOverrideType.ADD_OVERRIDE);   
+        }
+        catch (PolicyError e)
+        {
+            throw new RuntimeException ("policy error: " + e);
+        }
+    }
+
+    private void setReplyEndTime (TimingServer server, long unixTime)
+    {
+        UtcT corbaTime = Time.corbaTime (unixTime);
+
+        org.omg.CORBA.ORB orb = setup.getClientOrb();
+        org.omg.CORBA.Any a   = orb.create_any();
+        UtcTHelper.insert (a, corbaTime);
+        try
+        {
+            Policy p =
+                orb.create_policy (REPLY_END_TIME_POLICY_TYPE.value, a);
+            server._set_policy_override (new Policy[]{ p }, 
+                                         SetOverrideType.ADD_OVERRIDE);   
+        }
+        catch (PolicyError e)
+        {
+            throw new RuntimeException ("policy error: " + e);
+        }
+    }
+
+    private void setRelativeRoundtripTimeout (TimingServer server, 
+                                              long millis)
+    {
+        org.omg.CORBA.ORB orb = setup.getClientOrb();
+        org.omg.CORBA.Any a   = orb.create_any();
+        a.insert_ulonglong (millis * 10000);
+        try
+        {
+            Policy p =
+                orb.create_policy (RELATIVE_RT_TIMEOUT_POLICY_TYPE.value, a);
+            server._set_policy_override (new Policy[]{ p }, 
+                                         SetOverrideType.ADD_OVERRIDE);   
+        }
+        catch (PolicyError e)
+        {
+            throw new RuntimeException ("policy error: " + e);
+        }
+    }
+
+}
