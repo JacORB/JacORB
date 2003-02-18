f8a7bccade3a5a2dff236b45a15406bff810483a
Andre Speigel <spiegel@drmirror.net>
2003-02-18 09:23:49 +0000

    Initial revision.

---
+++ test/regression/src/org/jacorb/test/orb/policies/SyncScopeServerImpl.java
@@ -0,0 +1,39 @@
+
+package org.jacorb.test.orb.policies;
+
+import org.jacorb.Tests.*;
+
+public class SyncScopeServerImpl extends SyncScopeServerPOA
+{
+
+    public void oneway_op(int delay)
+    {
+        synchronized (this)
+        {
+            try
+            {
+                if (delay > 0) this.wait (delay);
+            }
+            catch (InterruptedException ex)
+            {
+                System.out.println ("wait interrupted");
+            }
+        }
+    }
+
+    public void operation(int delay)
+    {
+        synchronized (this)
+        {
+            try
+            {
+                if (delay > 0) this.wait (delay);
+            }
+            catch (InterruptedException ex)
+            {
+                System.out.println ("wait interrupted");
+            }
+        }
+    }
+
+}
