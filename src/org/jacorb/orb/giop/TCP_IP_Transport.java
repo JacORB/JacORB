ffcd2b861dbdbe7570dc699178c7647068016910
Andre Speigel <spiegel@drmirror.net>
2003-04-27 12:41:35 +0000

    (statistics_provider): Field removed. (getStatisticsProvider): Accessor removed. (c'tors): Remove StatisticsProvider handling. (flush, write): Don't collect statistics, this is now done one layer above.

--- src/org/jacorb/orb/giop/TCP_IP_Transport.java
+++ src/org/jacorb/orb/giop/TCP_IP_Transport.java
@@ -49,9 +49,6 @@ public abstract class TCP_IP_Transport
     String connection_info;
     Socket socket;
 
-    //the statistics provider, may stay null
-    private StatisticsProvider statistics_provider = null;
-
     //used to unregister this transport
     protected TransportManager transport_manager = null;
 
@@ -66,16 +63,13 @@ public abstract class TCP_IP_Transport
         this.b_out = other.b_out;
         this.dump_incoming = other.dump_incoming;
         this.connection_info = other.connection_info;
-        this.statistics_provider = other.statistics_provider;
         this.transport_manager = other.transport_manager;
         this.transport_listener = other.transport_listener;
         this.finalTimeout = other.finalTimeout;
     }
 
-    public TCP_IP_Transport( StatisticsProvider statistics_provider,
-                             TransportManager transport_manager )
+    public TCP_IP_Transport( TransportManager transport_manager )
     {
-        this.statistics_provider = statistics_provider;
         this.transport_manager = transport_manager;
 
         String dump_outgoing =
@@ -183,10 +177,6 @@ public abstract class TCP_IP_Transport
             throw to_COMM_FAILURE (ex);
         }
 
-        if( statistics_provider != null )
-        {
-            statistics_provider.messageChunkSent( length );
-        }
     }
 
 
@@ -207,23 +197,11 @@ public abstract class TCP_IP_Transport
             throw to_COMM_FAILURE (ex);
         }
 
-        if( statistics_provider != null )
-        {
-            statistics_provider.flushed();
-        }
     }
     
     public boolean is_connected()
     {
         return connected;
-    }
-
-    /**
-     * Get the statistics provider for transport usage statistics.
-     */
-    public StatisticsProvider getStatisticsProvider()
-    {
-        return statistics_provider;
     }
 
     public void setTransportListener( TransportListener transport_listener )
