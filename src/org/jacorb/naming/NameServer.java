cedc297745a71c6ccf39de3882e49c7183260b03
Nick Cross <jacorb@goots.org>
2003-06-10 11:30:13 +0000

    Change %2D to - for clarity RT879

--- src/org/jacorb/naming/NameServer.java
+++ src/org/jacorb/naming/NameServer.java
@@ -224,7 +224,7 @@ public class NameServer
              */
 
   	    props.put("jacorb.orb.objectKeyMap.NameService",
-                  "StandardNS/NameServer%2DPOA/_root");
+                  "StandardNS/NameServer-POA/_root");
 
 	    /*
              * set a connection time out : after 30 secs. idle time,
