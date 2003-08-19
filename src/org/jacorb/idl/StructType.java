df1f244e14beaf37bdece34c4e4339102bd3a588
Gerald Brose <mail@geraldbrose.de>
2003-08-19 13:26:56 +0000

    (printStructClass) added code to print a toString() method (bug #336)

--- src/org/jacorb/idl/StructType.java
+++ src/org/jacorb/idl/StructType.java
@@ -534,8 +534,32 @@ class StructType
                 ps.println( d.name() + ";" );
             }
             ps.println( "\t}" );
-
         }
+
+        // generate toStrign method for structs, see bug #336
+
+        ps.println( "\t/** convenience method, not per IDL mapping */" );
+        ps.println( "\tpublic String toString()" );
+        ps.println( "\t{" );
+
+        StringBuffer sb = new StringBuffer("struct " + className + "[ \"");
+
+        if( memberlist != null )
+        {
+            for( Enumeration e = memberlist.v.elements(); e.hasMoreElements(); )
+            {
+                Member m = (Member)e.nextElement();
+                Declarator d = m.declarator;
+                sb.append( " + " + d.name() + " + \" \"" );
+            }
+        }
+
+        sb.append(" + \" ] \"; ");
+
+        ps.println( "\t\treturn \"" + sb.toString() );
+        ps.println( "\t}" );
+
+
         ps.println( "}" );
     }
 
