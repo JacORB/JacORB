5fa34885318224d2f46a86729eb9a9a9eb1e7162
Gerald Brose <mail@geraldbrose.de>
2003-09-03 10:12:09 +0000

    now public

--- src/org/jacorb/idl/MemberList.java
+++ src/org/jacorb/idl/MemberList.java
@@ -28,7 +28,7 @@ package org.jacorb.idl;
 
 import java.util.*;
 
-class MemberList
+public class MemberList
     extends SymbolList
 {
     Vector extendVector = new Vector();
@@ -78,7 +78,8 @@ class MemberList
 
         if( enclosing_symbol != null && enclosing_symbol != s )
         {
-            System.err.println( "was " + enclosing_symbol.getClass().getName() + " now: " + s.getClass().getName() );
+            System.err.println( "was " + enclosing_symbol.getClass().getName() + 
+                                " now: " + s.getClass().getName() );
 
             throw new RuntimeException( "Compiler Error: trying to reassign container" );
         }
