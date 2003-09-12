89a04e32c25f4ecc08aa2435fb8e2137b497831a
Alphonse Bendt <jacorb@prismtech one>
2003-09-12 09:18:11 +0000

    not implemented methods throw NO_IMPLEMENT instead of doing nothing or returning null

--- src/org/jacorb/notification/AbstractProxy.java
+++ src/org/jacorb/notification/AbstractProxy.java
@@ -50,6 +50,7 @@ import org.omg.PortableServer.Servant;
 import org.apache.log.Hierarchy;
 import org.apache.log.Logger;
 import EDU.oswego.cs.dl.util.concurrent.SynchronizedInt;
+import org.omg.CORBA.NO_IMPLEMENT;
 
 /**
  * ProxyBase.java
@@ -172,25 +173,26 @@ public abstract class AbstractProxy implements FilterAdminOperations,
 
     public EventType[] obtain_subscription_types(ObtainInfoMode obtainInfoMode)
     {
-        return null;
+        throw new NO_IMPLEMENT();
     }
 
     public void validate_event_qos(Property[] qosProps,
                                    NamedPropertyRangeSeqHolder propSeqHolder)
         throws UnsupportedQoS
     {
+        throw new NO_IMPLEMENT();
     }
 
     public void validate_qos(Property[] qosProps,
                              NamedPropertyRangeSeqHolder propSeqHolder)
         throws UnsupportedQoS
     {
-        throw new UnsupportedQoS();
+        throw new NO_IMPLEMENT();
     }
 
     public void set_qos(Property[] qosProps) throws UnsupportedQoS
     {
-        throw new UnsupportedQoS();
+        throw new NO_IMPLEMENT();
     }
 
     public Property[] get_qos()
@@ -202,12 +204,14 @@ public abstract class AbstractProxy implements FilterAdminOperations,
                              EventType[] eventTypes2)
         throws InvalidEventType
     {
+        throw new NO_IMPLEMENT();
     }
 
     public void subscription_change(EventType[] eventType,
                                     EventType[] eventType2)
         throws InvalidEventType
     {
+        throw new NO_IMPLEMENT();
     }
 
     public void priority_filter(MappingFilter filter)
@@ -232,7 +236,7 @@ public abstract class AbstractProxy implements FilterAdminOperations,
 
     public EventType[] obtain_offered_types(ObtainInfoMode obtaininfomode)
     {
-        return null;
+        throw new NO_IMPLEMENT();
     }
 
     public Integer getKey()
@@ -348,6 +352,5 @@ public abstract class AbstractProxy implements FilterAdminOperations,
     public int incErrorCounter() {
         return errorCounter_.increment();
     }
-
 
 }
