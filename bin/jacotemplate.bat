2610201444ee4e9d8687cfff95cbcd326b196511
Simon McQueen <sm@prismtech.com>
2003-10-21 09:06:32 +0000

    Use -Xbootclasspath in jaco scripts for JDKS < 1.4. Use -Djava.endorsed.dirs mechanism with JDK 1.4. See ../build.xml.

--- bin/jacotemplate.bat
+++ bin/jacotemplate.bat
@@ -1,3 +1,3 @@
 @echo off
 rem call java interpreter
-java -classpath "@@@;%CLASSPATH%" -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
+java @@@CLASSPATH@@@ -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
