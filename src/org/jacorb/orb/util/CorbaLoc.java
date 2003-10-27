bbc4b85413ed6d0e09c365188b0c0730248645b9
Andre Speigel <spiegel@drmirror.net>
2003-10-27 08:40:46 +0000

    Refactored to integrate corbaloc with ETF. Patch by Phil Mesnier <mesnier_p@ociweb.com> Important changes: (ObjectAddress): Inner class removed. Object addresses are now stored as ETF Profiles. (c'tor): Add ORB parameter.

--- src/org/jacorb/orb/util/CorbaLoc.java
+++ src/org/jacorb/orb/util/CorbaLoc.java
@@ -25,47 +25,93 @@ package org.jacorb.orb.util;
  * @version $Id$
  */
 
-import org.jacorb.orb.ParsedIOR;
+
 import java.io.*;
 import java.util.*;
+import org.jacorb.orb.ORB;
+import org.jacorb.orb.iiop.IIOPProfile;
+import org.omg.ETF.Profile;
 
 public class CorbaLoc
 {
+    private ORB orb;
     private String keyString;
     private byte[] key;
     private String bodyString;
+    private boolean is_rir;
 
-    public static class ObjectAddress
+    public Profile[] profileList;
+
+    public CorbaLoc(ORB o, String addr)
     {
-        /** protocol type information */
-        public String protocol_identifier = "iiop";
-
-        /** protocol version information  */
-        public int major = 1;
-        public int minor = 0;
-
-        /** IP address information */
-        public String host = "127.0.0.1";
-        public int port = 2809; // corbaloc default port number
-
-        public org.omg.IIOP.Version getVersion()
-        {
-            return new org.omg.IIOP.Version((byte)major, (byte)minor);
-        }
-    }
-
-    public ObjectAddress[] objectAddressList;
-
-    public CorbaLoc(String addr)
-    {
+        orb = o;
+        is_rir = false;
         parse(addr);
     }
 
+    public boolean rir()
+    {
+        return is_rir;
+    }
+
+    public String toString()
+    {
+        return "corbaloc:" + body();
+    }
+
+    public String body()
+    {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(bodyString);
+
+        if (keyString != null)
+            sb.append("/" + keyString);
+
+        return sb.toString();
+    }
+
+    public String getKeyString()
+    {
+        return keyString;
+    }
+
+    public byte[] getKey()
+    {
+        return key;
+    }
+
+    public void defaultKeyString(String s)
+    {
+        if( keyString == null )
+            keyString = s;
+        else
+            throw new RuntimeException("KeyString not empty, cannot default to " + s );
+    }
+
+    public String toCorbaName(String str_name)
+    {
+        if (getKeyString() == null)
+            defaultKeyString("NameService");
+
+        if (str_name != null && str_name.length() > 0)
+        {
+            try
+            {
+                return "corbaname:" + body() + "#" + str_name;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        else
+            return "corbaname:" + body();
+    }
 
     /**
      * parses a string representing a corbaloc: reference
      */
-
     private void parse(String addr)
     {
         if( addr == null || !addr.startsWith("corbaloc:"))
@@ -77,6 +123,7 @@ public class CorbaLoc
             sb = new String( addr.substring( addr.indexOf(':')+1 ) );
             if (addr.startsWith("corbaloc:rir:"))
             {
+                is_rir = true;
                 // default key string for rir protocol
                 keyString = "NameService";
             }
@@ -96,84 +143,63 @@ public class CorbaLoc
         if( sb.indexOf(',') > 0 )
         {
             StringTokenizer tokenizer = new StringTokenizer( sb, "," );
-            objectAddressList = new ObjectAddress[tokenizer.countTokens()];
+            profileList = new Profile[tokenizer.countTokens()];
+            int pIndex = 0;
+            for( int i = 0; i < profileList.length; i++ )
+            {
+                Profile p = parseAddress(tokenizer.nextToken());
+                if (p == null)
+                    continue;
+                profileList[pIndex] = p;
+                pIndex++;
+            }
+            while (pIndex < profileList.length)
+            {
+                profileList[pIndex] = null;
+                pIndex++;
+            }
 
-            for( int i = 0; i < objectAddressList.length; i++ )
-                objectAddressList[i] = parseObjectAddress(tokenizer.nextToken());
         }
         else
-            objectAddressList = new ObjectAddress[]{ parseObjectAddress(sb) };
+            profileList = new Profile[]{ parseAddress(sb) };
 
         bodyString = sb;
     }
 
-    private ObjectAddress parseObjectAddress(String addr)
+    private Profile parseAddress(String addr)
     {
-        if( addr.indexOf(':') == -1)
-            throw new IllegalArgumentException("Illegal object address format: " + addr);
-
-        ObjectAddress result = new ObjectAddress();
-
-        if( addr.equals("rir:"))
+        int colon = addr.indexOf(':');
+        if (colon == -1)
+            throw new IllegalArgumentException(
+                "Illegal object address format: " + addr);
+        if (addr.equals("rir:"))
         {
+            is_rir = true;
             /* resolve initials references protocol */
-            result.protocol_identifier = "rir";
-            return result;
+            return null;
         }
-        else if ( addr.indexOf(':') == 0 ||
-                  addr.startsWith("iiop:") ||
-                  addr.startsWith("ssliop:"))
+
+        Profile result = null;
+        if (orb == null 
+            && (colon == 0
+                || addr.startsWith("iiop:")
+                || addr.startsWith("ssliop:")))
+            result = new IIOPProfile(addr);
+        else if (orb != null)
         {
-            if( addr.indexOf(':') != 0 )
+            String token = addr.substring(0, colon);
+            List factories = orb.getTransportManager().getFactoriesList();
+            for (Iterator i = factories.iterator();
+                 result == null && i.hasNext();)
             {
-                //protocol exclicitely specified
-                result.protocol_identifier = addr.substring( 0, addr.indexOf(':') );
+                org.omg.ETF.Factories f = (org.omg.ETF.Factories)i.next();
+                result = f.decode_corbaloc(addr);
             }
-            //else: use default "iiop"
-
-            String version_and_host = addr.substring( addr.indexOf(':')+1);
-            if( version_and_host.length() == 0)
-                throw new IllegalArgumentException("Illegal IIOP protocol format in object address format: " + addr);
-
-            /* IIOP */
-            String host;
-            if( version_and_host.indexOf( '@' ) != -1 )
-            {
-                String version = version_and_host.substring(0, version_and_host.indexOf( '@' ));
-                if( version.indexOf('.') != -1 )
-                {
-                    try
-                    {
-                        result.major = Integer.parseInt(version.substring(0,version.indexOf('.')));
-                        result.minor = Integer.parseInt(version.substring(version.indexOf('.')+1));
-                    }
-                    catch( NumberFormatException nfe )
-                    {
-                        throw new IllegalArgumentException("Illegal version format for IIOP protocol in object address format: " + addr);
-                    }
-                }
-                host = version_and_host.substring( version_and_host.indexOf( '@' )+1);
-            }
-            else
-                host = version_and_host;
-            if( host.indexOf(':') != -1 )
-            {
-                try
-                {
-                    result.host = host.substring(0, host.indexOf(':'));
-                    result.port = Integer.parseInt( host.substring( host.indexOf(':')+1));
-                }
-                catch( NumberFormatException ill )
-                {
-                    throw new IllegalArgumentException("Illegal port number in IIOP object address format: " + addr);
-                }
-            }
-            else
-                result.host = host;
-            return result;
         }
-        else
-            throw new IllegalArgumentException("Illegal protocol in object address format: " + addr);
+        if (result == null)
+            throw new IllegalArgumentException(
+                "Unknown protocol in object address format: " + addr);
+        return result;
     }
 
     private static boolean legalChar(char c)
@@ -274,74 +300,14 @@ public class CorbaLoc
         return sb.toString();
     }
 
-
-    public String toString()
-    {
-        return "corbaloc:" + body();
-    }
-
-    public String body()
-    {
-        StringBuffer sb = new StringBuffer();
-
-        sb.append(bodyString);
-
-        if( keyString != null )
-            sb.append("/" + keyString );
-
-        return sb.toString();
-    }
-
-
-    public String getKeyString()
-    {
-        return keyString;
-    }
-
-    public byte[] getKey()
-    {
-        return key;
-    }
-
-    public void defaultKeyString(String s)
-    {
-        if( keyString == null )
-            keyString = s;
-        else
-            throw new RuntimeException("KeyString not empty, cannot default to " + s );
-    }
-
-
-    public String toCorbaName(String str_name)
-    {
-        if( getKeyString() == null )
-            defaultKeyString("NameService");
-
-        if( str_name != null && str_name.length() > 0 )
-        {
-            try
-            {
-                return "corbaname:" +
-                    body() + "#" + str_name;
-            }
-            catch( Exception e)
-            {
-                return null;
-            }
-        }
-        else
-            return "corbaname:" + body();
-
-    }
-
     public static void main(String[] args)
     {
+        String [] noarg = new String[]{};
+        ORB orb = (org.jacorb.orb.ORB)ORB.init(noarg,null);
         for( int i = 0; i < args.length; i++ )
         {
-            System.out.println( new CorbaLoc( args[i] ).toString());
+            System.out.println( new CorbaLoc(orb, args[i] ).toString());
         }
-
     }
-
 
 }
