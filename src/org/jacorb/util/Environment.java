a37fd1691607aee8dead1895019dea01bb85b9fb
Francisco Reverbel <reverbel@gmail.com>
2003-11-06 19:32:27 +0000

    Javadoc comments and implementation comments added to classForName().

--- src/org/jacorb/util/Environment.java
+++ src/org/jacorb/util/Environment.java
@@ -1077,8 +1077,47 @@ public class Environment
     }
 
     /**
-     * quick hack to avoid calling <code>Class.forName()</code>
-     * (needs refinement)
+     * Returns the <code>Class</code> object for the class or interface 
+     * with the given string name. This method is a replacement for 
+     * <code>Class.forName(String name)</code>. Unlike 
+     * <code>Class.forName(String name)</code> (which always uses the 
+     * caller's loader or one of its ancestors), <code>classForName</code>
+     * uses a thread-specific loader that has no delegation relationship
+     * with the caller's loader. It attempts the load the desired class 
+     * with the thread-specific context class loader and falls back to 
+     * <code>Class.forName(String name)</code> only if the context class 
+     * loader cannot load the class.  
+     * <p>
+     * Loading a class with a loader that is not necessarily an ancestor
+     * of the caller's loader is a crucial thing in many scenarios. As an
+     * example, assume that JacORB was loaded by the boot class loader,
+     * and suppose that some code in JacORB contains a call
+     * <code>Class.forName(someUserClass)</code>. Such usage of 
+     * <code>Class.forName</code> effectively forces the user to place 
+     * <code>someUserClass</code> in the boot class path. If 
+     * <code>classForName(someUserClass)</code> were used instead, the user
+     * class would be loaded by the context class loader, which by default 
+     * is set to the system (CLASSPATH) classloader. 
+     * <p>
+     * In this simple example above, the default setting of the context class
+     * loader allows classes in the boot classpath to reach classes in the 
+     * system classpath. In other scenarios, the context class loader might 
+     * be different from the system classloader. Middleware systems like 
+     * servlet containers or EJB containers set the context class loader so 
+     * that a given thread can reach user-provided classes that are not in 
+     * the system classpath.
+     * <p>
+     * For maximum flexibility, <code>classForName</code> should replace
+     * <code>Class.forName(String name)</code> in nearly all cases.
+     * 
+     * @param name the fully qualified name of a class
+     *
+     * @return the Class object for that class
+     *
+     * @throws IllegalArgumentException if <code>name</code> is null
+     * @throws ClassNotFoundException if the named class cannot be found
+     * @throws LinkageError if the linkage fails
+     * @throws ExceptionInInitializerError if the class initialization fails
      */
 
     public static Class classForName(String name) 
@@ -1088,10 +1127,18 @@ public class Environment
             throw new IllegalArgumentException("Class name must not be null!");
         try 
         {
+            // Here we prefer classLoader.loadClass() over the three-argument 
+            // form of Class.forName(), as the latter is reported to cause 
+            // caching of stale Class instances (due to a buggy cache of 
+            // loaded classes).
             return Thread.currentThread().getContextClassLoader().loadClass(name);
         }
         catch (Exception e) 
         {
+            // As a fallback, we prefer Class.forName(name) because it loads 
+            // array classes (i.e., it handles arguments like 
+            // "[Lsome.class.Name;" or "[[I;", which classLoader.loadClass()
+            // does not handle).
             return Class.forName(name);
         }
     }
