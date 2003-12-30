8320e3c9c1771fb48a3a208994d005cf43030f93
Andre Speigel <spiegel@drmirror.net>
2003-12-30 15:21:53 +0000

    (all_policies, childs, createdReferences): Use HashMaps instead of Hashtables.

--- src/org/jacorb/poa/POA.java
+++ src/org/jacorb/poa/POA.java
@@ -69,7 +69,7 @@ public class POA
     private String                qualifiedName;
 
     // name -> child POA's
-    private Hashtable             childs = new Hashtable();
+    private Map                   childs = new HashMap();
 
     Servant                       defaultServant;
     ServantManager                servantManager;
@@ -110,10 +110,10 @@ public class POA
     // default: NORMAL
     protected BidirectionalPolicy       bidirectionalPolicy;
 
-    private Hashtable all_policies = null;
+    private Map all_policies = null;
 
     /** key: , value: CORBA.Object */
-    private Hashtable createdReferences;
+    private Map createdReferences;
 
     // stores the etherealize_objects value from the first call of destroy
     private boolean               etherealize;
@@ -141,8 +141,8 @@ public class POA
         poaManager = _poaManager;
         logPrefix = "POA " + name;
 
-        all_policies = new Hashtable();
-        createdReferences = new Hashtable();
+        all_policies = new HashMap();
+        createdReferences = new HashMap();
 
         if (policies != null)
         {
@@ -811,10 +811,10 @@ public class POA
         /* child poa creations are impossible now */
         // destroy all childs first
 
-        Enumeration en = childs.elements();
-        while (en.hasMoreElements())
+        Iterator en = childs.values().iterator();
+        while (en.hasNext())
         {
-            POA child = (POA) en.nextElement();
+            POA child = (POA) en.next();
             child.destroy(etherealize, wait_for_completion);
         }
 
@@ -1714,16 +1714,16 @@ public class POA
         checkDestructionApparent();
 
         int i = 0;
-        Enumeration enum;
+        Iterator enum;
         org.omg.PortableServer.POA[] children;
 
         synchronized (poaCreationLog)
         {
             children = new org.omg.PortableServer.POA[childs.size()];
-            enum = childs.elements();
-            while (enum.hasMoreElements())
+            enum = childs.values().iterator();
+            while (enum.hasNext())
             {
-                children[i] = (org.omg.PortableServer.POA) enum.nextElement();
+                children[i] = (org.omg.PortableServer.POA) enum.next();
                 i++;
             }
         }
