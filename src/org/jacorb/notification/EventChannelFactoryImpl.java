37dbe60d24ac9b9513c8581a919abb0b5ba2842a
Alphonse Bendt <jacorb@prismtech one>
2004-01-16 22:29:51 +0000

    commented out references to not yet checked in classes

--- src/org/jacorb/notification/EventChannelFactoryImpl.java
+++ src/org/jacorb/notification/EventChannelFactoryImpl.java
@@ -307,9 +307,9 @@ public class EventChannelFactoryImpl
         {
             logger_.fatalError( "create_channel()", e );
         }
-        catch (PersistenceException e) {
-            logger_.fatalError("error creating persistent channel", e);
-        }
+//         catch (PersistenceException e) {
+//             logger_.fatalError("error creating persistent channel", e);
+//         }
 
         throw new RuntimeException();
     }
@@ -334,8 +334,8 @@ public class EventChannelFactoryImpl
                UnsupportedQoS,
                ObjectNotActive,
                WrongPolicy,
-               ServantAlreadyActive,
-               PersistenceException
+               ServantAlreadyActive
+               //               PersistenceException
     {
         if (logger_.isDebugEnabled() ) {
             logger_.debug( "create_channel_servant " + key );
@@ -414,20 +414,20 @@ public class EventChannelFactoryImpl
                                   _uniqueQoSProperties,
                                   _uniqueAdminProperties );
 
-        if (_connectionReliability == Persistent.value) {
-            makePersistent(_channelServant);
-        }
+//         if (_connectionReliability == Persistent.value) {
+//             makePersistent(_channelServant);
+//         }
 
         return _channelServant;
     }
 
-    private void makePersistent(EventChannelImpl channel) throws PersistenceException {
-        logger_.info("The creation of a persistent Channel has been requested");
+//     private void makePersistent(EventChannelImpl channel) throws PersistenceException {
+//         logger_.info("The creation of a persistent Channel has been requested");
 
-        if (eventChannelStore_ != null) {
-            eventChannelStore_.storePersistentChannel(channel);
-        }
-    }
+//         if (eventChannelStore_ != null) {
+//             eventChannelStore_.storePersistentChannel(channel);
+//         }
+//     }
 
     void removeEventChannelServant( int id )
     {
