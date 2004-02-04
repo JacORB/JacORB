95e5ea416d0d41039d87249280c9a1aa3df7efc8
David R Robison <drrobison@openroadsconsulting.com>
2004-02-04 21:26:42 +0000

    Interoperability Tests with Adiron

--- test/sas/README.txt
+++ test/sas/README.txt
@@ -3,9 +3,9 @@ The following tests are available
 AdironTest IIOP Basic			Working
 AdironTest TLS Basic			Working
 AdironTest IIOP_CSIv2 UP		NO_PERMISSION
-AdironTest TLS_CSIv2 UP			Not Done
-AdironTest IIOP_CSIv2_UP UP		NO_PERMISSION
-AdironTest TLS_CSIv2_UP UP		Not Done
-AdironTest ALL UP				Not Done
+AdironTest TLS_CSIv2 UP			
+AdironTest IIOP_CSIv2_UP UP		Working
+AdironTest TLS_CSIv2_UP UP		
+AdironTest ALL UP				
 
 
---
+++ test/sas/src/org/jacorb/test/sas/UP_Client.java
@@ -0,0 +1,53 @@
+package org.jacorb.test.sas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.jacorb.security.sas.GssUpContext;
+import org.omg.CORBA.ORB;
+
+import InterOpTest.Johnson;
+import InterOpTest.JohnsonHelper;
+
+/**
+ * This is the client side of the sas demo. It just calls the single
+ * operation "printCert()" of the server. As you can see, sas is fully
+ * transparent.
+ *
+ * @author Nicolas Noffke
+ * @version $Id$
+ */
+
+public class UP_Client {
+	public static void main(String args[]) {
+		if (args.length != 3) {
+			System.out.println("Usage: <ior_file> <username> <password>");
+			System.exit(1);
+		}
+
+		try {
+			// set security credentials
+			GssUpContext.setUsernamePassword(args[1], args[2]);
+
+			// initialize the ORB.
+			ORB orb = ORB.init(args, null);
+
+			// get the server
+			URL iorURL = new URL(args[0]);
+			System.out.println("Reading object from ior: "+args[0]);
+			BufferedReader br = new BufferedReader(new InputStreamReader(iorURL.openStream()));//new FileReader(f));
+			org.omg.CORBA.Object obj = orb.string_to_object(br.readLine());
+			br.close();
+			Johnson johnson = JohnsonHelper.narrow(obj);
+			
+			// call
+			String hello = johnson.say_hello("hello from JacORB");
+			System.out.println(hello);
+
+			System.out.println("Call to server succeeded");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
