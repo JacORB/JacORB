debfd4d4af4cdbe6919770d2e6bc54f0682375e2
Nick Cross <jacorb@goots.org>
2004-02-06 16:55:17 +0000

    Logger name match other interceptor logger

--- src/org/jacorb/orb/portableInterceptor/ClientRequestInfoImpl.java
+++ src/org/jacorb/orb/portableInterceptor/ClientRequestInfoImpl.java
@@ -42,8 +42,8 @@ import org.jacorb.util.Debug;
  */
 
 public class ClientRequestInfoImpl
-    extends RequestInfoImpl 
-    implements ClientRequestInfo  
+    extends RequestInfoImpl
+    implements ClientRequestInfo
 {
     //from ClientRequestInfo
     public org.omg.CORBA.Object target = null;
@@ -60,7 +60,7 @@ public class ClientRequestInfoImpl
 
     public org.jacorb.orb.giop.ClientConnection connection = null;
 
-    public ClientRequestInfoImpl() 
+    public ClientRequestInfoImpl()
     {
         super();
     }
@@ -92,7 +92,7 @@ public class ClientRequestInfoImpl
 
          IIOPProfile profile = (IIOPProfile)pior.getEffectiveProfile();
          this.effective_profile    = profile.asTaggedProfile();
-         this.effective_components = profile.getComponents().asArray(); 
+         this.effective_components = profile.getComponents().asArray();
 
          if ( this.effective_components == null )
          {
@@ -110,18 +110,18 @@ public class ClientRequestInfoImpl
          this.request_os = ros;
 
          //allow (BiDir) interceptor to inspect the connection
-         this.connection = connection;  
+         this.connection = connection;
     }
 
     public void setRequest(org.jacorb.orb.dii.Request request)
-    {    
+    {
         arguments = new org.omg.Dynamic.Parameter[request.arguments.count()];
         for (int i = 0; i < arguments.length; i++)
         {
             try
             {
                 NamedValue value = request.arguments.item(i);
-        
+
                 ParameterMode mode = null;
                 if (value.flags() == ARG_IN.value)
                     mode = ParameterMode.PARAM_IN;
@@ -129,12 +129,12 @@ public class ClientRequestInfoImpl
                     mode = ParameterMode.PARAM_OUT;
                 else if (value.flags() == ARG_INOUT.value)
                     mode = ParameterMode.PARAM_INOUT;
-        
+
                 arguments[i] = new org.omg.Dynamic.Parameter(value.value(), mode);
             }
             catch (Exception e)
             {
-                Logger logger = Debug.getNamedLogger("pi");
+                Logger logger = Debug.getNamedLogger("jacorb.orb.interceptors");
                 if( logger.isDebugEnabled() )
                 {
                     logger.debug(e.getMessage());
@@ -152,62 +152,62 @@ public class ClientRequestInfoImpl
 
     public Enumeration getRequestServiceContexts()
     {
-        return request_ctx.elements(); 
+        return request_ctx.elements();
     }
 
     // implementation                        of
     // org.omg.PortableInterceptor.RequestInfoOperations interface
 
-    public Parameter[] arguments() 
+    public Parameter[] arguments()
     {
         if (! (caller_op == ClientInterceptorIterator.SEND_REQUEST) &&
             ! (caller_op == ClientInterceptorIterator.RECEIVE_REPLY))
-            throw new BAD_INV_ORDER("The attribute \"arguments\" is currently invalid!", 
+            throw new BAD_INV_ORDER("The attribute \"arguments\" is currently invalid!",
                                     10, CompletionStatus.COMPLETED_MAYBE);
 
         if (arguments == null)
-            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op", 
+            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op",
                                    1, CompletionStatus.COMPLETED_MAYBE);
         else
             return arguments;
     }
 
-    public TypeCode[] exceptions() 
+    public TypeCode[] exceptions()
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The attribute \"exceptions\" is currently invalid!", 
+            throw new BAD_INV_ORDER("The attribute \"exceptions\" is currently invalid!",
                                     10, CompletionStatus.COMPLETED_MAYBE);
 
         if (exceptions == null)
-            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op", 
+            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op",
                                    1, CompletionStatus.COMPLETED_MAYBE);
         else
             return exceptions;
     }
 
-    public Any result() 
+    public Any result()
     {
         if (caller_op != ClientInterceptorIterator.RECEIVE_REPLY)
-            throw new BAD_INV_ORDER("The attribute \"result\" is currently invalid!", 
+            throw new BAD_INV_ORDER("The attribute \"result\" is currently invalid!",
                                     10, CompletionStatus.COMPLETED_MAYBE);
 
         if (result == null)
-            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op", 
+            throw new NO_RESOURCES("Stream-based skeletons/stubs do not support this op",
                                    1, CompletionStatus.COMPLETED_MAYBE);
         else
             return result;
     }
 
-    public short sync_scope() 
+    public short sync_scope()
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The attribute \"sync_scope\" is currently invalid!", 
+            throw new BAD_INV_ORDER("The attribute \"sync_scope\" is currently invalid!",
                                     10, CompletionStatus.COMPLETED_MAYBE);
 
         return org.omg.Messaging.SYNC_WITH_TRANSPORT.value;
     }
- 
-    public short reply_status() 
+
+    public short reply_status()
     {
         if ((caller_op == ClientInterceptorIterator.SEND_REQUEST) ||
             (caller_op == ClientInterceptorIterator.SEND_POLL))
@@ -217,26 +217,26 @@ public class ClientRequestInfoImpl
         return reply_status;
     }
 
-    public org.omg.CORBA.Object forward_reference() 
+    public org.omg.CORBA.Object forward_reference()
     {
         if( (caller_op != ClientInterceptorIterator.RECEIVE_OTHER) ||
             (reply_status != LOCATION_FORWARD.value) )
-            throw new BAD_INV_ORDER("The attribute \"forward_reference\" is currently " + 
+            throw new BAD_INV_ORDER("The attribute \"forward_reference\" is currently " +
                                     "invalid!", 10, CompletionStatus.COMPLETED_MAYBE);
 
         return forward_reference;
     }
 
-    public ServiceContext get_request_service_context(int id) 
+    public ServiceContext get_request_service_context(int id)
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The attribute \"operation_context\" is currently " + 
+            throw new BAD_INV_ORDER("The attribute \"operation_context\" is currently " +
                                     "invalid!", 10, CompletionStatus.COMPLETED_MAYBE);
 
         return super.get_request_service_context(id);
     }
 
-    public ServiceContext get_reply_service_context(int id) 
+    public ServiceContext get_reply_service_context(int id)
     {
         if ((caller_op == ClientInterceptorIterator.SEND_REQUEST) ||
             (caller_op == ClientInterceptorIterator.SEND_POLL))
@@ -245,14 +245,14 @@ public class ClientRequestInfoImpl
 
         return super.get_reply_service_context(id);
     }
-  
+
     // implementation of ClientRequestInfoOperations interface
-    public org.omg.CORBA.Object target() 
+    public org.omg.CORBA.Object target()
     {
         return target;
     }
-  
-    public org.omg.CORBA.Object effective_target() 
+
+    public org.omg.CORBA.Object effective_target()
     {
         return effective_target;
     }
@@ -260,46 +260,46 @@ public class ClientRequestInfoImpl
     public TaggedProfile effective_profile() {
         return effective_profile;
     }
-  
-    public Any received_exception() 
+
+    public Any received_exception()
     {
         if (caller_op != ClientInterceptorIterator.RECEIVE_EXCEPTION)
-            throw new BAD_INV_ORDER("The attribute \"received_exception\" is currently " + 
+            throw new BAD_INV_ORDER("The attribute \"received_exception\" is currently " +
                                     "invalid!", 10, CompletionStatus.COMPLETED_MAYBE);
 
         return received_exception;
     }
 
-    public String received_exception_id() 
+    public String received_exception_id()
     {
         if (caller_op != ClientInterceptorIterator.RECEIVE_EXCEPTION)
-            throw new BAD_INV_ORDER("The attribute \"received_exception_id\" is " + 
-                                    "currently invalid!", 10, 
+            throw new BAD_INV_ORDER("The attribute \"received_exception_id\" is " +
+                                    "currently invalid!", 10,
                                     CompletionStatus.COMPLETED_MAYBE);
 
         return received_exception_id;
     }
-   
-    public TaggedComponent get_effective_component(int id) 
+
+    public TaggedComponent get_effective_component(int id)
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The operation \"get_effective_component\" is " + 
-                                    "currently invalid!", 10, 
+            throw new BAD_INV_ORDER("The operation \"get_effective_component\" is " +
+                                    "currently invalid!", 10,
                                     CompletionStatus.COMPLETED_MAYBE);
 
         for(int _i = 0; _i < effective_components.length; _i++)
             if (effective_components[_i].tag == id)
                 return effective_components[_i];
-    
-        throw new BAD_PARAM("No TaggedComponent with id " + id + " found", 
-                            25, CompletionStatus.COMPLETED_MAYBE); 
+
+        throw new BAD_PARAM("No TaggedComponent with id " + id + " found",
+                            25, CompletionStatus.COMPLETED_MAYBE);
     }
-  
-    public TaggedComponent[] get_effective_components(int id) 
+
+    public TaggedComponent[] get_effective_components(int id)
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The operation \"get_effective_components\" is " + 
-                                    "currently invalid!", 10, 
+            throw new BAD_INV_ORDER("The operation \"get_effective_components\" is " +
+                                    "currently invalid!", 10,
                                     CompletionStatus.COMPLETED_MAYBE);
 
         Vector _store = new Vector();
@@ -308,7 +308,7 @@ public class ClientRequestInfoImpl
                 _store.addElement(effective_components[_i]);
 
         if (_store.size() == 0)
-            throw new BAD_PARAM("No TaggedComponents with id " + id + " found", 
+            throw new BAD_PARAM("No TaggedComponents with id " + id + " found",
                                 25, CompletionStatus.COMPLETED_MAYBE);
         else
         {
@@ -319,21 +319,21 @@ public class ClientRequestInfoImpl
             return _result;
         }
     }
-  
+
     /**
      * WARNING: This method relies on the DomainService to be available.
      * Make shure that the DS is running, if you want to call this method.
      */
 
-    public Policy get_request_policy(int type) 
+    public Policy get_request_policy(int type)
     {
         if (caller_op == ClientInterceptorIterator.SEND_POLL)
-            throw new BAD_INV_ORDER("The operation \"get_request_policy\" is currently " + 
+            throw new BAD_INV_ORDER("The operation \"get_request_policy\" is currently " +
                                     "invalid!", 10, CompletionStatus.COMPLETED_MAYBE);
 
         if (! orb.hasPolicyFactoryForType(type))
-            throw new INV_POLICY("No PolicyFactory for type " + type + 
-                                 " has been registered!", 1, 
+            throw new INV_POLICY("No PolicyFactory for type " + type +
+                                 " has been registered!", 1,
                                  CompletionStatus.COMPLETED_MAYBE);
         try
         {
@@ -345,21 +345,21 @@ public class ClientRequestInfoImpl
             throw _e;
         }
     }
- 
-    public void add_request_service_context(ServiceContext service_context, 
-                                            boolean replace) 
+
+    public void add_request_service_context(ServiceContext service_context,
+                                            boolean replace)
     {
 
         if (caller_op != ClientInterceptorIterator.SEND_REQUEST)
-            throw new BAD_INV_ORDER("The operation \"add_request_service_context\" is " + 
-                                    "currently invalid!", 10, 
+            throw new BAD_INV_ORDER("The operation \"add_request_service_context\" is " +
+                                    "currently invalid!", 10,
                                     CompletionStatus.COMPLETED_MAYBE);
 
         Integer _id = new Integer(service_context.context_id);
 
         if (! replace && request_ctx.containsKey(_id))
-            throw new BAD_INV_ORDER("The ServiceContext with id " + _id.toString() 
-                                    + " has already been set!", 11, 
+            throw new BAD_INV_ORDER("The ServiceContext with id " + _id.toString()
+                                    + " has already been set!", 11,
                                     CompletionStatus.COMPLETED_MAYBE);
 
         request_ctx.put(_id, service_context);
