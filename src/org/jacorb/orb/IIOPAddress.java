4ecc0a30eca6b5415d5f729e1fdf3576338e6ab9
Gerald Brose <mail@geraldbrose.de>
2004-02-09 07:56:56 +0000

    cosmetics (coding style)

--- src/org/jacorb/orb/IIOPAddress.java
+++ src/org/jacorb/orb/IIOPAddress.java
@@ -43,9 +43,9 @@ public class IIOPAddress
      *     converted by adding 65536 to it; this helps using values that were
      *     previously stored in a Java <code>short</code>. 
      */
-    public IIOPAddress (String host, int port)
+    public IIOPAddress(String host, int port)
     {
-        if (isIP (host))
+        if (isIP(host))
             this.ip = host;
         else
             this.hostname = host;
@@ -56,36 +56,41 @@ public class IIOPAddress
             this.port = port;
     }
     
-    public static IIOPAddress read (org.omg.CORBA.portable.InputStream in)
+    public static IIOPAddress read(org.omg.CORBA.portable.InputStream in)
     {
         String host = in.read_string();
         short  port = in.read_ushort();
-        return new IIOPAddress (host, port);
+        return new IIOPAddress(host, port);
     }
     
     /**
      * Returns true if host is a numeric IP address.
      */
-    private static boolean isIP (String host)
+    private static boolean isIP(String host)
     {
         int index       = 0;
         int numberStart = 0;
         int length      = host.length();
         char ch = ' ';
         
-        for (int i=0; i<4; i++)
+        for (int i = 0; i < 4; i++)
         {
             while (true)
             {
-                if (index >= length) break;
+                if (index >= length) 
+                    break;
                 ch = host.charAt(index);
-                if (ch == '.') break;
-                if (ch < '0' || ch > '9') return false;
+                if (ch == '.') 
+                    break;
+                if (ch < '0' || ch > '9') 
+                    return false;
                 index++;
             }
             if (index >= length && i == 3 
                 && (index - numberStart) <= 3 && (index-numberStart) > 0)
+            {
                 return true;
+            }
             else if (ch == '.' && (index - numberStart) <= 3
                                && (index - numberStart) > 0)
             {
@@ -114,7 +119,7 @@ public class IIOPAddress
             }
             catch (UnknownHostException ex)
             {
-                throw new RuntimeException ("could not resolve hostname: " 
+                throw new RuntimeException("could not resolve hostname: " 
                                             + hostname);
             }  
         }
@@ -131,8 +136,9 @@ public class IIOPAddress
     {
         if (hostname == null)
         {
-            hostname = DNSLookup.inverseLookup (ip);
-            if (hostname == null) hostname = ip;
+            hostname = DNSLookup.inverseLookup(ip);
+            if (hostname == null) 
+                hostname = ip;
         }
         return hostname;      
     }
@@ -146,7 +152,7 @@ public class IIOPAddress
         return port;
     }
     
-    public boolean equals (Object other)
+    public boolean equals(Object other)
     {
         if (other instanceof IIOPAddress)
         {
@@ -177,8 +183,8 @@ public class IIOPAddress
     {
     	CDROutputStream out = new CDROutputStream();
     	out.beginEncapsulatedArray();
-    	out.write_string (ip);
-    	out.write_ushort ((short)port);
+    	out.write_string(ip);
+    	out.write_ushort((short)port);
     	return out.getBufferCopy();
     }
 }
