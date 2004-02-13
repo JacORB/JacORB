5d9f297a0aece143c9e0f0d6e306a55325736ff8
Alphonse Bendt <jacorb@prismtech one>
2004-02-13 18:36:31 +0000

    new testcase. proxysupplier pushes an event to an consumer and the consumer throws Notconnected. the consumer should be disconnected

--- test/regression/src/org/jacorb/test/notification/engine/PushToConsumerTest.java
+++ test/regression/src/org/jacorb/test/notification/engine/PushToConsumerTest.java
@@ -21,39 +21,27 @@ package org.jacorb.test.notification.engine;
  *
  */
 
+import org.jacorb.notification.conf.Configuration;
+import org.jacorb.notification.conf.Default;
 import org.jacorb.notification.engine.PushToConsumerTask;
 import org.jacorb.notification.engine.TaskProcessor;
-import org.jacorb.notification.interfaces.MessageConsumer;
 import org.jacorb.notification.interfaces.Message;
-import org.jacorb.notification.queue.BoundedPriorityEventQueue;
-import org.jacorb.notification.queue.EventQueue;
-import org.jacorb.notification.queue.EventQueueOverflowStrategy;
 import org.jacorb.test.notification.MockMessage;
+import org.jacorb.util.Environment;
 
 import org.omg.CORBA.Any;
 import org.omg.CORBA.OBJECT_NOT_EXIST;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.TRANSIENT;
+import org.omg.CosEventComm.Disconnected;
 
-import java.util.Iterator;
-import java.util.Vector;
 
 import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import EDU.oswego.cs.dl.util.concurrent.SynchronizedInt;
-import org.jacorb.notification.conf.Configuration;
-import org.jacorb.notification.conf.Default;
-import org.jacorb.util.Environment;
-
 /**
- *  Unit Test for class PushToConsumer
- *
- *
- * Created: Mon Aug 18 16:11:37 2003
- *
  * @author Alphonse Bendt
  * @version $Id$
  */
@@ -62,19 +50,23 @@ public class PushToConsumerTest extends TestCase
 {
 
     TaskProcessor taskProcessor_;
+
     ORB orb = ORB.init();
 
-    public void setUp() throws Exception {
-
+    public void setUp() throws Exception
+    {
         taskProcessor_ = new TaskProcessor();
-
     }
 
-    public void tearDown() throws Exception {
+
+    public void tearDown() throws Exception
+    {
         taskProcessor_.dispose();
     }
 
-    public void testRepeatedDeliveryErrorsCauseTheConsumerToBeDisconnected() throws Exception {
+
+    public void testRepeatedDeliveryErrorsCauseTheConsumerToBeDisconnected() throws Exception
+    {
         MockMessage msg = new MockMessage();
 
         Any any = orb.create_any();
@@ -87,38 +79,47 @@ public class PushToConsumerTest extends TestCase
 
         task.setMessage(msg.getHandle());
 
-        MockEventConsumer eventConsumer = new MockEventConsumer() {
+        MockEventConsumer eventConsumer =
+            new MockEventConsumer()
+            {
                 int counter = 0;
                 boolean enabled = true;
 
-                public boolean hasPendingMessages() {
+                public boolean hasPendingMessages()
+                {
                     return true;
                 }
 
-                public void deliverPendingMessages() {
+                public void deliverPendingMessages()
+                {
                     throw new TRANSIENT();
                 }
 
-                public void deliverMessage(Message event) {
+                public void deliverMessage(Message event)
+                {
                     counter++;
-                    if (enabled) {
-                        throw new TRANSIENT();
-                    }
+                    if (enabled)
+                        {
+                            throw new TRANSIENT();
+                        }
                 }
 
-                public void enableDelivery() {
+                public void enableDelivery()
+                {
                     super.enableDelivery();
 
                     enabled = true;
                 }
 
-                public void disableDelivery() {
+                public void disableDelivery()
+                {
                     super.disableDelivery();
 
                     enabled = false;
                 }
 
-                public void check() {
+                public void check()
+                {
                     super.check();
                     Assert.assertTrue(counter > 0);
                 }
@@ -134,21 +135,20 @@ public class PushToConsumerTest extends TestCase
 
         task.schedule();
 
-        //taskProcessor_.schedulePushToConsumerTask(task);
-
         long sleepTime =
             Environment.getIntPropertyWithDefault( Configuration.BACKOUT_INTERVAL,
                                                    Default.DEFAULT_BACKOUT_INTERVAL )
             * (Environment.getIntPropertyWithDefault(Configuration.EVENTCONSUMER_ERROR_THRESHOLD,
-                                                    Default.DEFAULT_EVENTCONSUMER_ERROR_THRESHOLD)
-            + 2);
+                    Default.DEFAULT_EVENTCONSUMER_ERROR_THRESHOLD)
+               + 2);
 
         Thread.sleep(sleepTime);
 
         eventConsumer.check();
     }
 
-    public void testPushFailRetry() throws Exception {
+    public void testPushFailRetry() throws Exception
+    {
 
         MockMessage event1 =
             new MockMessage();
@@ -180,15 +180,22 @@ public class PushToConsumerTest extends TestCase
         task2.setMessage(event2.getHandle());
 
 
-        MockEventConsumer eventConsumer = new MockEventConsumer() {
+        MockEventConsumer eventConsumer =
+            new MockEventConsumer()
+            {
                 boolean once = false;
-                public void deliverMessage(Message event) {
-                    if (!once) {
-                        once = true;
-                        throw new TRANSIENT();
-                    } else {
-                        super.deliverMessage(event);
-                    }
+
+                public void deliverMessage(Message event) throws Disconnected
+                {
+                    if (!once)
+                        {
+                            once = true;
+                            throw new TRANSIENT();
+                        }
+                    else
+                        {
+                            super.deliverMessage(event);
+                        }
                 }
             };
 
@@ -198,13 +205,9 @@ public class PushToConsumerTest extends TestCase
 
         task.schedule();
 
-        //taskProcessor_.schedulePushToConsumerTask(task);
-
         Thread.sleep(100);
 
         task2.schedule();
-
-        //taskProcessor_.schedulePushToConsumerTask(task2);
 
         Thread.sleep(4000);
 
@@ -214,7 +217,9 @@ public class PushToConsumerTest extends TestCase
         eventConsumer.check();
     }
 
-    public void testPushFailDispose() throws Exception {
+
+    public void testPushFailDispose() throws Exception
+    {
 
         PushToConsumerTask task =
             new PushToConsumerTask(taskProcessor_,
@@ -227,12 +232,13 @@ public class PushToConsumerTest extends TestCase
 
         event.setAny(any);
 
-        //        task.setTaskFinishHandler(taskProcessor_.getTaskConfigurator().deliverTaskFinishHandler_);
-//         task.setTaskErrorHandler(taskProcessor_.getTaskConfigurator().deliverTaskErrorHandler_);
         task.setMessage(event.getHandle());
 
-        MockEventConsumer eventConsumer = new MockEventConsumer()  {
-                public void deliverMessage(Message event) {
+        MockEventConsumer eventConsumer =
+            new MockEventConsumer()
+            {
+                public void deliverMessage(Message event)
+                {
                     throw new OBJECT_NOT_EXIST();
                 }
             };
@@ -241,17 +247,52 @@ public class PushToConsumerTest extends TestCase
 
         task.schedule();
 
-        //taskProcessor_.schedulePushToConsumerTask(task);
+        Thread.sleep(1000);
+
+        eventConsumer.check();
+
+        assertTrue(eventConsumer.disposeCalled == 1);
+    }
+
+
+    public void testPush_throwDisconnected_Dispose() throws Exception
+    {
+        PushToConsumerTask task =
+            new PushToConsumerTask(taskProcessor_,
+                                   taskProcessor_.getTaskFactory());
+
+        MockMessage event =
+            new MockMessage();
+
+        Any any = orb.create_any();
+
+        event.setAny(any);
+
+        task.setMessage(event.getHandle());
+
+        MockEventConsumer eventConsumer =
+            new MockEventConsumer()
+            {
+                public void deliverMessage(Message event) throws Disconnected
+                {
+                    throw new Disconnected();
+                }
+            };
+
+        task.setMessageConsumer(eventConsumer);
+
+        task.schedule();
 
         Thread.sleep(1000);
 
         eventConsumer.check();
 
-
         assertTrue(eventConsumer.disposeCalled == 1);
     }
 
-    public void testPush() throws Exception {
+
+    public void testPush() throws Exception
+    {
 
         PushToConsumerTask task =
             new PushToConsumerTask(taskProcessor_,
@@ -280,35 +321,23 @@ public class PushToConsumerTest extends TestCase
         eventConsumer.check();
     }
 
-    /**
-     * Creates a new <code>PushToConsumerTest</code> instance.
-     *
-     * @param name test name
-     */
+
     public PushToConsumerTest (String name)
     {
         super(name);
     }
 
-    /**
-     * @return a <code>TestSuite</code>
-     */
+
     public static Test suite()
     {
         TestSuite suite = new TestSuite(PushToConsumerTest.class);
 
-        suite = new TestSuite();
-        suite.addTest(new PushToConsumerTest("testRepeatedDeliveryErrorsCauseTheConsumerToBeDisconnected"));
-
         return suite;
     }
 
-    /**
-     * Entry point
-     */
+
     public static void main(String[] args)
     {
         junit.textui.TestRunner.run(suite());
     }
-
 }
