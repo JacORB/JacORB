90393efb6ef33995a8be5b5dfa9b63bc09bccddb
Gerald Brose <mail@geraldbrose.de>
2004-03-04 15:08:36 +0000

    *** empty log message ***

---
+++ src/org/jacorb/naming/namemanager/NSTableCellRenderer.java
@@ -0,0 +1,24 @@
+package org.jacorb.naming.namemanager;
+
+import javax.swing.*;
+import javax.swing.tree.*;
+import java.awt.*;
+import javax.swing.table.*;
+
+/**
+ * 
+ */
+public class NSTableCellRenderer 
+    extends DefaultTableCellRenderer
+{
+    public Component getTableCellRendererComponent(
+                            JTable table, Object color,
+                            boolean isSelected, boolean hasFocus,
+                            int row, int column) 
+    {
+        super.getTableCellRendererComponent(table, color, isSelected, hasFocus, row, column);
+        setForeground(Color.BLUE);
+        return this;
+    }
+}
+ 
