c9f5d5922616b555d580952c5b574fe446646a8f
David R Robison <drrobison@openroadsconsulting.com>
2004-03-16 17:55:40 +0000

    Adiron Testbed

--- test/sas/src/org/jacorb/test/sas/Basic_Client.java
+++ test/sas/src/org/jacorb/test/sas/Basic_Client.java
@@ -1,13 +1,27 @@
 package org.jacorb.test.sas;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 
+import org.omg.BiDirPolicy.BIDIRECTIONAL_POLICY_TYPE;
+import org.omg.BiDirPolicy.BOTH;
+import org.omg.BiDirPolicy.BidirectionalPolicyValueHelper;
+import org.omg.CORBA.Any;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Policy;
+import org.omg.PortableServer.IdAssignmentPolicyValue;
+import org.omg.PortableServer.ImplicitActivationPolicyValue;
+import org.omg.PortableServer.LifespanPolicyValue;
+import org.omg.PortableServer.POA;
 
 import InterOpTest.Johnson;
 import InterOpTest.JohnsonHelper;
+import InterOpTest.Peter;
+import InterOpTest.PeterHelper;
+import InterOpTest.PeterPOA;
 
 /**
  * This is the client side of the sas demo. It just calls the single
@@ -18,7 +32,10 @@ import InterOpTest.JohnsonHelper;
  * @version $Id$
  */
 
-public class Basic_Client {
+public class Basic_Client extends PeterPOA {
+
+    private static ORB orb = null;
+    
 	public static void main(String args[]) {
 		if (args.length != 3) {
 			System.out.println("Usage: <ior_file> <username> <password>");
@@ -27,7 +44,37 @@ public class Basic_Client {
 
 		try {
 			// initialize the ORB.
-			ORB orb = ORB.init(args, null);
+			orb = ORB.init(args, null);
+            
+            // initialize the POA
+            POA rootPOA = (POA) orb.resolve_initial_references("RootPOA");
+            Policy[] policies = new Policy[4];
+            policies[0] = rootPOA.create_lifespan_policy(LifespanPolicyValue.TRANSIENT);
+            policies[1] = rootPOA.create_id_assignment_policy(IdAssignmentPolicyValue.SYSTEM_ID);
+            policies[2] = rootPOA.create_implicit_activation_policy( ImplicitActivationPolicyValue.IMPLICIT_ACTIVATION );
+            Any bidirAny = orb.create_any();
+            BidirectionalPolicyValueHelper.insert( bidirAny, BOTH.value );
+            policies[3] = orb.create_policy( BIDIRECTIONAL_POLICY_TYPE.value, bidirAny );
+            POA bidirPOA = rootPOA.create_POA( "BiDirPOA", rootPOA.the_POAManager(), policies );
+            bidirPOA.the_POAManager().activate();
+            
+            // run the ORB
+            Thread orbThread = new Thread(new Runnable() {
+                public void run() {
+                    runORB();
+                    System.out.println("ORB closed");
+                }
+            });
+            orbThread.start();
+
+            // create peter and write his IOR
+            Basic_Client server = new Basic_Client();
+            bidirPOA.activate_object(server);
+            Peter peter = PeterHelper.narrow(bidirPOA.servant_to_reference(server));
+            PrintWriter pw = new PrintWriter(new FileWriter("peter.ior"));
+            pw.println(orb.object_to_string(peter));
+            pw.flush();
+            pw.close();
 
 			// get the server
 			URL iorURL = new URL(args[0]);
@@ -38,12 +85,28 @@ public class Basic_Client {
 			Johnson johnson = JohnsonHelper.narrow(obj);
 			
 			// call
-			String hello = johnson.say_hello("hello from JacORB");
-			System.out.println(hello);
+            System.out.println("CSS1:"+johnson.say_hello("hello from JacORB"));
+            System.out.println("CSS2:"+johnson.say_hello("hello from JacORB"));
+            System.out.println("TSS1:"+johnson.say_hello_from_you(peter, 1));
+            System.out.println("TSS2:"+johnson.say_hello_from_you(peter, 1));
 
 			System.out.println("Call to server succeeded");
 		} catch (Exception ex) {
+            System.out.println("Error: " + ex + ": " + ex.getMessage());
 			ex.printStackTrace();
 		}
+        if (orb != null) orb.shutdown(false);
 	}
+    
+    private static void runORB() {
+        orb.run();
+    }
+
+    /* (non-Javadoc)
+     * @see InterOpTest.PeterOperations#say_hello(java.lang.String)
+     */
+    public String say_hello(String msg) {
+        System.out.println("TSS: "+msg);
+        return "Hello from Peter!\n" + msg;
+    }
 }
--- test/sas/src/org/jacorb/test/sas/UP_Client.java
+++ test/sas/src/org/jacorb/test/sas/UP_Client.java
@@ -1,14 +1,27 @@
 package org.jacorb.test.sas;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 
+import org.jacorb.sasPolicy.SASPolicyValues;
+import org.jacorb.sasPolicy.SASPolicyValuesHelper;
+import org.jacorb.sasPolicy.SAS_POLICY_TYPE;
 import org.jacorb.security.sas.GssUpContext;
+import org.omg.CORBA.Any;
 import org.omg.CORBA.ORB;
+import org.omg.CSIIOP.EstablishTrustInClient;
+import org.omg.PortableServer.IdAssignmentPolicyValue;
+import org.omg.PortableServer.LifespanPolicyValue;
+import org.omg.PortableServer.POA;
 
 import InterOpTest.Johnson;
 import InterOpTest.JohnsonHelper;
+import InterOpTest.Peter;
+import InterOpTest.PeterHelper;
+import InterOpTest.PeterPOA;
 
 /**
  * This is the client side of the sas demo. It just calls the single
@@ -19,7 +32,10 @@ import InterOpTest.JohnsonHelper;
  * @version $Id$
  */
 
-public class UP_Client {
+public class UP_Client extends PeterPOA {
+
+    private static ORB orb;
+    
 	public static void main(String args[]) {
 		if (args.length != 3) {
 			System.out.println("Usage: <ior_file> <username> <password>");
@@ -31,7 +47,37 @@ public class UP_Client {
 			GssUpContext.setUsernamePassword(args[1], args[2]);
 
 			// initialize the ORB.
-			ORB orb = ORB.init(args, null);
+			orb = ORB.init(args, null);
+            
+            // initialize the POA
+            POA rootPOA = (POA) orb.resolve_initial_references("RootPOA");
+            org.omg.CORBA.Policy [] policies = new org.omg.CORBA.Policy[3];
+            policies[0] = rootPOA.create_id_assignment_policy(IdAssignmentPolicyValue.USER_ID);
+            policies[1] = rootPOA.create_lifespan_policy(LifespanPolicyValue.PERSISTENT);
+            Any sasAny = orb.create_any();
+            SASPolicyValuesHelper.insert( sasAny, new SASPolicyValues(EstablishTrustInClient.value, EstablishTrustInClient.value, true) );
+            policies[2] = orb.create_policy(SAS_POLICY_TYPE.value, sasAny);
+            POA securePOA = rootPOA.create_POA("SecurePOA", rootPOA.the_POAManager(), policies);
+            rootPOA.the_POAManager().activate();
+            
+            // run the ORB
+            Thread orbThread = new Thread(new Runnable() {
+                public void run() {
+                    runORB();
+                    System.out.println("ORB closed");
+                }
+            });
+            orbThread.start();
+
+            // create peter and write his IOR
+            UP_Client server = new UP_Client();
+            securePOA.activate_object_with_id("SecurePeter".getBytes(), server);
+            Peter peter = PeterHelper.narrow(rootPOA.servant_to_reference(server));
+            org.omg.CORBA.Object demo = securePOA.servant_to_reference(server);
+            PrintWriter pw = new PrintWriter(new FileWriter("peter.ior"));
+            pw.println(orb.object_to_string(demo));
+            pw.flush();
+            pw.close();
 
 			// get the server
 			URL iorURL = new URL(args[0]);
@@ -42,12 +88,26 @@ public class UP_Client {
 			Johnson johnson = JohnsonHelper.narrow(obj);
 			
 			// call
-			String hello = johnson.say_hello("hello from JacORB");
-			System.out.println(hello);
+            System.out.println("CSS1:"+johnson.say_hello("hello from JacORB"));
+            System.out.println("CSS2:"+johnson.say_hello("hello from JacORB"));
+            System.out.println("TSS1:"+johnson.say_hello_from_you(peter, 1));
+            System.out.println("TSS2:"+johnson.say_hello_from_you(peter, 1));
 
 			System.out.println("Call to server succeeded");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+        if (orb != null) orb.shutdown(false);
 	}
+    
+    private static void runORB() {
+        orb.run();
+    }
+
+    /* (non-Javadoc)
+     * @see InterOpTest.PeterOperations#say_hello(java.lang.String)
+     */
+    public String say_hello(String msg) {
+        return "Hello from Peter!\n" + msg;
+    }
 }
