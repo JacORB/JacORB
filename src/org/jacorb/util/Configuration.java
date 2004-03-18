582c2f6d5e8608af055ed9e84a3c599de65e6ebf
Gerald Brose <mail@geraldbrose.de>
2004-03-18 13:22:11 +0000

    (init) new order for loading common and specific properties files into the configuration (loadPropertiesFromFile,loadPropertiesFromClassPath) new helper methods.

--- src/org/jacorb/util/Configuration.java
+++ src/org/jacorb/util/Configuration.java
@@ -47,7 +47,8 @@ import java.util.Iterator;
 public class Configuration
     extends org.apache.avalon.framework.configuration.DefaultConfiguration
 {
-    private static final String fileSuffix = ".properties";
+    private static final String fileSuffix = ".orb.properties";
+    private static final String COMMON_PROPS = "common" + fileSuffix;
 
     private Configuration config;
     private String configName; 
@@ -79,81 +80,80 @@ public class Configuration
      * loads properties from files. Properties are loaded int the
      * following order, with later properties overriding earlier ones,
      * 1) System properties (incl. command line)
-     * 2) Configuration file, if any
-     * 3) ORB properties (set in the code). Note that these will thus 
-     *    always take effect!
+     * 2) common.orb.properties file
+     * 2) specific configuration file for the ORB (if any)
+     * 3) the ORB properties set in the client code and passed int through ORB.init(). 
+     *    (Note that these will thus always take effect!)
      */
 
     private void init(String name, Properties orbProperties)
     {
-        BufferedInputStream bin = null;
-        Properties properties = new Properties();
+        String separator = System.getProperty("file.separator");
+        String home = System.getProperty("user.home");
+        String lib = System.getProperty("java.home");
 
-        // first include system properties
+        // 1) include system properties
         setAttributes( System.getProperties() );
 
-        // find config directory:
-        String separator = 
-            System.getProperty("file.separator");
-        String jacorbHome = 
-            System.getProperty("jacorb.home");
+        // 2) look for orb.common.properties
 
-        if (jacorbHome == null)
+        // look for common properties files in java.home/lib first
+        Properties commonProps = 
+            loadPropertiesFromFile( lib + separator + "lib" + separator + COMMON_PROPS);
+
+        if (commonProps!= null)
+            setAttributes(commonProps);
+
+        // look for common properties files in user.home next
+        commonProps = 
+            loadPropertiesFromFile( home + separator + COMMON_PROPS );
+ 
+        if (commonProps!= null)
+            setAttributes(commonProps);
+
+        // look for common properties files on the classpath next
+        commonProps = 
+            loadPropertiesFromClassPath( COMMON_PROPS );
+
+        if (commonProps!= null)
+            setAttributes(commonProps);
+
+        // 3) look for specific properties file
+
+        String configDir = 
+            System.getProperty("jacorb.config.dir");
+
+        if (configDir == null)
+            configDir = System.getProperty("jacorb.home");
+
+        if (configDir != null )
+            configDir += separator + "etc";
+        else
         {
             System.err.println("[ jacorb.home unset! Will use '.']");
-            jacorbHome = ".";
+            configDir = ".";
         }
-
-        String path = jacorbHome + separator + "etc" + separator;
+        
+        String propFileName = configDir + separator + name + fileSuffix;
 
         // now load properties file from file system
-        try
-        {
-            bin =
-                new BufferedInputStream(new FileInputStream( path + 
-                                                             name +
-                                                             fileSuffix));
-            properties.load(bin);
-            bin.close();
+        Properties orbConfig = loadPropertiesFromFile(propFileName );
 
-            setAttributes(properties);
-            System.out.println("[configuration " + name + " loaded]");
-            return;
-        }
-        catch(java.io.IOException io )
+        if (orbConfig!= null)
         {
+            System.out.println("[configuration " + name + " loaded from file]");
+            setAttributes(orbConfig);
         }
 
         // now load properties file from classpath
-        try
+        orbConfig = loadPropertiesFromClassPath( name +  fileSuffix );
+        if (orbConfig!= null)
         {
-            java.net.URL url = null;
-            
-            // try first file name
-            url = ClassLoader.getSystemResource(name + fileSuffix);
-            if (url!=null)
-            {
-                properties.load( url.openStream() );
-                setAttributes(properties);
-                System.out.println("[configuration " + name + 
-                                   " loaded from classpath resource " +
-                                   url + "]");
-                return;
-            }
-            else
-            {
-                System.err.println("[No configuration for " + name + 
-                                   " found on classpath ]");
-
-            }
-        }
-        catch (java.io.IOException ioe)
-        {
-            System.err.println("[Error loading configuration " + name + 
-                               ": " + ioe.getMessage() + "]");
+            System.out.println("[configuration " + name + " loaded from classpath]");
+            setAttributes(orbConfig);
         }
 
-        // now load propertoes passed to ORB.init(), these will override any
+        // 4) load properties passed to ORB.init(), these will override any
         // settings in config files or system properties!
         if (orbProperties != null)
             setAttributes(orbProperties);
@@ -176,5 +176,59 @@ public class Configuration
             setAttribute(key, (String)properties.get(key));
         }
     }
+
+
+    /**
+     * Loads properties from a file
+     * @param fileName the name of a properties file
+     * @return a properties object or null, if fileName not found
+     */
+
+    private static Properties loadPropertiesFromFile(String fileName)
+    {
+        try
+        {
+            BufferedInputStream bin =
+                new BufferedInputStream( new FileInputStream(fileName));
+            Properties result = new Properties();
+            result.load(bin);
+            return result;
+        }
+        catch( java.io.IOException io )
+        {
+            io.printStackTrace(); //debug only
+            return null;
+        }
+    }
+
+
+    /**
+     * load properties file from classpath
+     * @param name the name of the properties file.
+     * @return a properties object or null, if name not found
+     */
+
+    private static Properties loadPropertiesFromClassPath(String name)
+    {
+        Properties result = null;
+        try
+        {
+            java.net.URL url = ClassLoader.getSystemResource(name);
+            if (url!=null)           
+            {
+                result = new Properties();
+                result.load( url.openStream() );
+            }
+        }
+        catch (java.io.IOException ioe)
+        {
+            ioe.printStackTrace(); //debug only
+        }
+        return result;
+    }
+
+
+
+
 
 }
