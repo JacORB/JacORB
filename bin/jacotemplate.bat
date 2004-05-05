e5774a16465b254b4a4584ff46c47bd146f61122
Gerald Brose <mail@geraldbrose.de>
2004-05-05 08:00:17 +0000

    added @@@JACORB_HOME@@@

--- bin/jacotemplate.bat
+++ bin/jacotemplate.bat
@@ -1,3 +1,3 @@
 @echo off
 rem call java interpreter
-java @@@CLASSPATH@@@ -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
+java @@@CLASSPATH@@@ -Djacorb.home=@@@JACORB_HOME@@@ -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
