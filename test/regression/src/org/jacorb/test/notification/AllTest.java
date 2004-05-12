4f0b0b8b738ff5ba3a147347d583eb8d5f248edf
Alphonse Bendt <jacorb@prismtech one>
2004-05-12 11:36:26 +0000

    corrected type in LICENSE

--- test/regression/src/org/jacorb/test/notification/AllTest.java
+++ test/regression/src/org/jacorb/test/notification/AllTest.java
@@ -13,7 +13,7 @@ package org.jacorb.test.notification;
  *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *   Library General Public License for more details.o
+ *   Library General Public License for more details.
  *
  *   You should have received a copy of the GNU Library General Public
  *   License along with this library; if not, write to the Free
