a889bd67bb3c3a2d1f047d41f08169a06f02f448
Alphonse Bendt <jacorb@prismtech one>
2004-06-18 23:09:22 +0000

    tests to test new custom filter interface

---
+++ test/regression/src/org/jacorb/test/notification/plugins/BSHFilterTest.java
@@ -0,0 +1,94 @@
+package org.jacorb.test.notification;
+
+import org.omg.CORBA.Any;
+import org.omg.CosNotification.EventType;
+import org.omg.CosNotifyFilter.ConstraintExp;
+import org.omg.CosNotifyFilter.ConstraintInfo;
+import org.omg.CosNotifyFilter.InvalidConstraint;
+
+import org.jacorb.notification.ApplicationContext;
+import org.jacorb.notification.filter.bsh.BSHFilter;
+
+import junit.framework.Test;
+
+/**
+ * @author Alphonse Bendt
+ */
+
+public class BSHFilterTest extends NotificationTestCase {
+
+    BSHFilter filter_;
+
+    ApplicationContext appContext_;
+
+    Any testData_;
+
+    ////////////////////////////////////////
+
+    public BSHFilterTest(String test, NotificationTestCaseSetup setup) {
+        super(test, setup);
+    }
+
+    ////////////////////////////////////////
+
+    public void setUp() throws Exception {
+        appContext_ =
+            new ApplicationContext(getORB(), getPOA() );
+
+        appContext_.configure(getConfiguration());
+
+        filter_ = new BSHFilter(appContext_);
+
+        filter_.configure(getConfiguration());
+
+        testData_ = getORB().create_any();
+
+        testData_.insert_long(10);
+    }
+
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        appContext_.dispose();
+    }
+
+    private void attachFilter(String filterExpr) throws InvalidConstraint {
+        ConstraintExp[] _constraintExp = new ConstraintExp[1];
+        EventType[] _eventType = new EventType[1];
+        _eventType[0] = new EventType("*", "*");
+
+        _constraintExp[0] = new ConstraintExp(_eventType, filterExpr);
+        ConstraintInfo[] _info = filter_.add_constraints(_constraintExp);
+    }
+
+    /**
+     * create remote filter object and invoke match operation on it
+     */
+    public void testReturnTrue() throws Exception {
+        String filterExpr = "return true";
+
+        attachFilter(filterExpr);
+
+        // this should match
+        assertTrue(filter_.match(testData_));
+    }
+
+    public void testSimpleMatch() throws Exception {
+        attachFilter("event.extract_long() == 10");
+
+        filter_.match(testData_);
+    }
+
+
+    public void testReturnFalse() throws Exception {
+        attachFilter("return false");
+
+        assertFalse(filter_.match(testData_));
+    }
+
+
+    public static Test suite() throws Exception {
+        return NotificationTestCase.suite(BSHFilterTest.class);
+    }
+}
---
+++ test/regression/src/org/jacorb/test/notification/plugins/FilterFactoryTest.java
@@ -0,0 +1,68 @@
+package org.jacorb.test.notification.plugins;
+
+/*
+ *        JacORB - a free Java ORB
+ *
+ *   Copyright (C) 1997-2003  Gerald Brose.
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+import org.jacorb.notification.filter.bsh.BSHFilter;
+import org.jacorb.test.notification.NotificationTestCase;
+import org.jacorb.test.notification.NotificationTestCaseSetup;
+
+import org.omg.CosNotifyFilter.Filter;
+import org.omg.CosNotifyFilter.FilterFactory;
+
+import junit.framework.Test;
+
+/**
+ * @author Alphonse Bendt
+ * @version $Id$
+ */
+public class FilterFactoryTest extends NotificationTestCase {
+
+    FilterFactory filterFactory_;
+
+    public void setUp() throws Exception {
+        filterFactory_ = getDefaultChannel().default_filter_factory();
+    }
+
+
+    public FilterFactoryTest(String name, NotificationTestCaseSetup setup) {
+        super(name, setup);
+    }
+
+
+    public void testCreateBSHFilter() throws Exception {
+        Filter _filter = filterFactory_.create_filter(BSHFilter.CONSTRAINT_GRAMMAR);
+
+        assertEquals(BSHFilter.CONSTRAINT_GRAMMAR, _filter.constraint_grammar());
+    }
+
+
+    public void testCreateNonExisting() throws Exception {
+        try {
+            filterFactory_.create_filter("ACME");
+            fail();
+        } catch (Exception e) {}
+    }
+
+
+    public static Test suite() throws Exception {
+        return NotificationTestCase.suite(FilterFactoryTest.class);
+    }
+}
