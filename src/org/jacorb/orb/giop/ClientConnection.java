94361eacd73d61b0eea3e9505576c2e37f4ce6c7
Andre Speigel <spiegel@drmirror.net>
2004-07-19 08:23:50 +0000

    (sendRequest): When receiving an exception, remove the ReplyPlaceholder to avoid memory leak.  This is part of the fix for bug 478, suggested by Marc Heide.

--- src/org/jacorb/orb/giop/ClientConnection.java
+++ src/org/jacorb/orb/giop/ClientConnection.java
@@ -278,7 +278,21 @@ public class ClientConnection
             replies.put( key, placeholder );
         }
 
-        sendRequest( os, response_expected );
+        try
+        {
+            sendRequest( os, response_expected );
+        }
+        catch( org.omg.CORBA.SystemException e )
+        {
+           //remove reply receiver from list
+           //because there will be no response to this request
+           synchronized( replies )
+           {
+              replies.remove( key );
+           }
+           throw e;
+        }
+
     }
 
     public void sendRequest( MessageOutputStream os,
