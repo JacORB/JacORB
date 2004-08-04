d51d7d659db354fd82140e9f194e1ffc3159856d
Andre Speigel <spiegel@drmirror.net>
2004-08-04 08:41:17 +0000

    Untabify.

--- src/org/jacorb/naming/ContextLister.java
+++ src/org/jacorb/naming/ContextLister.java
@@ -30,6 +30,7 @@ import java.util.*;
  * to a PrintStream
  * 
  * @author Gerald Brose
+ * @version $Id$
  */
 
 public class ContextLister
@@ -40,10 +41,10 @@ public class ContextLister
 
     public ContextLister(org.omg.CORBA.ORB orb) 
     {
-	this.orb = orb;
+        this.orb = orb;
         // initialise Naming Service via ORB
         try 
-	{
+        {
             org.omg.CORBA.Object obj = 
                 orb.resolve_initial_references("NameService");
             root_context = NamingContextExtHelper.narrow( obj );
@@ -55,18 +56,18 @@ public class ContextLister
             System.err.println(corba_exception);
         }
 
-	if( root_context == null ) {
-	    System.err.println("No Naming Context available, giving up ...");
-	    System.exit( 1 );
-	}    
+        if( root_context == null ) {
+            System.err.println("No Naming Context available, giving up ...");
+            System.exit( 1 );
+        }    
     }
 
     public ContextLister( org.omg.CORBA.ORB orb, String str ) 
     {
-	this.orb = orb;
+        this.orb = orb;
         // initialise Naming Service via stringified IOR
         try 
-	{
+        {
             org.omg.CORBA.Object obj = orb.string_to_object( str );
             root_context = NamingContextExtHelper.narrow( obj );
         }
@@ -74,125 +75,125 @@ public class ContextLister
             System.err.println(corba_exception);
         }
 
-	if( root_context == null ) {
-	    System.err.println("No Naming Context available, giving up ...");
-	    System.exit( 1 );
-	}  
+        if( root_context == null ) {
+            System.err.println("No Naming Context available, giving up ...");
+            System.exit( 1 );
+        }  
     }
 
     private void mark(NamingContextExt nc) {
-	contexts.put( orb.object_to_string(nc), "" );
+        contexts.put( orb.object_to_string(nc), "" );
     }
 
     private boolean isMarked(NamingContextExt nc) 
     {
-	return contexts.containsKey(orb.object_to_string(nc));
+        return contexts.containsKey(orb.object_to_string(nc));
     }
 
 
     public void list(java.io.PrintStream ps) 
     {
-	list( root_context, "   ", ps);
+        list( root_context, "   ", ps);
     }
 
     private void list( NamingContextExt n, String indent, java.io.PrintStream ps ) 
     {
-	if( isMarked(n)) 
+        if( isMarked(n)) 
         {
-	    return;
-	}
+            return;
+        }
 
-	mark(n);
+        mark(n);
 
-	try 
+        try 
         {
-	    BindingListHolder blsoh = 
-		new BindingListHolder(new Binding[0]);
+            BindingListHolder blsoh = 
+                new BindingListHolder(new Binding[0]);
 
-	    BindingIteratorHolder bioh = 
-		new BindingIteratorHolder();
+            BindingIteratorHolder bioh = 
+                new BindingIteratorHolder();
 
-	    n.list( 0, blsoh, bioh );
+            n.list( 0, blsoh, bioh );
 
-	    BindingHolder bh = new BindingHolder();
+            BindingHolder bh = new BindingHolder();
 
-	    if( bioh.value == null )
-		return; 
+            if( bioh.value == null )
+                return; 
 
-	    while( bioh.value.next_one( bh )) 
+            while( bioh.value.next_one( bh )) 
             {
-		String stringName = root_context.to_string( bh.value.binding_name);
-		ps.print( indent + stringName );
-		if( bh.value.binding_type.value() == BindingType._ncontext ) 
+                String stringName = root_context.to_string( bh.value.binding_name);
+                ps.print( indent + stringName );
+                if( bh.value.binding_type.value() == BindingType._ncontext ) 
                 {
-		    String _indent = indent + "\t";
-		    ps.println("/");
-		    
-		    NameComponent [] name = root_context.to_name(stringName);
-		    NamingContextExt sub_context = 
-			NamingContextExtHelper.narrow( n.resolve(name) );
-		    list( sub_context, _indent, ps );
-		}
-		else
-		    System.out.println();
-	    }
-	} 
-	catch (Exception e) 
-	{
-	    e.printStackTrace();
-	}
+                    String _indent = indent + "\t";
+                    ps.println("/");
+                    
+                    NameComponent [] name = root_context.to_name(stringName);
+                    NamingContextExt sub_context = 
+                        NamingContextExtHelper.narrow( n.resolve(name) );
+                    list( sub_context, _indent, ps );
+                }
+                else
+                    System.out.println();
+            }
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String args[]) 
     {
-	org.omg.CORBA.ORB orb = 
+        org.omg.CORBA.ORB orb = 
             org.omg.CORBA.ORB.init(args,null);
 
-	ContextLister ctxLister;
-	PrintStream pw = System.out;
-	String url = null;
+        ContextLister ctxLister;
+        PrintStream pw = System.out;
+        String url = null;
 
-	for( int i = 0; i < args.length; i += 2 )
-	{
-	    try
-	    {
-		if( args[i].startsWith("-f"))
-		{
-		    try
-		    {
-			pw = new PrintStream( new FileOutputStream( args[i+1] ));		    
-		    }
-		    catch( IOException ioe)
-		    {
-			System.err.println( ioe.getMessage() );
-			System.exit(1);
-		    }
-		    continue;
-		}
-		if( args[i].startsWith("-url"))
-		{
-		    url = args[i+1];
-		}
-	    } 
-	    catch( Exception e )
-	    {
-		System.err.println("Usage: org.jacorb.naming.ContextLister [-url object url] [-f output file]");
-		System.exit(1);
+        for( int i = 0; i < args.length; i += 2 )
+        {
+            try
+            {
+                if( args[i].startsWith("-f"))
+                {
+                    try
+                    {
+                        pw = new PrintStream( new FileOutputStream( args[i+1] ));                   
+                    }
+                    catch( IOException ioe)
+                    {
+                        System.err.println( ioe.getMessage() );
+                        System.exit(1);
+                    }
+                    continue;
+                }
+                if( args[i].startsWith("-url"))
+                {
+                    url = args[i+1];
+                }
+            } 
+            catch( Exception e )
+            {
+                System.err.println("Usage: org.jacorb.naming.ContextLister [-url object url] [-f output file]");
+                System.exit(1);
 
-	    }
-	}
+            }
+        }
 
-	if( url != null )
-	{
-	    ctxLister = new ContextLister(orb, url);
-	}
-	else
-	{
-	    ctxLister = new ContextLister(orb);
-	}
+        if( url != null )
+        {
+            ctxLister = new ContextLister(orb, url);
+        }
+        else
+        {
+            ctxLister = new ContextLister(orb);
+        }
 
-	ctxLister.list( pw );
+        ctxLister.list( pw );
         orb.shutdown(true);
     }
 }
