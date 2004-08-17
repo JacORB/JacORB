fbc49239af3b415a5ba8593d9d683cfba81eea0a
Alphonse Bendt <jacorb@prismtech one>
2004-08-17 13:52:48 +0000

    (disconnect) instead of setting pushConsumer_ to null set to NULL_CONSUMER

--- src/org/jacorb/notification/servant/StructuredProxyPushSupplierImpl.java
+++ src/org/jacorb/notification/servant/StructuredProxyPushSupplierImpl.java
@@ -28,74 +28,93 @@ import org.jacorb.notification.engine.PushStructuredOperation;
 import org.jacorb.notification.interfaces.Message;
 import org.jacorb.notification.interfaces.MessageConsumer;
 import org.omg.CosEventChannelAdmin.AlreadyConnected;
+import org.omg.CosNotification.EventType;
+import org.omg.CosNotification.StructuredEvent;
 import org.omg.CosNotifyChannelAdmin.ProxySupplierHelper;
 import org.omg.CosNotifyChannelAdmin.ProxyType;
 import org.omg.CosNotifyChannelAdmin.StructuredProxyPushSupplierOperations;
 import org.omg.CosNotifyChannelAdmin.StructuredProxyPushSupplierPOATie;
+import org.omg.CosNotifyComm.InvalidEventType;
 import org.omg.CosNotifyComm.StructuredPushConsumer;
+import org.omg.CosNotifyComm.StructuredPushConsumerOperations;
 import org.omg.PortableServer.Servant;
 
 /**
  * @author Alphonse Bendt
- * @version $Id$
+ * @version $Id: StructuredProxyPushSupplierImpl.java,v 1.9 2004/07/12 11:19:56
+ *          alphonse.bendt Exp $
  */
 
-public class StructuredProxyPushSupplierImpl
-    extends AbstractProxySupplier
-    implements StructuredProxyPushSupplierOperations
+public class StructuredProxyPushSupplierImpl extends AbstractProxySupplier implements
+        StructuredProxyPushSupplierOperations
 {
-    private StructuredPushConsumer pushConsumer_;
+    private final static StructuredPushConsumerOperations NULL_CONSUMER = new StructuredPushConsumerOperations()
+    {
+        public void push_structured_event(StructuredEvent event)
+        {
+        }
+
+        public void disconnect_structured_push_consumer()
+        {
+        }
+
+        public void offer_change(EventType[] added, EventType[] removed) throws InvalidEventType
+        {
+        }
+    };
+
+    private StructuredPushConsumerOperations pushConsumer_;
+    
+    private final Object refLock_ = new Object();
 
     ////////////////////////////////////////
 
-
-    public ProxyType MyType() {
+    public ProxyType MyType()
+    {
         return ProxyType.PUSH_STRUCTURED;
     }
 
     /**
      * TODO check error handling when push fails
      */
-    public void deliverMessage( final Message message )
+    public void deliverMessage(final Message message)
     {
         if (logger_.isDebugEnabled())
         {
-            logger_.debug( "deliverMessage() connected="
-                           + isConnected()
-                           + " suspended="
-                           + isSuspended()
-                           + " enabled="
-                           + isEnabled() );
+            logger_.debug("deliverMessage() connected=" + isConnected() + " suspended="
+                    + isSuspended() + " enabled=" + isEnabled());
         }
 
-        if ( isConnected() ) {
+        if (isConnected())
+        {
 
-            if ( !isSuspended() && isEnabled() )
+            if (!isSuspended() && isEnabled())
+            {
+                try
                 {
-                    try {
-                        pushConsumer_.push_structured_event( message.toStructuredEvent() );
-                    } catch (Throwable e) {
-                        PushStructuredOperation _failedOperation =
-                            new PushStructuredOperation(pushConsumer_, message);
+                    pushConsumer_.push_structured_event(message.toStructuredEvent());
+                } catch (Throwable e)
+                {
+                    PushStructuredOperation _failedOperation = new PushStructuredOperation(
+                            pushConsumer_, message);
 
-                        handleFailedPushOperation(_failedOperation, e);
-                    }
+                    handleFailedPushOperation(_failedOperation, e);
                 }
+            }
             else
-                {
-                    // not enabled
-                    enqueue( message );
-                }
+            {
+                // not enabled
+                enqueue(message);
+            }
         }
         else
         {
-            logger_.debug( "Not connected" );
+            logger_.debug("Not connected");
         }
     }
 
-
-    public void connect_structured_push_consumer( StructuredPushConsumer consumer )
-        throws AlreadyConnected
+    public void connect_structured_push_consumer(StructuredPushConsumer consumer)
+            throws AlreadyConnected
     {
         assertNotConnected();
 
@@ -109,72 +128,64 @@ public class StructuredProxyPushSupplierImpl
         connectClient(consumer);
     }
 
-
     public void disconnect_structured_push_supplier()
     {
         dispose();
     }
-
 
     public void deliverPendingData()
     {
         Message[] _events = getAllMessages();
 
         if (_events != null)
+        {
+            for (int x = 0; x < _events.length; ++x)
             {
-                for (int x = 0; x < _events.length; ++x)
-                    {
-                        deliverMessage(_events[x]);
+                deliverMessage(_events[x]);
 
-                        _events[x].dispose();
-                    }
+                _events[x].dispose();
             }
+        }
     }
 
-
-    protected void connectionResumed() {
+    protected void connectionResumed()
+    {
         scheduleDeliverPendingMessagesOperation_.run();
     }
-
 
     protected void disconnectClient()
     {
         pushConsumer_.disconnect_structured_push_consumer();
 
-        pushConsumer_ = null;
+        pushConsumer_ = NULL_CONSUMER;
     }
-
 
     public List getSubsequentFilterStages()
     {
-        return CollectionsWrapper.singletonList( this );
+        return CollectionsWrapper.singletonList(this);
     }
-
 
     public MessageConsumer getMessageConsumer()
     {
         return this;
     }
 
-
     public boolean hasMessageConsumer()
     {
         return true;
     }
 
-
     public synchronized Servant getServant()
     {
-        if ( thisServant_ == null )
+        if (thisServant_ == null)
         {
-            thisServant_ = new StructuredProxyPushSupplierPOATie( this );
+            thisServant_ = new StructuredProxyPushSupplierPOATie(this);
         }
         return thisServant_;
     }
 
-
     public org.omg.CORBA.Object activate()
     {
-        return ProxySupplierHelper.narrow( getServant()._this_object(getORB()) );
+        return ProxySupplierHelper.narrow(getServant()._this_object(getORB()));
     }
 }
