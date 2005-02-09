c9ecfdb8f3ced95d878fd54aaa105253b6ec774d
Andre Speigel <spiegel@drmirror.net>
2005-02-09 09:50:10 +0000

    (ctor, getInstance): Take a Configuration argument. (MAX_DEFAULT, maxConnectionThreads): New instance fields.

--- src/org/jacorb/orb/giop/MessageReceptorPool.java
+++ src/org/jacorb/orb/giop/MessageReceptorPool.java
@@ -20,6 +20,9 @@
 
 package org.jacorb.orb.giop;
 
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.framework.configuration.Configuration;
+
 import org.jacorb.util.threadpool.*;
 
 /**
@@ -34,11 +37,32 @@ import org.jacorb.util.threadpool.*;
 
 public class MessageReceptorPool
 {
+    private static final int MAX_DEFAULT = 1000;
+
     private static MessageReceptorPool singleton = null;
+
+    private int maxConnectionThreads = 1000;
     private ThreadPool pool = null;
 
-    private MessageReceptorPool()
+    private MessageReceptorPool(Configuration myConfiguration)
     {
+        org.jacorb.config.Configuration configuration =
+            (org.jacorb.config.Configuration) myConfiguration;
+
+        int maxConnectionThreads = MAX_DEFAULT;
+
+        final String attribute = "jacorb.connection.max_threads";
+
+        maxConnectionThreads =
+            configuration.getAttributeAsInteger(attribute, MAX_DEFAULT);
+
+        Logger logger = configuration.getNamedLogger("jacorb.orb.giop");
+
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("Maximum connection threads: " + maxConnectionThreads);
+        }
+
         pool = 
             new ThreadPool( new ConsumerFactory(){
                     public Consumer create()
@@ -46,15 +70,15 @@ public class MessageReceptorPool
                         return new MessageReceptor();
                     }
                 },
-                               1000, //maximum number of connections
+                               maxConnectionThreads, //maximum number of connections
                                5 ); //max idle threads
     }
 
-    public static synchronized MessageReceptorPool getInstance()
+    public static synchronized MessageReceptorPool getInstance(Configuration myConfiguration)
     {
         if( singleton == null )
         {
-            singleton = new MessageReceptorPool();
+            singleton = new MessageReceptorPool(myConfiguration);
         }
 
         return singleton;
