cdcca9e152b6723d37a61533db416373564b4cd9
Alphonse Bendt <jacorb@prismtech one>
2005-08-21 13:11:30 +0000

    (c'tor): load Helper class via the Service classes Classloader if available

--- src/org/jacorb/notification/container/CORBAObjectComponentAdapter.java
+++ src/org/jacorb/notification/container/CORBAObjectComponentAdapter.java
@@ -23,6 +23,7 @@ package org.jacorb.notification.container;
 
 import java.lang.reflect.Method;
 
+import org.jacorb.util.ObjectUtil;
 import org.picocontainer.PicoContainer;
 import org.picocontainer.PicoInitializationException;
 import org.picocontainer.PicoIntrospectionException;
@@ -43,10 +44,8 @@ public class CORBAObjectComponentAdapter extends AbstractComponentAdapter
     /**
      * Component Adapter to specify a reference to a CORBA Object.
      * 
-     * @param service
-     *            CORBA Interface the delegate offers
-     * @param delegate
-     *            CORBA object that offers the service
+     * @param service CORBA Interface the delegate offers
+     * @param delegate CORBA object that offers the service
      */
     public CORBAObjectComponentAdapter(Class service, org.omg.CORBA.Object delegate)
     {
@@ -58,7 +57,16 @@ public class CORBAObjectComponentAdapter extends AbstractComponentAdapter
 
         try
         {
-            Class _helperClass = Class.forName(_helperClassName);
+            final Class _helperClass;
+            if (service.getClassLoader() != null)
+            {
+                _helperClass = service.getClassLoader().loadClass(_helperClassName);
+            }
+            else
+            {
+                _helperClass = ObjectUtil.classForName(_helperClassName);
+            }
+            
             Method _idMethod = _helperClass.getMethod("id", new Class[0]);
             String _id = (String) _idMethod.invoke(null, null);
 
