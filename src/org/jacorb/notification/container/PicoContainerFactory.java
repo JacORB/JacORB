c4efffabaec76d5921cd103897aac45920513e30
Alphonse Bendt <jacorb@prismtech one>
2005-08-21 13:12:51 +0000

    (createContainer): split method into smaller methods

--- src/org/jacorb/notification/container/PicoContainerFactory.java
+++ src/org/jacorb/notification/container/PicoContainerFactory.java
@@ -24,21 +24,17 @@ package org.jacorb.notification.container;
 import java.lang.reflect.Constructor;
 
 import org.apache.avalon.framework.logger.Logger;
-import org.jacorb.notification.MessageFactory;
 import org.jacorb.notification.engine.ConfigurablePushTaskExecutorFactory;
 import org.jacorb.notification.engine.DefaultTaskProcessor;
 import org.jacorb.notification.engine.PushTaskExecutorFactory;
-import org.jacorb.notification.engine.TaskProcessor;
-import org.jacorb.notification.filter.ETCLEvaluator;
 import org.jacorb.notification.filter.impl.DefaultETCLEvaluator;
 import org.jacorb.notification.impl.DefaultMessageFactory;
 import org.jacorb.notification.impl.PoolingEvaluationContextFactory;
-import org.jacorb.notification.interfaces.EvaluationContextFactory;
 import org.jacorb.orb.ORB;
-import org.picocontainer.ComponentAdapter;
 import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.PicoContainer;
 import org.picocontainer.defaults.CachingComponentAdapter;
+import org.picocontainer.defaults.CachingComponentAdapterFactory;
 import org.picocontainer.defaults.ComponentAdapterFactory;
 import org.picocontainer.defaults.ConstructorInjectionComponentAdapter;
 import org.picocontainer.defaults.ConstructorInjectionComponentAdapterFactory;
@@ -55,86 +51,90 @@ public class PicoContainerFactory
         return createRootContainer(null, orb);
     }
 
-    public static MutablePicoContainer createRootContainer(PicoContainer parent, ORB orb)
+    public static MutablePicoContainer createRootContainer(MutablePicoContainer optionalParent, ORB orb)
     {
-        final org.jacorb.config.Configuration config = orb.getConfiguration();
-        final Logger _logger = config.getNamedLogger(PicoContainerFactory.class.getName());
+        final org.jacorb.config.Configuration _config = orb.getConfiguration();
+        final Logger _logger = _config.getNamedLogger(PicoContainerFactory.class.getName());
 
         final MutablePicoContainer _container;
 
-        if (parent == null)
+        if (optionalParent == null)
         {
-            ComponentAdapterFactory _componentAdapterFactory = new ConstructorInjectionComponentAdapterFactory();
-
-            _container = new DefaultPicoContainer(_componentAdapterFactory);
-
-            _container.registerComponentInstance(ComponentAdapterFactory.class,
-                    _componentAdapterFactory);
-
-            _logger.debug("Created Top Level Container");
+            _container = createContainer(_logger);
         }
         else
         {
-            ComponentAdapterFactory _componentAdapterFactory = (ComponentAdapterFactory) parent
-                    .getComponentInstance(ComponentAdapterFactory.class);
-
-            _container = new DefaultPicoContainer(_componentAdapterFactory, parent);
-
-            _logger.debug("Created Container with Parent");
+            _container = createContainer(optionalParent, _logger);
         }
 
         // register existing orb
         _container.registerComponentInstance(org.omg.CORBA.ORB.class, orb);
 
-        // register services that need to be looked up via orb using custom componentadapter
-        _container.registerComponent(new CachingComponentAdapter(new BiDirGiopPOAComponentAdapter(new POAComponentAdapter())));
+        registerORBServices(_container);
 
-        _container.registerComponent(new CachingComponentAdapter(
-                new DynAnyFactoryComponentAdapter()));
+        registerCoreServices(_container);
 
-        _container.registerComponent(new CachingComponentAdapter(
-                new ConfigurationComponentAdapter()));
+        return _container;
+    }
 
-        _container.registerComponent(new CachingComponentAdapter(
-                new FilterFactoryComponentAdapter()));
-
-        _container.registerComponent(new CachingComponentAdapter(new RepositoryComponentAdapter()));
-
-        _container.registerComponent(new CachingComponentAdapter(
+    private static void registerCoreServices(final MutablePicoContainer container)
+    {
+        container.registerComponent(new CachingComponentAdapter(
                 new PushTaskExecutorFactoryComponentAdapter(
-                        new ConstructorInjectionComponentAdapter(PushTaskExecutorFactory.class,
-                                ConfigurablePushTaskExecutorFactory.class))));
-
-        // register core services
+                        new ConstructorInjectionComponentAdapter(PushTaskExecutorFactory.class, ConfigurablePushTaskExecutorFactory.class))));
 
         // etcl evaluator
 
-        ComponentAdapter _evaluatorAdapter = newComponentAdapter(_container, ETCLEvaluator.class,
-                DefaultETCLEvaluator.class);
-
-        _container.registerComponent(_evaluatorAdapter);
+        container.registerComponentImplementation(DefaultETCLEvaluator.class);
 
         // message factory
 
-        ComponentAdapter _messageFactoryAdapter = newComponentAdapter(_container,
-                MessageFactory.class, DefaultMessageFactory.class);
-
-        _container.registerComponent(_messageFactoryAdapter);
+        container.registerComponentImplementation(DefaultMessageFactory.class);
 
         // taskprocessor
 
-        ComponentAdapter _taskProcessorAdapter = newComponentAdapter(_container,
-                TaskProcessor.class, DefaultTaskProcessor.class);
-
-        _container.registerComponent(_taskProcessorAdapter);
+        container.registerComponentImplementation(DefaultTaskProcessor.class);
 
         // evaluation context factory
 
-        ComponentAdapter _evaluationContextAdapter = newComponentAdapter(_container,
-                EvaluationContextFactory.class, PoolingEvaluationContextFactory.class);
+        container.registerComponentImplementation(PoolingEvaluationContextFactory.class);
+    }
 
-        _container.registerComponent(_evaluationContextAdapter);
+    private static void registerORBServices(final MutablePicoContainer container)
+    {
+        // register services that need to be looked up via orb using custom componentadapter
+        container.registerComponent(new CachingComponentAdapter(new BiDirGiopPOAComponentAdapter(new POAComponentAdapter())));
 
+        container.registerComponent(new CachingComponentAdapter(new DynAnyFactoryComponentAdapter()));
+
+        container.registerComponent(new CachingComponentAdapter(new ConfigurationComponentAdapter()));
+
+        container.registerComponent(new CachingComponentAdapter(new FilterFactoryComponentAdapter()));
+
+        container.registerComponent(new CachingComponentAdapter(new RepositoryComponentAdapter()));
+    }
+
+    private static MutablePicoContainer createContainer(MutablePicoContainer parent, final Logger logger)
+    {
+        final MutablePicoContainer _container;
+        _container = parent.makeChildContainer();
+        
+        logger.debug("Created Container with Parent");
+        return _container;
+    }
+
+    private static MutablePicoContainer createContainer(final Logger logger)
+    {
+        final MutablePicoContainer _container;
+        final ConstructorInjectionComponentAdapterFactory _nonCachingCAFactory = new ConstructorInjectionComponentAdapterFactory();
+        ComponentAdapterFactory _cachingCAFactory = new CachingComponentAdapterFactory(_nonCachingCAFactory);
+
+        _container = new DefaultPicoContainer(_cachingCAFactory);
+
+        _container.registerComponentInstance(ComponentAdapterFactory.class,
+                _nonCachingCAFactory);
+
+        logger.debug("Created Top Level Container");
         return _container;
     }
 
@@ -145,24 +145,11 @@ public class PicoContainerFactory
         return child;
     }
 
-    /**
-     * create a ComponentAdapter using the ComponentAdapterFactory registered in the provided
-     * container. the resulting ComponentAdapter is wrapped in a CachingComponentAdapter.
-     */
-    private static ComponentAdapter newComponentAdapter(PicoContainer container, Class service,
-            Class implementation)
-    {
-        ComponentAdapterFactory _adapterFactory = (ComponentAdapterFactory) container
-                .getComponentInstance(ComponentAdapterFactory.class);
-
-        return new CachingComponentAdapter(_adapterFactory.createComponentAdapter(service,
-                implementation, null));
-    }
 
     /**
      * helper method for easier debugging of unresolved dependencies.
      * 
-     * DO NOT DELETE even if method is not referenced.
+     * do NOT delete even if method is not referenced.
      */
     public static void dumpDependencies(PicoContainer container, Class clazzToBeCreated)
     {
