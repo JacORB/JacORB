653a3bc3287fe9fc2d1e84012f569b71ad4a2c3f
Andre Speigel <spiegel@drmirror.net>
2006-05-11 09:35:10 +0000

    (main): Add shutdown hook as suggested by David Rodenbaugh in bug #605.

--- src/org/jacorb/naming/NameServer.java
+++ src/org/jacorb/naming/NameServer.java
@@ -337,6 +337,20 @@ public class NameServer
                 logger.info("NS up");
             }
 
+            // This shutdown hook fixes the fact that servants aren't being
+            // etherealized because orb.shutdown is not getting called
+            // when ns is killed.
+            Thread shutdownHook = new Thread()
+            {
+               public void run()
+               {
+                  logger.info( "shutdownHook invoked" );
+                  orb.shutdown( true );
+               }
+            };
+            Runtime.getRuntime().addShutdownHook( shutdownHook );
+
+
             /* either block indefinitely or time out */
 
             if( time_out == 0 )
@@ -348,6 +362,17 @@ public class NameServer
             /* shutdown. This will etherealize all servants, thus
                saving their state */
             orb.shutdown( true );
+
+            try
+            {
+               Runtime.getRuntime().removeShutdownHook( shutdownHook );
+            }
+            catch( Exception removeShutdownHookException )
+            {
+               // removeShutdownHook will throw ignorable illegal state
+               // exception if got here via signal.  Ignore exception.
+            }
+
 
             //      System.exit(0);
         }
