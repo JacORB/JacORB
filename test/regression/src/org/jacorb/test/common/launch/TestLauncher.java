a007a1ab668a831c6dbe1a4ad976253d13965a40
Alphonse Bendt <jacorb@prismtech one>
2006-05-12 14:34:33 +0000

    (main): check if FixSunDelegateBug class is really available.

--- test/regression/src/org/jacorb/test/common/launch/TestLauncher.java
+++ test/regression/src/org/jacorb/test/common/launch/TestLauncher.java
@@ -26,27 +26,28 @@ import java.util.*;
 import java.text.*;
 
 import org.jacorb.test.common.*;
+import org.jacorb.util.ObjectUtil;
 
 /**
  * This is the main class for launching regression tests.  It takes care
  * of launching the client side (on which the JUnit tests are actually executed)
  * against an appropriate JacORB version.  The client-side code itself
  * launches servers when appropriate.  This is the invocation syntax:
- * 
+ *
  *   java [ -Djacorb.test.client.version=CLIENT_VERSION ]
  *        [ -Djacorb.test.server.version=SERVER_VERSION ]
  *        [ -Djacorb.test.coverage=on/off ]
  *        org.jacorb.test.common.launch.TestLauncher
  *        TESTSUITE
- * 
+ *
  * Here, CLIENT_VERSION and SERVER_VERSION are ids of available JacORB
- * installations, as specified in the file 
+ * installations, as specified in the file
  * $JACORB_HOME/test/regression/test.properties.
  * The third optional property, jacorb.test.coverage, specifies whether
  * coverage information should be collected during this test run or not.
  * The final argument, after the name of the class itself, is the name
  * of the TESTSUITE to execute (e.g. org.jacorb.test.AllTest).
- * 
+ *
  * @author Andre Spiegel spiegel@gnu.org
  * @version $Id$
  */
@@ -55,22 +56,22 @@ public class TestLauncher
     private static String[] args = null;
     private static PrintWriter outFile = null;
     private static Date testDate = new java.util.Date();
-    
+
     private static DateFormat idFormatter =
         new SimpleDateFormat ("yyyy-MM-dd.HH-mm-ss");
-    
+
     private static DateFormat dateStringFormatter =
         new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss Z");
-    
+
     private static class Listener extends Thread
     {
         private BufferedReader in = null;
-        
+
         public Listener (InputStream in)
         {
             this.in = new BufferedReader (new InputStreamReader (in));
         }
-  
+
         public void run()
         {
             try
@@ -85,13 +86,13 @@ public class TestLauncher
             }
             catch (Exception ex)
             {
-                System.out.println ("exception reading from test client: " 
+                System.out.println ("exception reading from test client: "
                                     + ex.toString());
             }
         }
 
     }
-    
+
     public static void printTestHeader (PrintWriter out)
     {
         out.println ("-------------------------------------------------------------------------------");
@@ -110,55 +111,55 @@ public class TestLauncher
         out.println();
         out.println ("-------------------------------------------------------------------------------");
     }
-    
+
     public static void printTestHeader (PrintStream out)
     {
         PrintWriter x = new PrintWriter (out);
         printTestHeader(x);
         x.flush();
     }
-    
+
     public static String getSuiteName()
     {
         return args[0];
     }
-    
+
     public static Date getTestDate()
     {
         return testDate;
     }
-    
+
     public static String getTestID()
     {
         return idFormatter.format (getTestDate());
     }
-    
+
     public static String getTestDateString()
     {
         return dateStringFormatter.format (getTestDate());
     }
-    
+
     public static boolean getCoverage()
     {
         String cs = System.getProperty("jacorb.test.coverage", "false");
         return cs.equals("true") || cs.equals("on") || cs.equals("yes");
     }
-    
+
     public static String getClientVersion()
     {
         return System.getProperty ("jacorb.test.client.version", "cvs");
     }
-    
+
     public static String getServerVersion()
     {
         return System.getProperty ("jacorb.test.server.version", "cvs");
     }
-    
+
     public static String getTestUser()
     {
         return System.getProperty ("user.name", "<unknown>");
     }
-    
+
     public static String getTestPlatform()
     {
         return "java " + System.getProperty ("java.version")
@@ -167,7 +168,7 @@ public class TestLauncher
              + System.getProperty ("os.version") + " ("
              + System.getProperty ("os.arch") + ")";
     }
-    
+
     public static String getOutFilename()
     {
         String dir = TestUtils.testHome() + "/output/" + getTestID();
@@ -176,17 +177,17 @@ public class TestLauncher
         if (!dirF.exists()) dirF.mkdir();
         return dir + "/report.txt";
     }
-    
+
     public static void main(String[] args) throws Exception
     {
         TestLauncher.args = args;
         outFile = new PrintWriter (new FileWriter (getOutFilename()));
         printTestHeader (outFile);
         printTestHeader (System.out);
-   
+
         String mainClass = "junit.textui.TestRunner";
         String testHome = TestUtils.osDependentPath(TestUtils.testHome());
-        
+
         String classpath = testHome + "/classes/";
         classpath = TestUtils.pathAppend(classpath, testHome + "/lib/junit.jar");
         classpath = TestUtils.pathAppend(classpath, testHome + "/lib/easymock-1.1.jar");
@@ -197,56 +198,64 @@ public class TestLauncher
         props.put("jacorb.test.coverage", getCoverage() ? "true" : "false");
         props.put("jacorb.test.client.version", getClientVersion());
         props.put("jacorb.test.server.version", getServerVersion());
-        props.put("javax.rmi.CORBA.UtilClass",
-            "org.jacorb.test.orb.rmi.FixSunDelegateBug");
-        
+        try
+        {
+            ObjectUtil.classForName("org.jacorb.test.orb.rmi.FixSunDelegateBug");
+            props.put("javax.rmi.CORBA.UtilClass",
+                "org.jacorb.test.orb.rmi.FixSunDelegateBug");
+            System.err.println("Using org.jacorb.test.orb.rmi.FixSunDelegateBug");
+        }
+        catch(Exception e)
+        {
+        }
+
         props.put("jacorb.test.home", testHome);
 
-        if (TestUtils.isWindows()) 
+        if (TestUtils.isWindows())
         {
-            try 
+            try
             {
                 String systemRoot = TestUtils.systemRoot();
                 props.put("jacorb.SystemRoot", systemRoot);
             }
-            catch (IOException e) 
+            catch (IOException e)
             {
                 throw e;
             }
-            catch (RuntimeException e) 
+            catch (RuntimeException e)
             {
                 throw e;
             }
         }
-        
-        
+
+
         JacORBLauncher launcher = JacORBLauncher.getLauncher
         (
             getClientVersion(), getCoverage()
         );
-        
+
         if (getCoverage())
         {
-            String coveragePath=new String(launcher.getJacorbHome() 
-                    + "/test/regression/output/" 
+            String coveragePath=new String(launcher.getJacorbHome()
+                    + "/test/regression/output/"
                     + getTestID() + "/coverage-client.ec");
             coveragePath = TestUtils.osDependentPath(coveragePath);
             props.put ("emma.coverage.out.file", coveragePath);
         }
         String printArgs = "";
-        for (int i=0; i<args.length; i++) 
+        for (int i=0; i<args.length; i++)
         {
             printArgs += args[i] + " ";
-        }	
-        
+        }
+
         Process p = launcher.launch(classpath, props, mainClass, args);
         Listener outL = new Listener(p.getInputStream());
         outL.start();
         Listener errL = new Listener(p.getErrorStream());
         errL.start();
-        
+
         p.waitFor();
-        
+
         outFile.close();
     }
 }
