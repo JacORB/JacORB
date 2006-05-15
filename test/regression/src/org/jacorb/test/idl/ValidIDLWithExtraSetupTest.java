0d40749eeeedec6e96c161300a21c487662cd468
Alphonse Bendt <jacorb@prismtech one>
2006-05-15 09:46:50 +0000

    RT#1180: Problem with idl compiler
    added testcase. no further action necessary as the problem
    has been fixed already (Bugzilla #606)

--- test/regression/src/org/jacorb/test/idl/ValidIDLWithExtraSetupTest.java
+++ test/regression/src/org/jacorb/test/idl/ValidIDLWithExtraSetupTest.java
@@ -79,6 +79,7 @@ public class ValidIDLWithExtraSetupTest extends AbstractIDLTestcase
         suite.addTest(new ValidIDLWithExtraSetupTest("-I" + testHome + "/../../idl/omg", testHome + "/idl/compiler/misc/rt1051.idl"));
         suite.addTest(new ValidIDLWithExtraSetupTest("-DDefB", testHome + "/idl/compiler/misc/Ping1.idl"));
         suite.addTest(new ValidIDLWithExtraSetupTest("-ami_callback", testHome + "/idl/compiler/misc/ami.idl"));
+        suite.addTest(new ValidIDLWithExtraSetupTest("-ami_callback", testHome + "/idl/compiler/misc/rt1180.idl"));
 
         return suite;
     }
