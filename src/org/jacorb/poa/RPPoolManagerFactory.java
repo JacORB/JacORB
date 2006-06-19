65fef6278c62360b1dea783e514b03ad229a8526
Alphonse Bendt <jacorb@prismtech one>
2006-06-19 10:29:53 +0000

    renamed variable

--- src/org/jacorb/poa/RPPoolManagerFactory.java
+++ src/org/jacorb/poa/RPPoolManagerFactory.java
@@ -45,14 +45,14 @@ public class RPPoolManagerFactory
             throw new ConfigurationException("jacorb.poa.thread_pool_max must be >= " + threadPoolMin + "(jacorb.poa.thread_pool_min)" );
         }
 
-        boolean isPoolShared = configuration.getAttributeAsBoolean("jacorb.poa.thread_pool_shared", false);
+        boolean poolsShouldBeShared = configuration.getAttributeAsBoolean("jacorb.poa.thread_pool_shared", false);
 
         if (logger.isDebugEnabled())
         {
-            logger.debug("RequestProcessorPoolFactory settings: thread_pool_min=" + threadPoolMin + " thread_pool_max=" + threadPoolMax + " thread_pool_shared=" + isPoolShared);
+            logger.debug("RequestProcessorPoolFactory settings: thread_pool_min=" + threadPoolMin + " thread_pool_max=" + threadPoolMax + " thread_pool_shared=" + poolsShouldBeShared);
         }
 
-        if (isPoolShared)
+        if (poolsShouldBeShared)
         {
             delegate = new SharedPoolFactory();
         }
