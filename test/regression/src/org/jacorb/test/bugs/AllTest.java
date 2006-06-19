1d7cc6fb52227ee4be1eef15c0f6a0e2611b3483
Alphonse Bendt <jacorb@prismtech one>
2006-06-19 13:23:43 +0000

    added test

--- test/regression/src/org/jacorb/test/bugs/AllTest.java
+++ test/regression/src/org/jacorb/test/bugs/AllTest.java
@@ -24,6 +24,7 @@ import junit.framework.*;
 
 import org.jacorb.test.bugs.bugjac10.TypeCodeTestCase;
 import org.jacorb.test.bugs.bugjac149.ObjectReplacementTest;
+import org.jacorb.test.bugs.bugjac178.POAThreadingTest;
 import org.jacorb.test.bugs.bugjac182.BugJac182Test;
 import org.jacorb.test.bugs.bugjac303.BugJac303Test;
 import org.jacorb.test.bugs.bugjac45.BugJac45Test;
@@ -66,6 +67,7 @@ public class AllTest extends JacORBTestSuite
       suite.addTestSuite(BugJac303Test.class);
       suite.addTest(ObjectReplacementTest.suite());
       suite.addTest(BugJac182Test.suite());
+      suite.addTest(POAThreadingTest.suite());
 
       return suite;
    }
