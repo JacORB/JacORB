bdda31e4803dd664cf3c5b31d3d86fd63ad4afb0
Alphonse Bendt <jacorb@prismtech one>
2006-06-22 09:51:04 +0000

    removed unused import

--- src/org/jacorb/notification/AbstractChannelFactory.java
+++ src/org/jacorb/notification/AbstractChannelFactory.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
