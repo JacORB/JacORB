43e51fd4d000ce3a1e5c2269d93ab2d82a280fa8
Alphonse Bendt <jacorb@prismtech one>
2006-07-06 12:38:01 +0000

    (initSecurity): new to disable ssl testing

--- test/regression/src/org/jacorb/test/orb/connection/BiDirSetup.java
+++ test/regression/src/org/jacorb/test/orb/connection/BiDirSetup.java
@@ -2,13 +2,18 @@ package org.jacorb.test.orb.connection;
 
 import java.util.Properties;
 
-import org.omg.PortableServer.*;
-import org.omg.BiDirPolicy.*;
-import org.omg.CORBA.*;
+import junit.framework.Test;
 
-import junit.framework.*;
-
-import org.jacorb.test.common.*;
+import org.jacorb.test.common.ClientServerSetup;
+import org.omg.BiDirPolicy.BIDIRECTIONAL_POLICY_TYPE;
+import org.omg.BiDirPolicy.BOTH;
+import org.omg.BiDirPolicy.BidirectionalPolicyValueHelper;
+import org.omg.CORBA.Any;
+import org.omg.CORBA.Policy;
+import org.omg.PortableServer.IdAssignmentPolicyValue;
+import org.omg.PortableServer.ImplicitActivationPolicyValue;
+import org.omg.PortableServer.LifespanPolicyValue;
+import org.omg.PortableServer.POA;
 
 /**
  * @author Andre Spiegel
@@ -17,8 +22,8 @@ import org.jacorb.test.common.*;
 public class BiDirSetup extends ClientServerSetup
 {
     private POA biDirPOA = null;
-    
-    public BiDirSetup (Test test, 
+
+    public BiDirSetup (Test test,
                        Properties clientProperties,
                        Properties serverProperties)
     {
@@ -30,19 +35,19 @@ public class BiDirSetup extends ClientServerSetup
     {
         return "org.jacorb.test.orb.connection.BiDirServerImpl";
     }
-    
+
     public void setUp() throws Exception
     {
         super.setUp();
-        
+
         Policy[] policies = new Policy[4];
-        policies[0] = 
+        policies[0] =
             clientRootPOA.create_lifespan_policy(LifespanPolicyValue.TRANSIENT);
 
-        policies[1] = 
+        policies[1] =
             clientRootPOA.create_id_assignment_policy(IdAssignmentPolicyValue.SYSTEM_ID);
 
-        policies[2] = 
+        policies[2] =
             clientRootPOA.create_implicit_activation_policy( ImplicitActivationPolicyValue.IMPLICIT_ACTIVATION );
 
         Any any = clientOrb.create_any();
@@ -53,7 +58,7 @@ public class BiDirSetup extends ClientServerSetup
         biDirPOA = clientRootPOA.create_POA( "BiDirPOA",
                                              clientRootPOA.the_POAManager(),
                                              policies );
-        biDirPOA.the_POAManager().activate();  
+        biDirPOA.the_POAManager().activate();
     }
 
     public POA getBiDirPOA()
@@ -61,4 +66,12 @@ public class BiDirSetup extends ClientServerSetup
         return biDirPOA;
     }
 
+    /**
+     * <code>initSecurity</code> is a dummy implementation to prevent security
+     * init for this test - this tests counts transports which are disrupted by
+     * security initialisation.
+     */
+    protected void initSecurity()
+    {
+    }
 }
