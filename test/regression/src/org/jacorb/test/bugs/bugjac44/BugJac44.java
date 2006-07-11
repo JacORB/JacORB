d667723f937574ec9844a7f4d32de41a7dc6cf86
Nick Cross <jacorb@goots.org>
2006-07-11 15:01:13 +0000

    JAC#31 Add test for i2jpackage.

---
+++ test/regression/src/org/jacorb/test/bugs/bugjac44/BugJac44.java
@@ -0,0 +1,34 @@
+package org.jacorb.test.bugs.bugjac44;
+
+/*
+ *        JacORB  - a free Java ORB
+ *
+ *   Copyright (C) 1997-2001  Gerald Brose.
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
+import apmInterface.SA_Connection;
+
+
+/**
+ *  Verifies that the IDL was generated into the correct package
+ *  by the import statement above.
+ *
+ */
+public class BugJac44
+{
+}
