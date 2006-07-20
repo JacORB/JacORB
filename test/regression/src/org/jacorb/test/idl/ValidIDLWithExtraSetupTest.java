6f3fc874bb1f43731647706b91ec614b7fd20b4b
Nick Cross <jacorb@goots.org>
2006-07-20 14:01:54 +0000

    Add bug514.idl

--- test/regression/src/org/jacorb/test/idl/ValidIDLWithExtraSetupTest.java
+++ test/regression/src/org/jacorb/test/idl/ValidIDLWithExtraSetupTest.java
@@ -174,7 +174,7 @@ public class ValidIDLWithExtraSetupTest extends AbstractIDLTestcase
         suite.addTest(new ValidIDLWithExtraSetupTest("-genEnhanced", "bugJac149.idl"));
         suite.addTest(new ValidIDLWithExtraSetupTest(new String[] {"-ami_callback", "-diistub"}, "ami.idl"));
         suite.addTest(new ValidIDLWithExtraSetupTest("-sloppy_names", TEST_HOME + "/idl/compiler/fail/sloppy.idl"));
-
+        suite.addTest(new ValidIDLWithExtraSetupTest(new String[] {"-ir", "-i2jpackage", "test:de.siemens.hyades.test"}, "bug514.idl"));
         return suite;
     }
 }
