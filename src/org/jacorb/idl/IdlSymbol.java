2fd4195117142179b0de7611da8cd7c95ccaa2cd
Alphonse Bendt <jacorb@prismtech one>
2006-08-03 12:31:17 +0000

    (printClassComment): don't use tabs in comment

--- src/org/jacorb/idl/IdlSymbol.java
+++ src/org/jacorb/idl/IdlSymbol.java
@@ -667,16 +667,16 @@ public class IdlSymbol
      * <code>printClassComment</code> is used by inherited classes to print
      * the class comment.
      *
-     * @param className a <code>String</code> value
+     * @param name a <code>String</code> value
      * @param ps a <code>PrintWriter</code> value
      */
     protected final void printClassComment(String type, String name, PrintWriter ps)
     {
         ps.println("/**");
-        ps.println(" *\tGenerated from IDL " + type.trim() + " \"" + name + "\".");
+        ps.println(" * Generated from IDL " + type.trim() + " \"" + name + "\".");
         ps.println(" *");
-        ps.println(" *\t@author JacORB IDL compiler V " + parser.compiler_version);
-        ps.println(" *\t@version generated at " + parser.currentDate);
+        ps.println(" * @author JacORB IDL compiler V " + parser.compiler_version);
+        ps.println(" * @version generated at " + parser.currentDate);
         ps.println(" */\n");
     }
 }
