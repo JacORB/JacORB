2383951664a4795288fe6923fc9d524a15136704
Andre Speigel <spiegel@drmirror.net>
2006-09-01 09:30:27 +0000

    (useCorbaloc): New field and accessor method. (createPOA, getCorbaloc): New methods. (main): Use them.

--- test/regression/src/org/jacorb/test/common/TestServer.java
+++ test/regression/src/org/jacorb/test/common/TestServer.java
@@ -20,13 +20,13 @@ package org.jacorb.test.common;
  *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+import java.util.*;
+
 import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.logger.Logger;
-import org.omg.CORBA.ORB;
-import org.omg.PortableServer.POA;
-import org.omg.PortableServer.POAHelper;
-import org.omg.PortableServer.Servant;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
 
 /**
  * A server program that can set up an arbitrary CORBA servant.
@@ -51,13 +51,90 @@ import org.omg.PortableServer.Servant;
  */
 public class TestServer
 {
+    private static Boolean useCorbaloc = null;
+    
+    /**
+     * Returns true if this TestServer should make its object available
+     * via a corbaloc URL.
+     */
+    public static boolean useCorbaloc()
+    {
+        if (useCorbaloc == null)
+        {
+            String prop = System.getProperty ("jacorb.test.corbaloc.enable",
+                                              "false");
+            useCorbaloc = Boolean.valueOf(prop);
+        }
+        return useCorbaloc.booleanValue();
+    }
+    
+    /**
+     * Creates and returns the POA with which the test server object should be
+     * registered.  In the normal case, this is just the RootPOA.  When
+     * Corbaloc is used, a child POA with the appropriate settings is created
+     * and returned.
+     */
+    public static POA createPOA (ORB orb) throws Exception
+    {
+        POA rootPOA = POAHelper.narrow
+        (
+            orb.resolve_initial_references ("RootPOA")
+        );
+        rootPOA.the_POAManager().activate();
+        if (useCorbaloc())
+        {
+            Policy[] p = new Policy[2];
+            p[0] = rootPOA.create_lifespan_policy (LifespanPolicyValue.PERSISTENT);
+            p[1] = rootPOA.create_id_assignment_policy (IdAssignmentPolicyValue.USER_ID);
+            
+            POA poa = rootPOA.create_POA
+            (
+                System.getProperty("jacorb.test.corbaloc.poaname"),
+                rootPOA.the_POAManager(), p
+            );
+            return poa;
+        }
+        else
+        {
+            return rootPOA;
+        }
+    }
+    
+    /**
+     * Returns the Corbaloc URL under which the test server object
+     * can be accessed.  This only works if jacorb.test.corbaloc.enable is
+     * true, and the additional parameters are set via properties, see below.
+     */
+    public static String getCorbaloc()
+    {
+        StringBuffer result = new StringBuffer();
+        result.append ("corbaloc::localhost:");
+        result.append (System.getProperty ("jacorb.test.corbaloc.port"));
+        result.append ("/");
+        result.append (System.getProperty ("jacorb.test.corbaloc.implname"));
+        result.append ("/");
+        result.append (System.getProperty ("jacorb.test.corbaloc.poaname"));
+        result.append ("/");
+        result.append (System.getProperty ("jacorb.test.corbaloc.objectid"));
+        return result.toString();
+    }
+    
     public static void main (String[] args)
     {
         Logger logger = null;
         try
         {
+            Properties props = new Properties();
+            if (useCorbaloc())
+            {
+                props.put ("OAPort",
+                           System.getProperty("jacorb.test.corbaloc.port"));
+                props.put ("jacorb.implname",
+                           System.getProperty("jacorb.test.corbaloc.implname"));
+            }
+            
             //init ORB
-            ORB orb = ORB.init( args, null );
+            ORB orb = ORB.init (args, props);
 
             log("init ORB");
 
@@ -72,9 +149,7 @@ public class TestServer
             }
 
             //init POA
-            POA poa =
-                POAHelper.narrow( orb.resolve_initial_references( "RootPOA" ));
-            poa.the_POAManager().activate();
+            POA poa = createPOA (orb);
 
             log("init POA");
 
@@ -93,10 +168,25 @@ public class TestServer
                 log("configured Servant");
             }
 
-            // create the object reference
-            org.omg.CORBA.Object obj = poa.servant_to_reference( servant );
-
-            System.out.println ("SERVER IOR: " + orb.object_to_string(obj));
+            if (useCorbaloc())
+            {
+                String oid = System.getProperty ("jacorb.test.corbaloc.objectid");
+                poa.activate_object_with_id (oid.getBytes(), servant);
+                String shortcut = System.getProperty ("jacorb.test.corbaloc.shortcut");
+                if (shortcut != null)
+                    ((org.jacorb.orb.ORB)orb).addObjectKey(shortcut,
+                            System.getProperty ("jacorb.test.corbaloc.implname")
+                    + "/" + System.getProperty ("jacorb.test.corbaloc.poaname")
+                    + "/" + System.getProperty ("jacorb.test.corbaloc.objectid"));
+                    
+                System.out.println ("SERVER IOR: " + getCorbaloc());
+            }
+            else
+            {
+                // create the object reference
+                org.omg.CORBA.Object obj = poa.servant_to_reference( servant );
+                System.out.println ("SERVER IOR: " + orb.object_to_string(obj));
+            }
             System.out.flush();
 
             if (logger != null)
