716c86f6741df573de4a2665b162012fdb07a5a0
Andre Speigel <spiegel@drmirror.net>
2007-02-06 09:17:10 +0000

    (read): Adapt to API change in ETF.idl.

--- test/regression/src/org/jacorb/test/orb/etf/wiop/WIOPConnection.java
+++ test/regression/src/org/jacorb/test/orb/etf/wiop/WIOPConnection.java
@@ -52,14 +52,14 @@ public class WIOPConnection extends _ConnectionLocalBase
         delegate.write (is_first, is_last, data, offset, length, time_out);
     }
 
-    public void read (BufferHolder data,
-                      int offset,
-                      int min_length,
-                      int max_length,
-                      long time_out)
+    public int read (BufferHolder data,
+                     int offset,
+                     int min_length,
+                     int max_length,
+                     long time_out)
     {
         WIOPFactories.setTransportInUse(true);
-        delegate.read (data, offset, min_length, max_length, time_out);
+        return delegate.read (data, offset, min_length, max_length, time_out);
     }
 
     public void flush()
