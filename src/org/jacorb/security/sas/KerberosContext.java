08661f38c2a12e6580c76a8444fabca482ded324
Alexander Bykov <alexander.bykov@prismtech.com>
2009-11-27 13:42:55 +0000

    Bugzilla 830 - fix for NullPointerException when connecting to non-SAS corba object

--- src/org/jacorb/security/sas/KerberosContext.java
+++ src/org/jacorb/security/sas/KerberosContext.java
@@ -20,18 +20,19 @@ package org.jacorb.security.sas;
  *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-import org.jacorb.config.*;
-import org.slf4j.Logger;
 import org.ietf.jgss.GSSContext;
 import org.ietf.jgss.GSSCredential;
 import org.ietf.jgss.GSSException;
 import org.ietf.jgss.GSSManager;
 import org.ietf.jgss.GSSName;
 import org.ietf.jgss.Oid;
+import org.jacorb.config.Configuration;
+import org.jacorb.config.ConfigurationException;
 import org.omg.CORBA.ORB;
 import org.omg.CSI.KRB5MechOID;
 import org.omg.CSIIOP.CompoundSecMechList;
 import org.omg.IOP.Codec;
+import org.slf4j.Logger;
 
 public class KerberosContext 
     implements ISASContext
@@ -58,11 +59,10 @@ public class KerberosContext
         {
             Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
             GSSManager gssManager = GSSManager.getInstance();
-            clientCreds = 
-                gssManager.createCredential(null, 
-                                            GSSCredential.INDEFINITE_LIFETIME, 
-                                            krb5Oid, 
-                                            GSSCredential.INITIATE_ONLY);
+            clientCreds = gssManager.createCredential(null,
+                                                      GSSCredential.INDEFINITE_LIFETIME,
+                                                      krb5Oid,
+                                                      GSSCredential.INITIATE_ONLY);
         } 
         catch (Exception e) 
         {
@@ -75,79 +75,106 @@ public class KerberosContext
         return KRB5MechOID.value.substring(4);
     }
 
-    public byte[] createClientContext(ORB orb, Codec codec, CompoundSecMechList csmList) {
-        // see if context supported
-        //if ((csmList.mechanism_list[0].as_context_mech.target_supports & EstablishTrustInClient.value) == 0) {
-        //  // SAS context not supported
-        //  return new byte[0];
-        //}
-
-        // check for acceptable security mech
-        //try {
-        //  byte[] mechOid = csmList.mechanism_list[0].as_context_mech.client_authentication_mech;
-        //  Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
-        //  if (!mechOid.equals(krb5Oid.getDER())) {
-        //  logger.warn("Kerberos mechanism not supported");
-        //  return new byte[0];
-        //  }
-        //} catch (GSSException e) {
-        //  logger.warn("Error getting Client Context: "+e);
-        //  return new byte[0];
-        //}
-
-        // generate context
+    public byte[] createClientContext(ORB orb, Codec codec, CompoundSecMechList csmList) 
+    {
         byte[] contextToken = new byte[0];
-        try {
-            byte[] target = csmList.mechanism_list[0].as_context_mech.target_name;
+        if ( csmList != null )
+        {
+            try
+            {
+                byte[] target = csmList.mechanism_list[0].as_context_mech.target_name;
 
-            Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
-            GSSManager gssManager = GSSManager.getInstance();
-            GSSName myPeer = gssManager.createName(target, null, krb5Oid);
-            if (clientCreds == null) clientCreds = gssManager.createCredential(null, GSSCredential.INDEFINITE_LIFETIME, krb5Oid, GSSCredential.INITIATE_ONLY);
-            GSSContext myContext = gssManager.createContext(myPeer, krb5Oid, clientCreds, GSSContext.INDEFINITE_LIFETIME);
-            contextToken = myContext.initSecContext(contextToken, 0, contextToken.length);
-        } catch (Exception e) {
-            logger.error("Error creating Kerberos context: "+e);
+                Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
+                GSSManager gssManager = GSSManager.getInstance();
+                GSSName myPeer = gssManager.createName(target, null, krb5Oid);
+                if (clientCreds == null)
+                {
+                    clientCreds = gssManager.createCredential(null, 
+                                                              GSSCredential.INDEFINITE_LIFETIME, 
+                                                              krb5Oid, 
+                                                              GSSCredential.INITIATE_ONLY);
+                }
+                GSSContext myContext = gssManager.createContext(myPeer, 
+                                                                krb5Oid, 
+                                                                clientCreds, 
+                                                                GSSContext.INDEFINITE_LIFETIME);
+                contextToken = myContext.initSecContext(contextToken, 0, contextToken.length);
+            } 
+            catch (Exception e) 
+            {
+                logger.error("Error creating Kerberos context: "+e);
+            }
         }
         return contextToken;
     }
 
-    public String getClientPrincipal() {
+    public String getClientPrincipal() 
+    {
         String principal = "";
-        try {
+        try 
+        {
             Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
             GSSManager gssManager = GSSManager.getInstance();
-            if (clientCreds == null) clientCreds = gssManager.createCredential(null, GSSCredential.INDEFINITE_LIFETIME, krb5Oid, GSSCredential.INITIATE_ONLY);
+            if (clientCreds == null)
+            {
+                clientCreds = gssManager.createCredential(null, 
+                                                          GSSCredential.INDEFINITE_LIFETIME, 
+                                                          krb5Oid, 
+                                                          GSSCredential.INITIATE_ONLY);
+            }
             principal = clientCreds.getName().toString();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             logger.error("Error getting created principal: "+e);
         }
         return principal;
     }
 
-    public void initTarget() {
-        try {
+    public void initTarget() 
+    {
+        try 
+        {
             Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
             GSSManager gssManager = GSSManager.getInstance();
-            if (targetCreds == null) targetCreds = gssManager.createCredential(null, GSSCredential.INDEFINITE_LIFETIME, krb5Oid, GSSCredential.ACCEPT_ONLY);
-        } catch (GSSException e) {
+            if (targetCreds == null)
+            { 
+                targetCreds = gssManager.createCredential(null, 
+                                                          GSSCredential.INDEFINITE_LIFETIME, 
+                                                          krb5Oid, 
+                                                          GSSCredential.ACCEPT_ONLY);
+            }
+        } 
+        catch (GSSException e) 
+        {
             logger.warn("Error accepting Kerberos context: "+e);
         }
     }
 
-    public boolean validateContext(ORB orb, Codec codec, byte[] contextToken) {
+    public boolean validateContext(ORB orb, Codec codec, byte[] contextToken) 
+    {
         byte[] token = null;
 
-        try {
+        try 
+        {
             Oid krb5Oid = new Oid(KRB5MechOID.value.substring(4));
             GSSManager gssManager = GSSManager.getInstance();
-            if (targetCreds == null) targetCreds = gssManager.createCredential(null, GSSCredential.INDEFINITE_LIFETIME, krb5Oid, GSSCredential.ACCEPT_ONLY);
+            if (targetCreds == null)
+            { 
+                targetCreds = gssManager.createCredential(null, 
+                                                          GSSCredential.INDEFINITE_LIFETIME, 
+                                                          krb5Oid, 
+                                                          GSSCredential.ACCEPT_ONLY);
+            }
             validatedContext = gssManager.createContext(targetCreds);
             token = validatedContext.acceptSecContext(contextToken, 0, contextToken.length);
-        } catch (GSSException e) {
+        } 
+        catch (GSSException e)
+        {
             logger.error("Error accepting Kerberos context: "+e);
         }
-        if (token == null) {
+        if (token == null)
+        {
             logger.warn("Could not accept token");
             return false;
         }
@@ -155,13 +182,22 @@ public class KerberosContext
         return true;
     }
 
-    public String getValidatedPrincipal() {
-        if (validatedContext == null) return null;
-        try {
-            return validatedContext.getSrcName().toString();
-        } catch (GSSException e) {
-            logger.error("Error getting name: "+e);
+    public String getValidatedPrincipal() 
+    {
+        if (validatedContext == null)
+        {
+            return null;
         }
+        
+        try 
+        {
+            return validatedContext.getSrcName().toString();
+        } 
+        catch (GSSException e) 
+        {
+            logger.error("Error getting name: " + e);
+        }
+        
         return null;
     }
 }
