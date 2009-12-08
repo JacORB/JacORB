0f47c832a30ec299aeccb59e5b254ce8af656b38
Alexander Bykov <alexander.bykov@prismtech.com>
2009-12-08 15:20:34 +0000

    SLF4J jars upgraded to the 1.5.10 version; updated documentation

--- demo/sas/GssUpServer.bat
+++ demo/sas/GssUpServer.bat
@@ -1,5 +1,5 @@
 @echo off
 set JACORBDEF=-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton
 set CP=build/classes
-set XCP=../../lib/jacorb.jar;../../lib/slf4j-api-1.5.6.jar;../../lib/slf4j-jdk14-1.5.6.jar;../../lib/antlr-2.7.2.jar;
+set XCP=../../lib/jacorb.jar;../../lib/slf4j-api-1.5.10.jar;../../lib/slf4j-jdk14-1.5.10.jar;../../lib/antlr-2.7.2.jar;
 java -Xbootclasspath/p:%XCP% -cp %CP% %JACORBDEF% -Dcustom.props=GssUpServer.properties org.jacorb.demo.sas.GssUpServer sas.ior
--- demo/sas/KerberosServer.bat
+++ demo/sas/KerberosServer.bat
@@ -2,5 +2,5 @@
 set JACORBDEF=-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton
 set KERBEROSDEF=-Djava.security.auth.login.config=SAS.login -Djava.security.krb5.realm=OPENROADSCONSULTING.COM -Djava.security.krb5.kdc=openroadsconsulting.com
 set CP=build/classes
-set XCP=../../lib/jacorb.jar;../../lib/slf4j-api-1.5.6.jar;../../lib/slf4j-jdk14-1.5.6.jar;../../lib/antlr-2.7.2.jar;
+set XCP=../../lib/jacorb.jar;../../lib/slf4j-api-1.5.10.jar;../../lib/slf4j-jdk14-1.5.10.jar;../../lib/antlr-2.7.2.jar;
 java -Xbootclasspath/p:%XCP% -cp %CP% %JACORBDEF% %KERBEROSDEF% -Dcustom.props=KerberosServer.properties org.jacorb.demo.sas.KerberosServer sas.ior testPass
