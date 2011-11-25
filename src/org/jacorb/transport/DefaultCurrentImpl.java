1d73dec67a7163cb9f7f1eefaeae5101084b1b1c
Nick Cross <jacorb@goots.org>
2011-11-25 20:33:04 +0000

    Remove compile warnings

--- src/org/jacorb/transport/DefaultCurrentImpl.java
+++ src/org/jacorb/transport/DefaultCurrentImpl.java
@@ -31,19 +31,19 @@ import org.omg.CORBA.LocalObject;
 import org.slf4j.Logger;
 
 /**
- * 
+ *
  * A default implementation of the
- * 
+ *
  * @see org.jacorb.transport.Current interface. Provides access to information
  *      about a Transport - the Traits.
- * 
+ *
  * @author Iliyan Jeliazkov
- * 
+ *
  */
 public class DefaultCurrentImpl extends LocalObject implements Current, TransportListener, Configurable {
 
     private int statistics_provider_index_ = -1;
-    
+
     /**
      * ctor
      */
@@ -52,28 +52,26 @@ public class DefaultCurrentImpl extends LocalObject implements Current, Transpor
     }
 
     public void configure(Configuration configuration) throws ConfigurationException {
-        
-        org.jacorb.config.Configuration cfg = (org.jacorb.config.Configuration)configuration;
-        
-        logger_ = cfg.getLogger ("jacorb.transport.current");
+
+       logger_ = configuration.getLogger ("jacorb.transport.current");
 
         // Plug-in a statistics provider, we know how to use
         List statsProviderClassNames =
-            cfg.getAttributeList( "jacorb.connection.statistics_providers");
+            configuration.getAttributeList( "jacorb.connection.statistics_providers");
         statsProviderClassNames.add(DefaultStatisticsProvider.class.getName());
-        
+
         StringBuffer buff = new StringBuffer();
         for (Iterator iter = statsProviderClassNames.iterator (); iter.hasNext ();) {
             buff.append (iter.next ());
             if (iter.hasNext ())
                 buff.append (',');
         }
-        cfg.setAttribute("jacorb.connection.statistics_providers", buff.toString());
-        
+        configuration.setAttribute("jacorb.connection.statistics_providers", buff.toString());
+
         statistics_provider_index_ = statsProviderClassNames.size () - 1;
     }
 
-    
+
 
     //
     // TCPConnectionListener interface
@@ -90,7 +88,7 @@ public class DefaultCurrentImpl extends LocalObject implements Current, Transpor
 
     /**
      * Retrieves the last event for the current thread.
-     * 
+     *
      * @return Event
      * @throws BadContext
      */
@@ -125,35 +123,35 @@ public class DefaultCurrentImpl extends LocalObject implements Current, Transpor
 
     public long bytes_sent() throws NoContext {
         Event e = getLatestTransportCurentEvent();
-        DefaultStatisticsProvider p = 
+        DefaultStatisticsProvider p =
             (DefaultStatisticsProvider)e.getStatisticsProvider (statistics_provider_index_);
         return p.bytes_sent_;
     }
 
     public long bytes_received() throws NoContext {
         Event e = getLatestTransportCurentEvent();
-        DefaultStatisticsProvider p = 
+        DefaultStatisticsProvider p =
             (DefaultStatisticsProvider)e.getStatisticsProvider (statistics_provider_index_);
         return p.bytes_received_;
     }
 
     public long messages_sent() throws NoContext {
         Event e = getLatestTransportCurentEvent();
-        DefaultStatisticsProvider p = 
+        DefaultStatisticsProvider p =
             (DefaultStatisticsProvider)e.getStatisticsProvider (statistics_provider_index_);
         return p.messages_sent_;
     }
 
     public long messages_received() throws NoContext {
         Event e = getLatestTransportCurentEvent();
-        DefaultStatisticsProvider p = 
+        DefaultStatisticsProvider p =
             (DefaultStatisticsProvider)e.getStatisticsProvider (statistics_provider_index_);
         return p.messages_received_;
     }
 
     public long open_since() throws NoContext {
         Event e = getLatestTransportCurentEvent();
-        DefaultStatisticsProvider p = 
+        DefaultStatisticsProvider p =
             (DefaultStatisticsProvider)e.getStatisticsProvider (statistics_provider_index_);
         return p.created_;
     }
