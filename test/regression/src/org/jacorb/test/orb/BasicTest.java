02fc7d77c6fd0ab5158b0ba7c45d92184445c650
Quynh Nguyen <nguyenq@stladvantage.com>
2012-08-16 14:51:49 -0500

    Adding the validate_connection test.

--- test/regression/src/org/jacorb/test/orb/BasicTest.java
+++ test/regression/src/org/jacorb/test/orb/BasicTest.java
@@ -35,6 +35,8 @@ import org.omg.CORBA.FloatHolder;
 import org.omg.CORBA.IntHolder;
 import org.omg.CORBA.LongHolder;
 import org.omg.CORBA.ShortHolder;
+import org.omg.CORBA.PolicyListHolder;
+import org.omg.CORBA.Policy;
 
 public class BasicTest extends ClientServerTestCase
 {
@@ -64,6 +66,9 @@ public class BasicTest extends ClientServerTestCase
                                    "org.jacorb.test.orb.BasicServerImpl" );
 
         suite.addTest( new BasicTest( "test_ping", setup ));
+        
+        // validate_connection test
+        suite.addTest( new BasicTest( "validate_connection", setup ));
 
         // short tests
         suite.addTest( new BasicTest( "test_pass_in_short", setup ) );
@@ -604,4 +609,15 @@ public class BasicTest extends ClientServerTestCase
         assertEquals( Double.POSITIVE_INFINITY, result, 0 );
     }
 
+
+	// validate_connection
+
+	public void validate_connection()
+	{
+		PolicyListHolder h = new PolicyListHolder(new Policy[2]);
+
+		boolean result = server._validate_connection(h);
+	}
+
 }
+
