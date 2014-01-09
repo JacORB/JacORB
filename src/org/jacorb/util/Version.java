e4577807f910941322e2d5ea84bfd45e7d7635ba
Nick Cross <ncross@redhat.com>
2014-01-09 15:14:02 +0000

    Update date

--- src/org/jacorb/util/Version.java
+++ src/org/jacorb/util/Version.java
@@ -29,7 +29,7 @@ package org.jacorb.util;
 public final class Version
 {
     public static final String version = "2.3.2-jbossorg-6";
-    public static final String date = "15-Oct-2013";
+    public static final String date = "09-Jan-2014";
     public static final String longVersion = version + ", " + date;
     public static final String yearString = "1997-2013";
 }
